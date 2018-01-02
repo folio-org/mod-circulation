a701be76ff345a0959ec9c4fb00ad91905d1b896
Marc Johnson <marc@k-int.co.uk>
2018-01-02 14:24:08 +0000

    Extract getting module instances during undeployment CIRC-58

--- okapi-registration/managed-deployment/undeploy.py
+++ okapi-registration/managed-deployment/undeploy.py
@@ -6,6 +6,18 @@ def find_module_id():
     with open('target/ModuleDescriptor.json') as descriptor_file:
         return json.load(descriptor_file)['id']
 
+def get_instances(okapi_address, module_id):
+    url = '{0}/_/discovery/modules/{1}'.format(okapi_address, module_id)
+
+    instances_response = requests.get(url)
+
+    if(instances_response.status_code == 200):
+        return list(map(lambda instance: instance['instId'], instances_response.json()))
+    else:
+        print('Could not enumerate instances of module {0}, status: {1}'.format(
+          module_id, instances_response.status_code))
+        return list()
+
 args = sys.argv
 
 module_id = find_module_id()
@@ -17,17 +29,7 @@ else:
     sys.stderr.write('Tenant ID must be passed on the command line')
     sys.exit()
 
-url = '{0}/_/discovery/modules/{1}'.format(okapi_address, module_id)
-
-instances_response = requests.get(url)
-
-if(instances_response.status_code == 200):
-    instance_ids = list(map(lambda instance: instance['instId'], instances_response.json()))
-
-    for instance_id in instance_ids:
-        instance_url = '{0}/_/discovery/modules/{1}/{2}'.format(okapi_address, module_id, instance_id)
-        delete_response = requests.delete(instance_url)
-        print('Delete Response for {0}: {1}'.format(instance_url, delete_response.status_code))
-
-else:
-    print('Could not enumerate instances of module {0}, status: {1}'.format(module_id, instances_response.status_code))
+for instance_id in get_instances(okapi_address, module_id):
+    instance_url = '{0}/_/discovery/modules/{1}/{2}'.format(okapi_address, module_id, instance_id)
+    delete_response = requests.delete(instance_url)
+    print('Delete Response for {0}: {1}'.format(instance_url, delete_response.status_code))
