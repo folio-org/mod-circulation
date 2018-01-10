8805a4b76b0e836dee837fa2bdee6348191f8102
Kurt Nordstrom <kurt.e.nordstrom@gmail.com>
2018-01-10 16:43:51 +0000

    Add missing file

---
+++ src/test/java/org/folio/circulation/api/loans/LoanAPIPolicyTests.java
@@ -0,0 +1,202 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.folio.circulation.api.loans;
+
+import java.util.UUID;
+import org.folio.circulation.api.support.APITests;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import io.vertx.core.json.JsonObject;
+import java.io.UnsupportedEncodingException;
+import java.lang.invoke.MethodHandles;
+import java.net.MalformedURLException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import org.folio.circulation.api.APITestSuite;
+import org.folio.circulation.api.support.builders.LoanRequestBuilder;
+import org.folio.circulation.api.support.http.InterfaceUrls;
+import org.folio.circulation.api.support.http.ResourceClient;
+import org.folio.circulation.support.http.client.IndividualResource;
+import org.folio.circulation.support.http.client.OkapiHttpClient;
+import org.folio.circulation.support.http.client.Response;
+import org.folio.circulation.support.http.client.ResponseHandler;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.junit.MatcherAssert.assertThat;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ *
+ * @author kurt
+ */
+public class LoanAPIPolicyTests extends APITests {
+  private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+    
+  static UUID pFallback;
+  static UUID p1;
+  static UUID p2;
+  static UUID p3;
+    
+  protected static final OkapiHttpClient client = APITestSuite.createClient(exception -> {
+    log.error("Request to circulation module failed:", exception);
+  });
+  
+  @Test
+  public void canRetrieveLoanPolicyId()
+    throws InterruptedException,
+    MalformedURLException,
+    TimeoutException,
+    UnsupportedEncodingException,
+    ExecutionException {
+    JsonObject itemJson1 = itemsFixture.basedUponInterestingTimes().getJson();
+    JsonObject itemJson2 = itemsFixture.basedUponSmallAngryPlanet().getJson();
+    JsonObject user1 = APITestSuite.userRecord1();
+    JsonObject user2 = APITestSuite.userRecord2();
+    UUID group1 = UUID.fromString(user1.getString("group"));
+    UUID group2 = UUID.fromString(user2.getString("group"));
+    UUID itemId1 = UUID.fromString(itemJson1.getString("id"));
+    UUID itemId2 = UUID.fromString(itemJson2.getString("id"));
+    UUID materialType1 = UUID.fromString(itemJson1.getString("materialTypeId"));
+    UUID materialType2 = UUID.fromString(itemJson1.getString("materialTypeId"));
+    UUID loanType1 = UUID.fromString(itemJson1.getString("permanentLoanTypeId"));
+    UUID loanType2 = UUID.fromString(itemJson1.getString("permanentLoanTypeId"));
+    
+    createLoanPolicies();
+    
+    //Set the loan rules
+    String rules = String.join("\n",
+      "fallback-policy: " + pFallback,
+      "m " + materialType1 + ": " + p1,
+      "m " + materialType1 + " + t " + loanType1 + " : " + p2,
+      "m " + materialType1 + " + t " + loanType1 + " + g " + group1 + " : " + p3//,
+      //"m " + mDefault + " + t " + tDefault + " + g " + gDefault + " + s " +  sDefault + " : " + pFourth
+      );
+    JsonObject newRulesRequest = new JsonObject().put("loanRulesAsTextFile", rules);
+    CompletableFuture<Response> completed = new CompletableFuture<>();
+
+    client.put(InterfaceUrls.loanRulesUrl(), newRulesRequest,
+      ResponseHandler.any(completed));
+
+    Response response = completed.get(5, TimeUnit.SECONDS);
+
+    assertThat(String.format(
+      "Failed to set loan rules: %s", response.getBody()),
+      response.getStatusCode(), is(204));
+    
+    //Get the loan rules
+    CompletableFuture<Response> getCompleted = new CompletableFuture<>();
+    client.get(InterfaceUrls.loanRulesUrl(), ResponseHandler.any(getCompleted));
+    Response getResponse = getCompleted.get(5, TimeUnit.SECONDS);
+    
+    JsonObject rulesJson = new JsonObject(getResponse.getBody());
+    
+    assertThat("Returned rules match submitted rules", rulesJson.getString("loanRulesAsTextFile"), is(rules));
+    
+    //Make a request for a new loan
+    IndividualResource loanResponse = loansClient.create(new LoanRequestBuilder()
+      .withId(UUID.randomUUID())
+      .withUserId(UUID.fromString(user1.getString("id")))
+      .withItemId(itemId1)
+      .withLoanDate(new DateTime(2017, 2, 27, 10, 23, 43, DateTimeZone.UTC))
+      .withDueDate(new DateTime(2017, 3, 29, 10, 23, 43, DateTimeZone.UTC))
+      .withStatus("Open"));
+
+    JsonObject loanJson = loanResponse.getJson();
+    ResourceClient policyResourceClient = ResourceClient.forLoanPolicies(client);
+    JsonObject policyJson = policyResourceClient.getById(UUID.fromString(loanJson.getString("loanPolicyId"))).getJson();
+    assertThat("policy is third policy", policyJson.getString("name"), is("Policy 1"));
+    
+    deleteLoanPolicies();
+  }
+  
+  private static void createLoanPolicies() 
+    throws InterruptedException,
+    MalformedURLException,
+    TimeoutException,
+    ExecutionException { 
+    
+    ResourceClient policyResourceClient = ResourceClient.forLoanPolicies(client);
+    
+    //policyResourceClient.deleteAll(); //Clear existing
+    
+    JsonObject p1Json = new JsonObject()
+       .put("name", "Policy 1")
+       .put("description", "Policy 1!!!")
+       .put("loanable", true)
+       .put("renewable", true)
+       .put("loansPolicy", new JsonObject()
+         .put("profileId", "ROLLING")
+         .put("closedLibraryDueDateManagementId", "KEEP_CURRENT_DATE"))
+       .put("renewalsPolicy", new JsonObject()
+         .put("renewFromId", "CURRENT_DUE_DATE")
+         .put("differentPeriod", false));
+    
+    p1 = policyResourceClient.create(p1Json).getId();
+    
+    JsonObject p2Json = new JsonObject()
+       .put("name", "Policy 2")
+       .put("description", "Policy 2!!!")
+       .put("loanable", true)
+       .put("renewable", true)
+       .put("loansPolicy", new JsonObject()
+         .put("profileId", "ROLLING")
+         .put("closedLibraryDueDateManagementId", "KEEP_CURRENT_DATE"))
+       .put("renewalsPolicy", new JsonObject()
+         .put("renewFromId", "CURRENT_DUE_DATE")
+         .put("differentPeriod", false));
+    
+    p2 = policyResourceClient.create(p2Json).getId();
+    
+    JsonObject p3Json = new JsonObject()
+       .put("name", "Policy 3")
+       .put("description", "Policy 3!!!")
+       .put("loanable", true)
+       .put("renewable", true)
+       .put("loansPolicy", new JsonObject()
+         .put("profileId", "ROLLING")
+         .put("closedLibraryDueDateManagementId", "KEEP_CURRENT_DATE"))
+       .put("renewalsPolicy", new JsonObject()
+         .put("renewFromId", "CURRENT_DUE_DATE")
+         .put("differentPeriod", false));
+    
+    p3 = policyResourceClient.create(p3Json).getId();    
+    
+    JsonObject pFallbackJson = new JsonObject()
+       .put("name", "Fallback")
+       .put("description", "Fallback!!!")
+       .put("loanable", false)
+       .put("renewable", false)
+       .put("loansPolicy", new JsonObject()
+         .put("profileId", "ROLLING")
+         .put("closedLibraryDueDateManagementId", "KEEP_CURRENT_DATE"))
+       .put("renewalsPolicy", new JsonObject()
+         .put("renewFromId", "CURRENT_DUE_DATE")
+         .put("differentPeriod", false));
+    
+    pFallback = policyResourceClient.create(pFallbackJson).getId();
+    
+  }
+  
+  private static void deleteLoanPolicies() 
+    throws MalformedURLException,
+    InterruptedException,
+    ExecutionException,
+    TimeoutException {
+    
+    ResourceClient policyResourceClient = ResourceClient.forLoanPolicies(client);
+    policyResourceClient.delete(p1);
+    policyResourceClient.delete(p2);
+    policyResourceClient.delete(p3);
+    policyResourceClient.delete(pFallback);
+  }
+}
