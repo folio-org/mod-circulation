20a4803d5f9e2857a5313fc089e6099f8ff37296
Julian Ladisch <julianladisch@users.noreply.github.com>
2018-01-29 14:49:13 +0100

    Replace duplicated String by constant.

--- src/main/java/org/folio/circulation/support/http/server/JsonResponse.java
+++ src/main/java/org/folio/circulation/support/http/server/JsonResponse.java
@@ -13,6 +13,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class JsonResponse {
+  private static final String MESSAGE = "message";
 
   //TODO: Needs a location
   public static void created(HttpServerResponse response,
@@ -53,7 +54,7 @@ public class JsonResponse {
       .collect(Collectors.toList()));
 
     JsonObject wrappedErrors = new JsonObject()
-      .put("message", message)
+      .put(MESSAGE, message)
       .put("parameters", parameters);
 
     response(response, wrappedErrors, 422);
@@ -61,7 +62,7 @@ public class JsonResponse {
 
   public static void loanRulesError(HttpServerResponse response, LoanRulesException e) {
     JsonObject body = new JsonObject();
-    body.put("message", e.getMessage());
+    body.put(MESSAGE, e.getMessage());
     body.put("line", e.getLine());
     body.put("column", e.getColumn());
     response(response, body, 422);
@@ -69,7 +70,7 @@ public class JsonResponse {
 
   public static void loanRulesError(HttpServerResponse response, DecodeException e) {
     JsonObject body = new JsonObject();
-    body.put("message", e.getMessage());  // already contains line and column number
+    body.put(MESSAGE, e.getMessage());  // already contains line and column number
     response(response, body, 422);
   }
 
