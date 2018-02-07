0f24bfdf1a84cc671a7f49dd293b2fe3c7616e51
Marc Johnson <marc@k-int.co.uk>
2018-02-07 13:56:18 +0000

    Move request to apply rules to loan rules client

--- src/main/java/org/folio/circulation/support/LoanRulesClient.java
+++ src/main/java/org/folio/circulation/support/LoanRulesClient.java
@@ -9,15 +9,18 @@ import org.slf4j.LoggerFactory;
 
 import java.lang.invoke.MethodHandles;
 import java.net.MalformedURLException;
+import java.net.URL;
 
 public class LoanRulesClient {
   private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
   private final OkapiHttpClient client;
-  private final WebContext context;
+  private final URL root;
 
-  public LoanRulesClient(OkapiHttpClient client, WebContext context) {
+  public LoanRulesClient(OkapiHttpClient client, WebContext context)
+    throws MalformedURLException {
+
     this.client = client;
-    this.context = context;
+    root = context.getOkapiBasedUrl("/circulation/loan-rules");
   }
 
   public void applyRules(
@@ -25,15 +28,24 @@ public class LoanRulesClient {
     String locationId,
     String materialTypeId,
     String patronGroup,
-    Handler<HttpClientResponse> responseHandler) throws MalformedURLException {
+    Handler<HttpClientResponse> responseHandler) {
 
-    String loanRulesQuery = String.format(
-      "?item_type_id=%s&loan_type_id=%s&patron_type_id=%s&shelving_location_id=%s",
-      materialTypeId, loanTypeId, patronGroup, locationId);
+    String loanRulesQuery = queryParameters(loanTypeId, locationId, materialTypeId, patronGroup);
 
     log.info(String.format("Applying loan rules for %s", loanRulesQuery));
 
-    client.get(context.getOkapiBasedUrl("/circulation/loan-rules/apply") +
-        loanRulesQuery, responseHandler);
+    client.get(String.format("%s/%s?%s", root, "apply", loanRulesQuery),
+      responseHandler);
+  }
+
+  private String queryParameters(
+    String loanTypeId,
+    String locationId,
+    String materialTypeId,
+    String patronGroup) {
+    
+    return String.format(
+      "item_type_id=%s&loan_type_id=%s&patron_type_id=%s&shelving_location_id=%s",
+      materialTypeId, loanTypeId, patronGroup, locationId);
   }
 }
