92d9fd5e1cdfe600706cd16d0ca93ed79b1d9049
Marc Johnson <marc@k-int.co.uk>
2018-02-12 16:49:27 +0000

    Update test via Okapi test for new versions

--- test-via-okapi.sh
+++ test-via-okapi.sh
@@ -9,7 +9,7 @@ circulation_instance_id=localhost-9605
 inventory_storage_module_id="mod-inventory-storage-7.2.2-SNAPSHOT"
 
 #Needs to be the specific version of mod-circulation-storage you want to use for testing
-circulation_storage_module_id="mod-circulation-storage-4.1.0-SNAPSHOT"
+circulation_storage_module_id="mod-circulation-storage-4.3.0-SNAPSHOT"
 
 #Needs to be the specific version of mod-users you want to use for testing
 users_storage_module_id="mod-users-14.4.1-SNAPSHOT"
