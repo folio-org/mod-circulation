89b08afb2ab95b4cf81b97f96ee950d5df86a6e8
Marc Johnson <marc@k-int.co.uk>
2018-04-20 12:15:11 +0100

    Move accept headers to standard headers in Okapi client CIRC-98

--- src/main/java/org/folio/circulation/support/http/client/OkapiHttpClient.java
+++ src/main/java/org/folio/circulation/support/http/client/OkapiHttpClient.java
@@ -49,7 +49,6 @@ public class OkapiHttpClient {
 
     HttpClientRequest request = client.postAbs(url.toString(), responseHandler);
 
-    request.headers().add("Accept","application/json, text/plain");
     request.headers().add("Content-type","application/json");
     request.headers().add(OKAPI_URL, okapiUrl.toString());
 
@@ -86,7 +85,6 @@ public class OkapiHttpClient {
 
     HttpClientRequest request = client.putAbs(url, responseHandler);
 
-    request.headers().add("Accept","application/json, text/plain");
     request.headers().add("Content-type","application/json");
     request.headers().add(OKAPI_URL, okapiUrl.toString());
 
@@ -121,7 +119,6 @@ public class OkapiHttpClient {
 
     HttpClientRequest request = client.getAbs(url, responseHandler);
 
-    request.headers().add("Accept","application/json");
     request.headers().add(OKAPI_URL, okapiUrl.toString());
 
     addStandardHeaders(request);
@@ -140,7 +137,6 @@ public class OkapiHttpClient {
 
     HttpClientRequest request = client.deleteAbs(url, responseHandler);
 
-    request.headers().add("Accept","application/json, text/plain");
     request.headers().add(OKAPI_URL, okapiUrl.toString());
 
     addStandardHeaders(request);
@@ -149,8 +145,9 @@ public class OkapiHttpClient {
 
     request.end();
   }
-
+  
   private void addStandardHeaders(HttpClientRequest request) {
+    addHeaderIfPresent(request, "Accept","application/json, text/plain");
     addHeaderIfPresent(request, TENANT, this.tenantId);
     addHeaderIfPresent(request, TOKEN, this.token);
     addHeaderIfPresent(request, USER_ID, this.userId);
