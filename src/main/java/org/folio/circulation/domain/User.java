e071eb1118dad298005cb44ea59e0a5dbabcf8ae
Marc Johnson <marc@k-int.co.uk>
2018-06-04 11:39:54 +0100

    Introduce property for internal representation of user

--- src/main/java/org/folio/circulation/domain/User.java
+++ src/main/java/org/folio/circulation/domain/User.java
@@ -6,8 +6,12 @@ import org.joda.time.DateTime;
 import static org.folio.circulation.support.JsonPropertyFetcher.getProperty;
 
 public class User extends JsonObject {
+  private final JsonObject representation;
+
   public User(JsonObject representation) {
     super(representation.getMap());
+
+    this.representation = representation;
   }
 
   Boolean isInactive() {
@@ -15,14 +19,15 @@ public class User extends JsonObject {
   }
 
   Boolean isActive() {
-    final Boolean active = getBoolean("active");
+    final Boolean active = representation.getBoolean("active");
 
     return active && !isExpired();
   }
 
   private Boolean isExpired() {
-    if(containsKey("expirationDate")) {
-      final DateTime expirationDate = DateTime.parse(getString("expirationDate"));
+    if(representation.containsKey("expirationDate")) {
+      final DateTime expirationDate = DateTime.parse(
+        representation.getString("expirationDate"));
 
       return expirationDate.isBefore(DateTime.now());
     }
@@ -32,6 +37,6 @@ public class User extends JsonObject {
   }
 
   public String getBarcode() {
-    return getProperty(this, "barcode");
+    return getProperty(representation, "barcode");
   }
 }
