bd956e84d84fc8093687ed8dda64ccbf2d0f46d4
Marc Johnson <marc@k-int.co.uk>
2018-06-19 17:34:10 +0100

    Pull tests for renewal by ID up to renewal tests base class CIRC-117

--- src/test/java/api/loans/RenewByIdTests.java
+++ src/test/java/api/loans/RenewByIdTests.java
@@ -1,121 +1,13 @@
 package api.loans;
 
-import api.APITestSuite;
-import io.vertx.core.json.JsonObject;
 import org.folio.circulation.support.http.client.IndividualResource;
 import org.folio.circulation.support.http.client.Response;
 import org.folio.circulation.support.http.server.ValidationError;
 import org.hamcrest.Matcher;
-import org.joda.time.DateTime;
-import org.joda.time.Seconds;
-import org.junit.Test;
 
-import java.net.MalformedURLException;
-import java.util.UUID;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeoutException;
-
-import static api.support.builders.ItemBuilder.CHECKED_OUT;
-import static api.support.matchers.ItemStatusCodeMatcher.hasItemStatus;
-import static api.support.matchers.TextDateTimeMatcher.withinSecondsAfter;
-import static api.support.matchers.ValidationErrorMatchers.*;
-import static org.hamcrest.CoreMatchers.allOf;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static api.support.matchers.ValidationErrorMatchers.hasParameter;
 
 public class RenewByIdTests extends RenewalTests {
-  @Test
-  public void canRenewRollingLoanFromSystemDate()
-    throws InterruptedException,
-    MalformedURLException,
-    TimeoutException,
-    ExecutionException {
-
-    IndividualResource smallAngryPlanet = itemsFixture.basedUponSmallAngryPlanet();
-    final IndividualResource jessica = usersFixture.jessica();
-
-    final UUID loanId = loansFixture.checkOutByBarcode(smallAngryPlanet, jessica,
-      new DateTime(2018, 4, 21, 11, 21, 43))
-      .getId();
-
-    //TODO: Renewal based upon system date,
-    // needs to be approximated, at least until we introduce a calendar and clock
-    DateTime approximateRenewalDate = DateTime.now();
-
-    final JsonObject renewedLoan = renew(smallAngryPlanet, jessica).getJson();
-
-    assertThat(renewedLoan.getString("id"), is(loanId.toString()));
-
-    assertThat("user ID should match barcode",
-      renewedLoan.getString("userId"), is(jessica.getId().toString()));
-
-    assertThat("item ID should match barcode",
-      renewedLoan.getString("itemId"), is(smallAngryPlanet.getId().toString()));
-
-    assertThat("status should be open",
-      renewedLoan.getJsonObject("status").getString("name"), is("Open"));
-
-    assertThat("action should be renewed",
-      renewedLoan.getString("action"), is("renewed"));
-
-    assertThat("renewal count should be incremented",
-      renewedLoan.getInteger("renewalCount"), is(1));
-
-    assertThat("last loan policy should be stored",
-      renewedLoan.getString("loanPolicyId"),
-      is(APITestSuite.canCirculateRollingLoanPolicyId().toString()));
-
-    assertThat("due date should be approximately 3 weeks after renewal date, based upon loan policy",
-      renewedLoan.getString("dueDate"),
-      withinSecondsAfter(Seconds.seconds(10), approximateRenewalDate.plusWeeks(3)));
-
-    smallAngryPlanet = itemsClient.get(smallAngryPlanet);
-
-    assertThat(smallAngryPlanet, hasItemStatus(CHECKED_OUT));
-  }
-
-  @Test
-  public void cannotRenewWhenItemCannotBeFound()
-    throws InterruptedException,
-    MalformedURLException,
-    TimeoutException,
-    ExecutionException {
-
-    final IndividualResource smallAngryPlanet = itemsFixture.basedUponSmallAngryPlanet();
-    final IndividualResource steve = usersFixture.steve();
-
-    loansFixture.checkOut(smallAngryPlanet, steve);
-
-    itemsClient.delete(smallAngryPlanet.getId());
-
-    Response response = attemptRenewal(smallAngryPlanet, steve);
-
-    assertThat(response.getJson(), hasErrorWith(allOf(
-      hasMessage(String.format("No item with ID %s exists", smallAngryPlanet.getId())),
-      matchItemRelatedParameter(smallAngryPlanet))));
-  }
-
-  @Test
-  public void cannotRenewLoanForDifferentUser()
-    throws InterruptedException,
-    MalformedURLException,
-    TimeoutException,
-    ExecutionException {
-
-    IndividualResource smallAngryPlanet = itemsFixture.basedUponSmallAngryPlanet();
-    IndividualResource james = usersFixture.james();
-    final IndividualResource jessica = usersFixture.jessica();
-
-    loansFixture.checkOutByBarcode(smallAngryPlanet, jessica,
-      new DateTime(2018, 4, 21, 11, 21, 43));
-
-    final Response response = attemptRenewal(smallAngryPlanet, james);
-
-    assertThat(response.getJson(), hasErrorWith(allOf(
-        hasMessage("Cannot renew item checked out to different user"),
-        matchUserRelatedParameter(james))));
-  }
-
   @Override
   Response attemptRenewal(IndividualResource user, IndividualResource item) {
     return loansFixture.attemptRenewalById(user, item);
