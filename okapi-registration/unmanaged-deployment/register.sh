cb66849a43fdc4a7d94cb424c9d34135c5158a18
Marc Johnson <marc@k-int.co.uk>
2018-06-26 12:53:48 +0100

    Register proxy for unmanaged module before discovery
    Might be related to OKAPI-608

--- okapi-registration/unmanaged-deployment/register.sh
+++ okapi-registration/unmanaged-deployment/register.sh
@@ -5,6 +5,12 @@ module_instance_id=${2}
 okapi_proxy_address=${3:-http://localhost:9130}
 tenant_id=${4:-demo_tenant}
 
+curl -w '\n' -D - -s \
+     -X POST \
+     -H "Content-type: application/json" \
+     -d @./target/ModuleDescriptor.json \
+     "${okapi_proxy_address}/_/proxy/modules"
+
 discovery_json=$(cat ./target/Discovery.json)
 
 discovery_json="${discovery_json/directaddresshere/$module_direct_address}"
@@ -14,12 +20,6 @@ curl -w '\n' -X POST -D -   \
      -H "Content-type: application/json"   \
      -d "${discovery_json}" \
      "${okapi_proxy_address}/_/discovery/modules"
-
-curl -w '\n' -D - -s \
-     -X POST \
-     -H "Content-type: application/json" \
-     -d @./target/ModuleDescriptor.json \
-     "${okapi_proxy_address}/_/proxy/modules"
 
 activate_json=$(cat ./target/activate.json)
 activate_json="${activate_json/moduleidhere/$module_id}"
