04758c59cba5cc69ee0081785c9f8df7fe8a0f4a
Marc Johnson <marc@k-int.co.uk>
2018-06-28 00:04:21 +0100

    Consolidate renewal API resources

--- src/main/java/org/folio/circulation/resources/RenewByBarcodeResource.java
+++ src/main/java/org/folio/circulation/resources/RenewByBarcodeResource.java
@@ -1,43 +1,25 @@
 package org.folio.circulation.resources;
 
 import io.vertx.core.http.HttpClient;
-import io.vertx.ext.web.Router;
-import io.vertx.ext.web.RoutingContext;
-import org.folio.circulation.domain.LoanRenewalService;
+import io.vertx.core.json.JsonObject;
+import org.folio.circulation.domain.Loan;
 import org.folio.circulation.domain.LoanRepository;
-import org.folio.circulation.domain.LoanRepresentation;
-import org.folio.circulation.domain.representations.LoanResponse;
-import org.folio.circulation.support.*;
-import org.folio.circulation.support.http.server.WebContext;
+import org.folio.circulation.support.HttpResult;
 
-public class RenewByBarcodeResource extends Resource {
+import java.util.concurrent.CompletableFuture;
+
+public class RenewByBarcodeResource extends RenewalResource {
   public RenewByBarcodeResource(HttpClient client) {
-    super(client);
+    super(client, "/circulation/renew-by-barcode");
   }
 
-  public void register(Router router) {
-    RouteRegistration routeRegistration = new RouteRegistration(
-      "/circulation/renew-by-barcode", router);
+  @Override
+  protected CompletableFuture<HttpResult<Loan>> findLoan(
+    JsonObject request,
+    LoanRepository loanRepository) {
 
-    routeRegistration.create(this::renew);
-  }
-
-  private void renew(RoutingContext routingContext) {
-    final WebContext context = new WebContext(routingContext);
-    final Clients clients = Clients.create(context, client);
-    final LoanRepository loanRepository = new LoanRepository(clients);
-    final LoanRepresentation loanRepresentation = new LoanRepresentation();
-    final LoanRenewalService loanRenewalService = LoanRenewalService.using(clients);
-
-    //TODO: Validation check for same user should be in the domain service
-
-    RenewByBarcodeRequest.from(routingContext.getBodyAsJson())
-      .after(loanRepository::findOpenLoanByBarcode)
-      .thenComposeAsync(r -> r.after(loanRenewalService::renew))
-      .thenComposeAsync(r -> r.after(loanRepository::updateLoan))
-      .thenApply(r -> r.map(loanRepresentation::extendedLoan))
-      .thenApply(LoanResponse::from)
-      .thenAccept(result -> result.writeTo(routingContext.response()));
+    return RenewByBarcodeRequest.from(request)
+      .after(loanRepository::findOpenLoanByBarcode);
   }
 
 }
---
+++ src/main/java/org/folio/circulation/resources/RenewalResource.java
@@ -0,0 +1,55 @@
+package org.folio.circulation.resources;
+
+import io.vertx.core.http.HttpClient;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import org.folio.circulation.domain.Loan;
+import org.folio.circulation.domain.LoanRenewalService;
+import org.folio.circulation.domain.LoanRepository;
+import org.folio.circulation.domain.LoanRepresentation;
+import org.folio.circulation.domain.representations.LoanResponse;
+import org.folio.circulation.support.Clients;
+import org.folio.circulation.support.HttpResult;
+import org.folio.circulation.support.RouteRegistration;
+import org.folio.circulation.support.http.server.WebContext;
+
+import java.util.concurrent.CompletableFuture;
+
+public abstract class RenewalResource extends Resource {
+  private final String rootPath;
+
+  RenewalResource(HttpClient client, String rootPath) {
+    super(client);
+    this.rootPath = rootPath;
+  }
+
+  @Override
+  public void register(Router router) {
+    RouteRegistration routeRegistration = new RouteRegistration(
+      rootPath, router);
+
+    routeRegistration.create(this::renew);
+  }
+
+  private void renew(RoutingContext routingContext) {
+    final WebContext context = new WebContext(routingContext);
+    final Clients clients = Clients.create(context, client);
+    final LoanRepository loanRepository = new LoanRepository(clients);
+    final LoanRepresentation loanRepresentation = new LoanRepresentation();
+    final LoanRenewalService loanRenewalService = LoanRenewalService.using(clients);
+
+    //TODO: Validation check for same user should be in the domain service
+
+    findLoan(routingContext.getBodyAsJson(), loanRepository)
+      .thenComposeAsync(r -> r.after(loanRenewalService::renew))
+      .thenComposeAsync(r -> r.after(loanRepository::updateLoan))
+      .thenApply(r -> r.map(loanRepresentation::extendedLoan))
+      .thenApply(LoanResponse::from)
+      .thenAccept(result -> result.writeTo(routingContext.response()));
+  }
+
+  protected abstract CompletableFuture<HttpResult<Loan>> findLoan(
+    JsonObject request,
+    LoanRepository loanRepository);
+}
