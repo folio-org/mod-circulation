b12bfe6bc2b3c07c618d3c2907f554814fa51272
Marc Johnson <marc@k-int.co.uk>
2018-07-04 16:17:45 +0100

    Extract function in single record mapper for failure scenarios

--- src/main/java/org/folio/circulation/support/SingleRecordMapper.java
+++ src/main/java/org/folio/circulation/support/SingleRecordMapper.java
@@ -7,14 +7,22 @@ import java.util.function.Function;
 
 public class SingleRecordMapper<T> {
   private final Function<JsonObject, T> mapper;
+  private final Function<Response, HttpResult<T>> resultOnFailure;
 
   public SingleRecordMapper(Function<JsonObject, T> mapper) {
+    this(mapper, (response -> HttpResult.failed(new ForwardOnFailure(response))));
+  }
+
+  private SingleRecordMapper(
+    Function<JsonObject, T> mapper,
+    Function<Response, HttpResult<T>> responseHttpResultFunction) {
     this.mapper = mapper;
+    resultOnFailure = responseHttpResultFunction;
   }
 
   public HttpResult<T> mapFrom(Response response) {
     return response != null && response.getStatusCode() == 200
     ? HttpResult.succeeded(mapper.apply(response.getJson()))
-    : HttpResult.failed(new ForwardOnFailure(response));
+    : resultOnFailure.apply(response);
   }
 }
