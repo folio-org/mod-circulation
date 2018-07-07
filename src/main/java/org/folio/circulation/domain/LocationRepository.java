f9be077a317cb380daa372c4460d5b611198c34c
Marc Johnson <marc@k-int.co.uk>
2018-07-07 20:34:04 +0100

    Use single record fetcher when getting a location

--- src/main/java/org/folio/circulation/domain/LocationRepository.java
+++ src/main/java/org/folio/circulation/domain/LocationRepository.java
@@ -3,57 +3,34 @@ package org.folio.circulation.domain;
 import io.vertx.core.json.JsonObject;
 import org.apache.commons.lang3.StringUtils;
 import org.folio.circulation.support.*;
-import org.folio.circulation.support.http.client.Response;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.lang.invoke.MethodHandles;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import static java.util.function.Function.identity;
 
 public class LocationRepository {
-  private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
-
   private CollectionResourceClient locationsStorageClient;
 
   public LocationRepository(Clients clients) {
     locationsStorageClient = clients.locationsStorage();
   }
 
-  public CompletableFuture<HttpResult<Item>> getLocation(
-    Item item) {
-
-    return getLocation(item.getLocationId(), item.getItemId())
+  public CompletableFuture<HttpResult<Item>> getLocation(Item item) {
+    return getLocation(item.getLocationId())
       .thenApply(result -> result.map(item::withLocation));
   }
 
-  private CompletableFuture<HttpResult<JsonObject>> getLocation(
-    String locationId,
-    String itemId) {
+  private CompletableFuture<HttpResult<JsonObject>> getLocation(String locationId) {
 
     //TODO: Add functions to explicitly distinguish between fatal not found
     // and allowable not found
-    final Function<Response, HttpResult<JsonObject>> mapResponse = response -> {
-      if(response != null && response.getStatusCode() == 200) {
-        return HttpResult.succeeded(response.getJson());
-      }
-      else {
-        log.warn("Could not get location {} for item {}",
-          locationId, itemId);
-
-        return HttpResult.succeeded(null);
-      }
-    };
-
-    return locationsStorageClient.get(locationId)
-      .thenApply(mapResponse)
-      .exceptionally(e -> HttpResult.failed(new ServerErrorFailure(e)));
+    return SingleRecordFetcher.json(locationsStorageClient, "locations",
+      response -> HttpResult.succeeded(null))
+      .fetchSingleRecord(locationId);
   }
 
   public CompletableFuture<HttpResult<Map<String, JsonObject>>> getLocations(
