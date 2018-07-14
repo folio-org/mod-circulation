990688458c88c6c27cf1ca1586adaafd82a017ea
Marc Johnson <marc@k-int.co.uk>
2018-07-14 13:12:12 +0100

    Use multiple records class when fetching request queue

--- src/main/java/org/folio/circulation/domain/RequestQueueFetcher.java
+++ src/main/java/org/folio/circulation/domain/RequestQueueFetcher.java
@@ -1,21 +1,13 @@
 package org.folio.circulation.domain;
 
-import io.vertx.core.json.JsonArray;
 import org.folio.circulation.support.Clients;
 import org.folio.circulation.support.HttpResult;
-import org.folio.circulation.support.ServerErrorFailure;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.lang.invoke.MethodHandles;
 import java.util.concurrent.CompletableFuture;
 
 import static org.folio.circulation.support.CqlHelper.encodeQuery;
-import static org.folio.circulation.support.JsonArrayHelper.mapToList;
 
 public class RequestQueueFetcher {
-  private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
-
   private final Clients clients;
 
   public RequestQueueFetcher(Clients clients) {
@@ -41,21 +33,10 @@ public class RequestQueueFetcher {
         final int maximumSupportedRequestQueueSize = 1000;
 
         return clients.requestsStorage().getMany(query, maximumSupportedRequestQueueSize, 0)
-          .thenApply(
-            response -> {
-              if (response.getStatusCode() == 200) {
-                final JsonArray foundRequests = response.getJson().getJsonArray("requests");
-
-                log.info("Found request queue: {}", foundRequests.encodePrettily());
-
-                return HttpResult.succeeded(
-                  new RequestQueue(mapToList(foundRequests, Request::from)));
-              } else {
-                return HttpResult.failed(new ServerErrorFailure(
-                  String.format("Failed to fetch request queue: %s: %s",
-                    response.getStatusCode(), response.getBody())));
-              }
-        });
+          .thenApply(response ->
+            MultipleRecords.from(response, Request::from, "requests")
+              .map(MultipleRecords::getRecords)
+              .map(RequestQueue::new));
       });
   }
 }
