2724c795eed9d42e29117ef3bfd3e0d376535514
jeremythuff <jeremythuff@gmail.com>
2018-11-13 16:51:54 -0600

    Added initial files for checkin api

---
+++ src/main/java/org/folio/circulation/resources/CheckInByBarcodeRequest.java
@@ -0,0 +1,63 @@
+package org.folio.circulation.resources;
+
+import io.vertx.core.json.JsonObject;
+import org.apache.commons.lang3.StringUtils;
+import org.folio.circulation.domain.FindByBarcodeQuery;
+import org.folio.circulation.domain.User;
+import org.folio.circulation.support.HttpResult;
+import org.folio.circulation.support.ValidationErrorFailure;
+
+import static org.folio.circulation.support.HttpResult.succeeded;
+import static org.folio.circulation.support.JsonPropertyFetcher.getProperty;
+import static org.folio.circulation.support.ValidationErrorFailure.failedResult;
+import static org.folio.circulation.support.ValidationErrorFailure.failure;
+
+public class CheckInByBarcodeRequest implements FindByBarcodeQuery {
+  private static final String USER_BARCODE = "userBarcode";
+  private static final String ITEM_BARCODE = "itemBarcode";
+
+  private final String itemBarcode;
+  private final String userBarcode;
+
+  private CheckInByBarcodeRequest(String itemBarcode, String userBarcode) {
+    this.itemBarcode = itemBarcode;
+    this.userBarcode = userBarcode;
+  }
+
+  public static HttpResult<CheckInByBarcodeRequest> from(JsonObject json) {
+    final String itemBarcode = getProperty(json, ITEM_BARCODE);
+
+    if(StringUtils.isBlank(itemBarcode)) {
+      return failedResult("Renewal request must have an item barcode", ITEM_BARCODE, null);
+    }
+
+    final String userBarcode = getProperty(json, USER_BARCODE);
+
+    if(StringUtils.isBlank(userBarcode)) {
+      return failedResult("Renewal request must have a user barcode", USER_BARCODE, null);
+    }
+
+    return succeeded(new CheckInByBarcodeRequest(itemBarcode, userBarcode));
+  }
+
+  @Override
+  public String getItemBarcode() {
+    return itemBarcode;
+  }
+
+  @Override
+  public String getUserBarcode() {
+    return userBarcode;
+  }
+
+  @Override
+  public ValidationErrorFailure userDoesNotMatchError() {
+    return failure("Cannot renew item checked out to different user",
+      USER_BARCODE, getUserBarcode());
+  }
+
+  @Override
+  public boolean userMatches(User user) {
+    return StringUtils.equals(user.getBarcode(), getUserBarcode());
+  }
+}
