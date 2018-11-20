5e8b33dbcc77aa205d1112b9f8585edd96cefbb8
Marc Johnson <marc@k-int.co.uk>
2018-11-20 10:37:56 +0000

    Use explicit parameters when fetching loans related to requests CIRC-165

--- src/main/java/org/folio/circulation/domain/LoanRepository.java
+++ src/main/java/org/folio/circulation/domain/LoanRepository.java
@@ -296,7 +296,7 @@ public class LoanRepository {
 
     HttpResult<String> queryResult = CqlHelper.encodeQuery(openLoansQuery);
     
-    return queryResult.after(query -> loansStorageClient.getMany(query)
+    return queryResult.after(query -> loansStorageClient.getMany(query, requests.size(), 0)
         .thenApply(this::mapResponseToLoans)
         .thenApply(multipleLoansResult -> multipleLoansResult.next(
           multipleLoans -> {
