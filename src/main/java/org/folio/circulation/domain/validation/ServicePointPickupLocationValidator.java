aa6dc662f1b4d33c14c216f7ada676c95735e7f7
Marc Johnson <marc@k-int.co.uk>
2018-11-22 11:56:23 +0000

    Remove redundant future when checking pickup service point CIRC-152

--- src/main/java/org/folio/circulation/domain/validation/ServicePointPickupLocationValidator.java
+++ src/main/java/org/folio/circulation/domain/validation/ServicePointPickupLocationValidator.java
@@ -1,10 +1,9 @@
 package org.folio.circulation.domain.validation;
 
-import static java.util.concurrent.CompletableFuture.completedFuture;
 import static org.folio.circulation.support.HttpResult.failed;
 
 import java.lang.invoke.MethodHandles;
-import java.util.concurrent.CompletableFuture;
+
 import org.folio.circulation.domain.Request;
 import org.folio.circulation.domain.RequestAndRelatedRecords;
 import org.folio.circulation.support.HttpResult;
@@ -15,32 +14,32 @@ import org.slf4j.LoggerFactory;
 public class ServicePointPickupLocationValidator {
   private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
   
-  public CompletableFuture<HttpResult<RequestAndRelatedRecords>> checkServicePointPickupLocation(
+  public HttpResult<RequestAndRelatedRecords> checkServicePointPickupLocation(
       HttpResult<RequestAndRelatedRecords> requestResult) {
-    
+
     Request request = null;
     if(requestResult.value() != null) {
       request = requestResult.value().getRequest();
     }
     if(request == null) {
       log.info("No request present in RequestAndRelatedRecords object");
-      return completedFuture(requestResult);
+      return requestResult;
     }
     if(request.getPickupServicePoint() != null) {
       log.info("Request {} has non-null pickup location", request.getId());
       if(request.getPickupServicePoint().isPickupLocation()) {
-        return completedFuture(requestResult);
+        return requestResult;
       } else {            
         log.info("Request {} has {} as a pickup location which is an invalid service point", 
             request.getId(), request.getPickupServicePointId());
 
-        return completedFuture(failed(ValidationErrorFailure.failure(
+        return failed(ValidationErrorFailure.failure(
             "Service point is not a pickup location", "pickupServicePointId",
-            request.getPickupServicePointId())));
+            request.getPickupServicePointId()));
       }
     } else {
       log.info("Request {} has null pickup location", request.getId());
-      return completedFuture(requestResult);
+      return requestResult;
     }
     
   }
