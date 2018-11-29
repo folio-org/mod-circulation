dd4e2263cf9d78552c5e3949ede7960b623b45ca
Marc Johnson <marc@k-int.co.uk>
2018-11-29 14:15:42 +0000

    Check in when returning at primary service point for home location makes item available CIRC-146

--- src/test/java/api/loans/scenarios/InTransitToHomeLocationTests.java
+++ src/test/java/api/loans/scenarios/InTransitToHomeLocationTests.java
@@ -251,6 +251,79 @@ public class InTransitToHomeLocationTests extends APITests {
   }
 
   @Test
+  public void isAvailableWhenCheckedInToReturnItemAtPrimaryServicePointForHomeLocation()
+    throws InterruptedException,
+    MalformedURLException,
+    TimeoutException,
+    ExecutionException {
+
+    final IndividualResource primaryServicePoint = servicePointsFixture.cd1();
+
+    final IndividualResource homeLocation =
+      locationsFixture.basedUponExampleLocation(builder -> builder
+        .servedBy(primaryServicePoint.getId())
+        .withPrimaryServicePoint(primaryServicePoint.getId()));
+
+    final IndividualResource james = usersFixture.james();
+
+    final IndividualResource nod = itemsFixture.basedUponNod(builder ->
+      builder.withTemporaryLocation(homeLocation.getId()));
+
+    final IndividualResource loan = loansFixture.checkOut(nod, james);
+
+    final CheckInByBarcodeResponse checkInResponse = loansFixture.checkInByBarcode(
+      new CheckInByBarcodeRequestBuilder()
+        .forItem(nod)
+        .at(primaryServicePoint.getId()));
+
+    JsonObject itemRepresentation = checkInResponse.getItem();
+
+    assertThat("item should be present in response",
+      itemRepresentation, notNullValue());
+
+    assertThat("title is included for item",
+      itemRepresentation.getString("title"), Is.is("Nod"));
+
+    assertThat("barcode is included for item",
+      itemRepresentation.getString("barcode"), Is.is("565578437802"));
+
+    assertThat("item status is not available",
+      itemRepresentation.getJsonObject("status").getString("name"), is("Available"));
+
+    assertThat("available item should not have a destination",
+      itemRepresentation.containsKey("inTransitDestinationServicePointId"),
+      is(false));
+
+    JsonObject loanRepresentation = checkInResponse.getLoan();
+
+    assertThat("closed loan should be present in response",
+      loanRepresentation, notNullValue());
+
+    assertThat("in transit item (in loan) should have a destination",
+      loanRepresentation.getJsonObject("item")
+        .containsKey("inTransitDestinationServicePointId"), is(false));
+
+    JsonObject updatedNod = itemsClient.getById(nod.getId()).getJson();
+
+    assertThat("stored item status is not available",
+      updatedNod.getJsonObject("status").getString("name"), is("Available"));
+
+    assertThat("available item in storage should not have a destination",
+      updatedNod.containsKey("inTransitDestinationServicePointId"), is(false));
+
+    final JsonObject storedLoan = loansStorageClient.getById(loan.getId()).getJson();
+
+    assertThat("stored loan status is not closed",
+      storedLoan.getJsonObject("status").getString("name"), is("Closed"));
+
+    assertThat("item status snapshot in storage is not available",
+      storedLoan.getString("itemStatus"), is("Available"));
+
+    assertThat("Checkin Service Point Id should be stored",
+      storedLoan.getString("checkinServicePointId"), is(primaryServicePoint.getId()));
+  }
+
+  @Test
   public void isAvailableWhenCheckedInToReturnItemAtOtherServingServicePointForHomeLocation()
     throws InterruptedException,
     MalformedURLException,
