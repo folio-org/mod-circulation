737c227a4d7d1e130bce5e9aaf729d88facd7d39
Oleksii Maksymov <Oleksii_Maksymov@epam.com>
2019-01-21 15:19:18 +0200

    fixed merge conflict

--- src/main/java/org/folio/circulation/domain/policy/LoanPolicy.java
+++ src/main/java/org/folio/circulation/domain/policy/LoanPolicy.java
@@ -14,6 +14,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
 
+import static org.folio.circulation.domain.policy.LoansPolicyProfile.getProfileByName;
 import static org.folio.circulation.support.HttpResult.failed;
 import static org.folio.circulation.support.JsonPropertyFetcher.getBooleanProperty;
 import static org.folio.circulation.support.JsonPropertyFetcher.getIntegerProperty;
