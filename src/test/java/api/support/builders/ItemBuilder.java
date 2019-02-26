6c9091706c1c51fdc8fd8a70d7a7ac443bc62812
Martin Tran <mtran@ebsco.com>
2019-02-26 11:42:44 -0500

    restore capability to set MISSING status for item

--- src/test/java/api/support/builders/ItemBuilder.java
+++ src/test/java/api/support/builders/ItemBuilder.java
@@ -13,6 +13,7 @@ public class ItemBuilder extends JsonBuilder implements Builder {
   public static final String AWAITING_PICKUP = "Awaiting pickup";
   public static final String IN_TRANSIT = "In transit";
   public static final String PAGED = "Paged";
+  public static final String MISSING = "Missing";
 
   private final UUID id;
   private final UUID holdingId;
@@ -78,6 +79,8 @@ public class ItemBuilder extends JsonBuilder implements Builder {
   public ItemBuilder available() {
     return withStatus(AVAILABLE);
   }
+
+  public ItemBuilder missing() {return withStatus(MISSING); }
 
   private ItemBuilder withStatus(String status) {
     return new ItemBuilder(
@@ -210,7 +213,7 @@ public class ItemBuilder extends JsonBuilder implements Builder {
       loanTypeId,
       this.enumeration);
   }
-  
+
   public ItemBuilder withId(UUID id) {
     return new ItemBuilder(
       id,
@@ -224,7 +227,7 @@ public class ItemBuilder extends JsonBuilder implements Builder {
       this.temporaryLoanTypeId,
       this.enumeration);
   }
-  
+
   public ItemBuilder withEnumeration(String enumeration) {
     return new ItemBuilder(
       id,
