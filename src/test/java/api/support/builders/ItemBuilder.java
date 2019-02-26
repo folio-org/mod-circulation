3ea1f6d312e96115561b7fd170bef379f78fe2df
Marc Johnson <marc@k-int.co.uk>
2019-02-26 15:55:17 +0000

    Undo unexpected changes to item builder CIRC-207 CIRC-208

--- src/test/java/api/support/builders/ItemBuilder.java
+++ src/test/java/api/support/builders/ItemBuilder.java
@@ -13,7 +13,6 @@ public class ItemBuilder extends JsonBuilder implements Builder {
   public static final String AWAITING_PICKUP = "Awaiting pickup";
   public static final String IN_TRANSIT = "In transit";
   public static final String PAGED = "Paged";
-  public static final String MISSING = "Missing";
 
   private final UUID id;
   private final UUID holdingId;
@@ -72,11 +71,13 @@ public class ItemBuilder extends JsonBuilder implements Builder {
     return itemRequest;
   }
 
-  public ItemBuilder checkOut() { return withStatus(CHECKED_OUT); }
+  public ItemBuilder checkOut() {
+    return withStatus(CHECKED_OUT);
+  }
 
-  public ItemBuilder available() { return withStatus(AVAILABLE); }
-
-  public ItemBuilder missing() {return withStatus(MISSING); }
+  public ItemBuilder available() {
+    return withStatus(AVAILABLE);
+  }
 
   private ItemBuilder withStatus(String status) {
     return new ItemBuilder(
@@ -209,7 +210,7 @@ public class ItemBuilder extends JsonBuilder implements Builder {
       loanTypeId,
       this.enumeration);
   }
-
+  
   public ItemBuilder withId(UUID id) {
     return new ItemBuilder(
       id,
@@ -223,7 +224,7 @@ public class ItemBuilder extends JsonBuilder implements Builder {
       this.temporaryLoanTypeId,
       this.enumeration);
   }
-
+  
   public ItemBuilder withEnumeration(String enumeration) {
     return new ItemBuilder(
       id,
