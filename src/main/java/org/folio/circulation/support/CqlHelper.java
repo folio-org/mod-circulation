3180b9d12d3f3c94ed21a7bc313df5fdeb5d36d3
Marc Johnson <marc@k-int.co.uk>
2019-05-02 17:30:13 +0100

    Move encoding of CQL parameter to CQL query class as only usage CIRC-284

--- src/main/java/org/folio/circulation/support/CqlHelper.java
+++ src/main/java/org/folio/circulation/support/CqlHelper.java
@@ -1,31 +1,16 @@
 package org.folio.circulation.support;
 
-import static java.lang.String.valueOf;
 import static org.folio.circulation.support.Result.failed;
-import static org.folio.circulation.support.Result.of;
 
-import java.lang.invoke.MethodHandles;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class CqlHelper {
-  private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
-
   private CqlHelper() { }
-
-  static Result<String> encodeQuery(String cqlQuery) {
-    log.info("Encoding query {}", cqlQuery);
-
-    return of(() -> URLEncoder.encode(cqlQuery, valueOf(StandardCharsets.UTF_8)));
-  }
 
   /**
    *
--- src/main/java/org/folio/circulation/support/CqlQuery.java
+++ src/main/java/org/folio/circulation/support/CqlQuery.java
@@ -1,6 +1,18 @@
 package org.folio.circulation.support;
 
+import static java.lang.String.valueOf;
+import static org.folio.circulation.support.Result.of;
+
+import java.lang.invoke.MethodHandles;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class CqlQuery {
+  private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
   private final String query;
 
   public CqlQuery(String query) {
@@ -8,6 +20,8 @@ public class CqlQuery {
   }
 
   Result<String> encode() {
-    return CqlHelper.encodeQuery(query);
+    log.info("Encoding query {}", query);
+
+    return of(() -> URLEncoder.encode(query, valueOf(StandardCharsets.UTF_8)));
   }
 }
