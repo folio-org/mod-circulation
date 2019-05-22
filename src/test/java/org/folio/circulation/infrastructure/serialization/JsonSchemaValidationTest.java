2d212b47c6f9b9b339ae7223f866b40ac4378561
Marc Johnson <marc@k-int.co.uk>
2019-05-22 15:31:50 +0100

    Extract method for loading a schema from a resource CIRC-421

--- src/test/java/org/folio/circulation/infrastructure/serialization/JsonSchemaValidationTest.java
+++ src/test/java/org/folio/circulation/infrastructure/serialization/JsonSchemaValidationTest.java
@@ -20,20 +20,15 @@ import org.junit.rules.ExpectedException;
 public class JsonSchemaValidationTest {
   @Test
   public void validationSucceedWithCompleteExample() throws IOException {
-    try (InputStream inputStream = getClass().getResourceAsStream(
-      "/check-in-by-barcode-request.json")) {
+    Schema schema = getCheckInByBarcodeSchema();
 
-      JSONObject rawSchema = new JSONObject(new JSONTokener(inputStream));
-      Schema schema = SchemaLoader.load(rawSchema);
+    final JSONObject example = new JSONObject();
 
-      final JSONObject example = new JSONObject();
+    example.put("itemBarcode", "246650492");
+    example.put("servicePointId", UUID.randomUUID().toString());
+    example.put("checkInDate", DateTime.now().toString(ISODateTimeFormat.dateTime()));
 
-      example.put("itemBarcode", "246650492");
-      example.put("servicePointId", UUID.randomUUID().toString());
-      example.put("checkInDate", DateTime.now().toString(ISODateTimeFormat.dateTime()));
-
-      schema.validate(example);
-    }
+    schema.validate(example);
   }
 
   @Rule
@@ -41,20 +36,26 @@ public class JsonSchemaValidationTest {
 
   @Test
   public void validationFailsWhenRequiredPropertyMissing() throws IOException {
-    try (InputStream inputStream = getClass().getResourceAsStream(
-      "/check-in-by-barcode-request.json")) {
+    Schema schema = getCheckInByBarcodeSchema();
+
+    final JSONObject example = new JSONObject();
+
+    example.put("itemBarcode", "246650492");
+    example.put("checkInDate", DateTime.now().toString(ISODateTimeFormat.dateTime()));
+
+    exceptionRule.expect(ValidationException.class);
+    exceptionRule.expectMessage(is("#: required key [servicePointId] not found"));
+
+    schema.validate(example);
+  }
+
+  private Schema getCheckInByBarcodeSchema() throws IOException {
+    try (InputStream inputStream = getClass()
+        .getResourceAsStream("/check-in-by-barcode-request.json")) {
 
       JSONObject rawSchema = new JSONObject(new JSONTokener(inputStream));
-      Schema schema = SchemaLoader.load(rawSchema);
 
-      final JSONObject example = new JSONObject();
-
-      example.put("itemBarcode", "246650492");
-      example.put("checkInDate", DateTime.now().toString(ISODateTimeFormat.dateTime()));
-
-      exceptionRule.expect(ValidationException.class);
-      exceptionRule.expectMessage(is("#: required key [servicePointId] not found"));
-      schema.validate(example);
+      return SchemaLoader.load(rawSchema);
     }
   }
 }
