bb6f5a0971911b48024922af8c8706c02719079f
Ryan Laddusaw <rladdusaw@library.tamu.edu>
2019-05-29 14:22:56 -0500

    Adding Marc's api test

--- src/test/java/api/loans/scenarios/CheckoutWithRequestScenarioTests.java
+++ src/test/java/api/loans/scenarios/CheckoutWithRequestScenarioTests.java
@@ -1,18 +1,29 @@
 package api.loans.scenarios;
 
+import static api.support.matchers.TextDateTimeMatcher.isEquivalentTo;
+import static org.folio.circulation.domain.policy.DueDateManagement.KEEP_THE_CURRENT_DUE_DATE;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.junit.MatcherAssert.assertThat;
 
 import java.net.MalformedURLException;
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneOffset;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 
+import org.folio.circulation.domain.policy.Period;
+import org.folio.circulation.support.ClockManager;
 import org.folio.circulation.support.http.client.IndividualResource;
 import org.folio.circulation.support.http.client.Response;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
 import org.junit.Test;
 
 import api.support.APITests;
+import api.support.builders.CheckOutByBarcodeRequestBuilder;
+import api.support.builders.LoanPolicyBuilder;
 import api.support.builders.RequestBuilder;
 import api.support.http.InventoryItemResource;
 
@@ -36,6 +47,62 @@ public class CheckoutWithRequestScenarioTests extends APITests {
       .by(charlotte));
 
     loansFixture.checkOutByBarcode(smallAngryPlanet, charlotte);
+
+    Response changedItem = itemsClient.getById(smallAngryPlanet.getId());
+
+    assertThat(changedItem.getJson().getJsonObject("status").getString("name"),
+      is("Checked out"));
+  }
+
+  @Test
+  public void checkingOutWithHoldRequestAppliesAlternatePeriod()
+    throws InterruptedException,
+    MalformedURLException,
+    TimeoutException,
+    ExecutionException {
+
+    final InventoryItemResource smallAngryPlanet = itemsFixture.basedUponSmallAngryPlanet();
+    final IndividualResource charlotte = usersFixture.charlotte();
+    final IndividualResource james = usersFixture.james();
+    final UUID pickupServicePointId = servicePointsFixture.cd1().getId();
+
+    final IndividualResource policy = loanPoliciesFixture.create(new LoanPolicyBuilder()
+      .withName("Limited loan period for items with hold requests")
+      .rolling(Period.weeks(3))
+      .withAlternateCheckoutLoanPeriod(Period.weeks(1))
+      .withClosedLibraryDueDateManagement(KEEP_THE_CURRENT_DUE_DATE.getValue()));
+
+    useLoanPolicyAsFallback(
+      policy.getId(),
+      requestPoliciesFixture.allowAllRequestPolicy().getId(),
+      noticePoliciesFixture.inactiveNotice().getId());
+
+    loansFixture.checkOutByBarcode(smallAngryPlanet, usersFixture.steve());
+
+    requestsClient.create(new RequestBuilder()
+      .hold()
+      .forItem(smallAngryPlanet)
+      .withPickupServicePointId(pickupServicePointId)
+      .by(charlotte));
+
+    requestsClient.create(new RequestBuilder()
+      .hold()
+      .forItem(smallAngryPlanet)
+      .withPickupServicePointId(pickupServicePointId)
+      .by(james));
+
+    loansFixture.checkInByBarcode(smallAngryPlanet);
+
+    final DateTime loanDate = new DateTime(2019, 5, 5, 11, 32, 12, DateTimeZone.UTC);
+
+    final IndividualResource loan = loansFixture.checkOutByBarcode(new CheckOutByBarcodeRequestBuilder()
+      .forItem(smallAngryPlanet)
+      .to(charlotte)
+      .at(pickupServicePointId)
+      .on(loanDate));
+
+    assertThat(loan.getJson().getString("dueDate"), isEquivalentTo(
+      new DateTime(2019, 5, 12, 23, 59, 59, DateTimeZone.UTC)));
 
     Response changedItem = itemsClient.getById(smallAngryPlanet.getId());
 
