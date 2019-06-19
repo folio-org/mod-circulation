3acd781d9a5152378143ea9b4f281cf8a6833f49
Dmytro Tur <dmytro_tur@epam.com>
2019-06-19 15:47:12 +0300

    Remove allowOverrideRenewalLoanByBarcodeWhenFirstRequestInQueueIsHold test

--- src/test/java/api/requests/RequestsAPILoanRenewalTests.java
+++ src/test/java/api/requests/RequestsAPILoanRenewalTests.java
@@ -11,14 +11,12 @@ import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 
-import org.folio.circulation.domain.policy.Period;
 import org.folio.circulation.support.http.client.IndividualResource;
 import org.folio.circulation.support.http.client.Response;
 import org.joda.time.DateTime;
 import org.junit.Test;
 
 import api.support.APITests;
-import api.support.builders.LoanPolicyBuilder;
 import api.support.builders.RequestBuilder;
 import api.support.http.InventoryItemResource;
 
@@ -102,49 +100,6 @@ public class RequestsAPILoanRenewalTests extends APITests {
     IndividualResource response = loansFixture.renewLoanById(smallAngryPlanet, rebecca);
 
     assertThat(response.getJson().getString("action"), is("renewed"));
-  }
-
-  @Test
-  public void allowOverrideRenewalLoanByBarcodeWhenFirstRequestInQueueIsHold() throws InterruptedException, MalformedURLException, TimeoutException, ExecutionException {
-
-    final InventoryItemResource smallAngryPlanet = itemsFixture.basedUponSmallAngryPlanet();
-    final IndividualResource rebecca = usersFixture.rebecca();
-
-    DateTime loanDueDate = new DateTime(
-      2018, APRIL, 21,
-      11, 21, 43
-    );
-
-    loansFixture.checkOutByBarcode(smallAngryPlanet, rebecca, loanDueDate);
-
-    LoanPolicyBuilder nonRenewablePolicy = new LoanPolicyBuilder()
-      .withName("Non Renewable Policy")
-      .rolling(Period.days(2))
-      .notRenewable();
-
-    UUID nonRenewablePolicyId = loanPoliciesFixture.create(nonRenewablePolicy).getId();
-
-    useLoanPolicyAsFallback(
-      nonRenewablePolicyId,
-      requestPoliciesFixture.allowAllRequestPolicy().getId(),
-      noticePoliciesFixture.activeNotice().getId()
-    );
-
-    requestsFixture.place(new RequestBuilder()
-      .hold()
-      .forItem(smallAngryPlanet)
-      .withPickupServicePointId(servicePointsFixture.cd1().getId())
-      .by(usersFixture.charlotte()));
-
-    loansFixture.attemptRenewalById(smallAngryPlanet, rebecca);
-
-    IndividualResource response = loansFixture.overrideRenewalByBarcode(
-      smallAngryPlanet,
-      rebecca,
-      "Renewal override",
-      "2018-12-21T13:30:00Z");
-
-    assertThat(response.getJson().getString("action"), is("renewedThroughOverride"));
   }
 
   @Test
