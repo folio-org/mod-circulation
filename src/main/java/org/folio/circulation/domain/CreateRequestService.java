cdf08b23cba921edb2422e3090ca3b2495594a28
William Welling <wwelling@library.tamu.edu>
2019-06-20 11:19:32 -0500

    share static methods between request services

--- src/main/java/org/folio/circulation/domain/CreateRequestService.java
+++ src/main/java/org/folio/circulation/domain/CreateRequestService.java
@@ -1,22 +1,14 @@
 package org.folio.circulation.domain;
 
-import static java.lang.String.format;
-import static org.folio.circulation.domain.representations.RequestProperties.REQUEST_TYPE;
 import static org.folio.circulation.support.Result.of;
-import static org.folio.circulation.support.Result.succeeded;
-import static org.folio.circulation.support.ValidationErrorFailure.failedValidation;
 import static org.folio.circulation.support.ValidationErrorFailure.singleValidationError;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
-import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
-import org.folio.circulation.domain.policy.RequestPolicy;
 import org.folio.circulation.domain.policy.RequestPolicyRepository;
 import org.folio.circulation.resources.RequestNoticeSender;
-import org.folio.circulation.support.ResponseWritableResult;
 import org.folio.circulation.support.Result;
 import org.folio.circulation.support.http.server.ValidationError;
 
@@ -48,111 +40,19 @@ public class CreateRequestService {
     RequestAndRelatedRecords requestAndRelatedRecords) {
 
     return of(() -> requestAndRelatedRecords)
-      .next(CreateRequestService::refuseWhenItemDoesNotExist)
-      .next(CreateRequestService::refuseWhenInvalidUserAndPatronGroup)
-      .next(CreateRequestService::refuseWhenItemIsNotValid)
-      .next(CreateRequestService::refuseWhenUserHasAlreadyRequestedItem)
+      .next(RequestServiceUtility::refuseWhenItemDoesNotExist)
+      .next(RequestServiceUtility::refuseWhenInvalidUserAndPatronGroup)
+      .next(RequestServiceUtility::refuseWhenItemIsNotValid)
+      .next(RequestServiceUtility::refuseWhenUserHasAlreadyRequestedItem)
       .after(this::refuseWhenUserHasAlreadyBeenLoanedItem)
       .thenComposeAsync(r -> r.after(requestPolicyRepository::lookupRequestPolicy))
-      .thenApply(r -> r.next(CreateRequestService::refuseWhenRequestCannotBeFulfilled))
-      .thenApply(r -> r.map(CreateRequestService::setRequestQueuePosition))
+      .thenApply(r -> r.next(RequestServiceUtility::refuseWhenRequestCannotBeFulfilled))
+      .thenApply(r -> r.map(RequestServiceUtility::setRequestQueuePosition))
       .thenComposeAsync(r -> r.after(updateItem::onRequestCreationOrMove))
       .thenComposeAsync(r -> r.after(updateLoanActionHistory::onRequestCreationOrMove))
       .thenComposeAsync(r -> r.after(updateLoan::onRequestCreationOrMove))
       .thenComposeAsync(r -> r.after(requestRepository::create))
       .thenApply(r -> r.next(requestNoticeSender::sendNoticeOnRequestCreated));
-  }
-
-  private static RequestAndRelatedRecords setRequestQueuePosition(RequestAndRelatedRecords requestAndRelatedRecords) {
-    // TODO: Extract to method to add to queue
-    requestAndRelatedRecords.withRequest(requestAndRelatedRecords.getRequest()
-      .changePosition(requestAndRelatedRecords.getRequestQueue().nextAvailablePosition()));
-
-    return requestAndRelatedRecords;
-  }
-
-  private static Result<RequestAndRelatedRecords> refuseWhenItemDoesNotExist(
-    RequestAndRelatedRecords requestAndRelatedRecords) {
-
-    if (requestAndRelatedRecords.getRequest().getItem().isNotFound()) {
-      return failedValidation("Item does not exist", "itemId", requestAndRelatedRecords.getRequest().getItemId());
-    } else {
-      return succeeded(requestAndRelatedRecords);
-    }
-  }
-
-  private static Result<RequestAndRelatedRecords> refuseWhenRequestCannotBeFulfilled(
-    RequestAndRelatedRecords requestAndRelatedRecords) {
-
-    RequestPolicy requestPolicy = requestAndRelatedRecords.getRequestPolicy();
-    RequestType requestType = requestAndRelatedRecords.getRequest().getRequestType();
-
-    if (!requestPolicy.allowsType(requestType)) {
-      return failureDisallowedForRequestType(requestType);
-    } else {
-      return succeeded(requestAndRelatedRecords);
-    }
-  }
-
-  private static Result<RequestAndRelatedRecords> refuseWhenItemIsNotValid(
-    RequestAndRelatedRecords requestAndRelatedRecords) {
-
-    Request request = requestAndRelatedRecords.getRequest();
-
-    if (!request.allowedForItem()) {
-      return failureDisallowedForRequestType(request.getRequestType());
-    } else {
-      return succeeded(requestAndRelatedRecords);
-    }
-  }
-
-  private static ResponseWritableResult<RequestAndRelatedRecords> failureDisallowedForRequestType(
-    RequestType requestType) {
-
-    final String requestTypeName = requestType.getValue();
-
-    return failedValidation(format("%s requests are not allowed for this patron and item combination", requestTypeName),
-      REQUEST_TYPE, requestTypeName);
-  }
-
-  private static Result<RequestAndRelatedRecords> refuseWhenInvalidUserAndPatronGroup(
-    RequestAndRelatedRecords requestAndRelatedRecords) {
-
-    Request request = requestAndRelatedRecords.getRequest();
-    User requester = request.getRequester();
-
-    // TODO: Investigate whether the parameter used here is correct
-    // Should it be the userId for both of these failures?
-    if (requester == null) {
-      return failedValidation("A valid user and patron group are required. User is null", "userId", null);
-
-    } else if (requester.getPatronGroupId() == null) {
-      return failedValidation("A valid patron group is required. PatronGroup ID is null", "PatronGroupId", null);
-    } else {
-      return succeeded(requestAndRelatedRecords);
-    }
-  }
-
-  public static Result<RequestAndRelatedRecords> refuseWhenUserHasAlreadyRequestedItem(
-    RequestAndRelatedRecords request) {
-
-    Optional<Request> requestOptional = request.getRequestQueue().getRequests().stream()
-      .filter(it -> isTheSameRequester(request, it) && it.isOpen()).findFirst();
-
-    if (requestOptional.isPresent()) {
-      Map<String, String> parameters = new HashMap<>();
-      parameters.put("requesterId", request.getRequest().getUserId());
-      parameters.put("itemId", request.getRequest().getItemId());
-      parameters.put("requestId", requestOptional.get().getId());
-      String message = "This requester already has an open request for this item";
-      return failedValidation(new ValidationError(message, parameters));
-    } else {
-      return of(() -> request);
-    }
-  }
-
-  private static boolean isTheSameRequester(RequestAndRelatedRecords it, Request that) {
-    return Objects.equals(it.getUserId(), that.getUserId());
   }
 
   private CompletableFuture<Result<RequestAndRelatedRecords>> refuseWhenUserHasAlreadyBeenLoanedItem(
--- src/main/java/org/folio/circulation/domain/MoveRequestService.java
+++ src/main/java/org/folio/circulation/domain/MoveRequestService.java
@@ -1,22 +1,14 @@
 package org.folio.circulation.domain;
 
-import static java.lang.String.format;
-import static org.folio.circulation.domain.representations.RequestProperties.REQUEST_TYPE;
 import static org.folio.circulation.support.Result.of;
-import static org.folio.circulation.support.Result.succeeded;
-import static org.folio.circulation.support.ValidationErrorFailure.failedValidation;
 import static org.folio.circulation.support.ValidationErrorFailure.singleValidationError;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
-import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
-import org.folio.circulation.domain.policy.RequestPolicy;
 import org.folio.circulation.domain.policy.RequestPolicyRepository;
 import org.folio.circulation.support.ItemRepository;
-import org.folio.circulation.support.ResponseWritableResult;
 import org.folio.circulation.support.Result;
 import org.folio.circulation.support.http.server.ValidationError;
 
@@ -49,7 +41,7 @@ public class MoveRequestService {
   public CompletableFuture<Result<RequestAndRelatedRecords>> moveRequest(
     RequestAndRelatedRecords requestAndRelatedRecords) {
     return of(() -> requestAndRelatedRecords)
-      .next(MoveRequestService::refuseWhenItemDoesNotExist)
+      .next(RequestServiceUtility::refuseWhenItemDoesNotExist)
       .after(updateRequestQueue::onMoveFrom)
       .thenComposeAsync(r -> r.after(this::lookupDestinationItem))
       .thenComposeAsync(r -> r.after(this::lookupDestinationItemRequestQueue))
@@ -83,110 +75,18 @@ public class MoveRequestService {
   private CompletableFuture<Result<RequestAndRelatedRecords>> updateRequest(
     RequestAndRelatedRecords requestAndRelatedRecords) {
     return of(() -> requestAndRelatedRecords)
-      .next(MoveRequestService::refuseWhenItemDoesNotExist)
-      .next(MoveRequestService::refuseWhenInvalidUserAndPatronGroup)
-      .next(MoveRequestService::refuseWhenItemIsNotValid)
-      .next(MoveRequestService::refuseWhenUserHasAlreadyRequestedItem)
+      .next(RequestServiceUtility::refuseWhenItemDoesNotExist)
+      .next(RequestServiceUtility::refuseWhenInvalidUserAndPatronGroup)
+      .next(RequestServiceUtility::refuseWhenItemIsNotValid)
+      .next(RequestServiceUtility::refuseWhenUserHasAlreadyRequestedItem)
       .after(this::refuseWhenUserHasAlreadyBeenLoanedItem)
       .thenComposeAsync(r -> r.after(requestPolicyRepository::lookupRequestPolicy))
-      .thenApply(r -> r.next(MoveRequestService::refuseWhenRequestCannotBeFulfilled))
-      .thenApply(r -> r.map(MoveRequestService::setRequestQueuePosition))
+      .thenApply(r -> r.next(RequestServiceUtility::refuseWhenRequestCannotBeFulfilled))
+      .thenApply(r -> r.map(RequestServiceUtility::setRequestQueuePosition))
       .thenComposeAsync(r -> r.after(updateItem::onRequestCreationOrMove))
       .thenComposeAsync(r -> r.after(updateLoanActionHistory::onRequestCreationOrMove))
       .thenComposeAsync(r -> r.after(updateLoan::onRequestCreationOrMove))
       .thenComposeAsync(r -> r.after(requestRepository::update));
-  }
-
-  private static RequestAndRelatedRecords setRequestQueuePosition(RequestAndRelatedRecords requestAndRelatedRecords) {
-    // TODO: Extract to method to add to queue
-    requestAndRelatedRecords.withRequest(requestAndRelatedRecords.getRequest()
-      .changePosition(requestAndRelatedRecords.getRequestQueue().nextAvailablePosition()));
-
-    return requestAndRelatedRecords;
-  }
-
-  private static Result<RequestAndRelatedRecords> refuseWhenItemDoesNotExist(
-    RequestAndRelatedRecords requestAndRelatedRecords) {
-
-    if (requestAndRelatedRecords.getRequest().getItem().isNotFound()) {
-      return failedValidation("Item does not exist", "itemId", requestAndRelatedRecords.getRequest().getItemId());
-    } else {
-      return succeeded(requestAndRelatedRecords);
-    }
-  }
-
-  private static Result<RequestAndRelatedRecords> refuseWhenRequestCannotBeFulfilled(
-    RequestAndRelatedRecords requestAndRelatedRecords) {
-
-    RequestPolicy requestPolicy = requestAndRelatedRecords.getRequestPolicy();
-    RequestType requestType = requestAndRelatedRecords.getRequest().getRequestType();
-
-    if (!requestPolicy.allowsType(requestType)) {
-      return failureDisallowedForRequestType(requestType);
-    } else {
-      return succeeded(requestAndRelatedRecords);
-    }
-  }
-
-  private static Result<RequestAndRelatedRecords> refuseWhenItemIsNotValid(
-    RequestAndRelatedRecords requestAndRelatedRecords) {
-
-    Request request = requestAndRelatedRecords.getRequest();
-
-    if (!request.allowedForItem()) {
-      return failureDisallowedForRequestType(request.getRequestType());
-    } else {
-      return succeeded(requestAndRelatedRecords);
-    }
-  }
-
-  private static ResponseWritableResult<RequestAndRelatedRecords> failureDisallowedForRequestType(
-    RequestType requestType) {
-
-    final String requestTypeName = requestType.getValue();
-
-    return failedValidation(format("%s requests are not allowed for this patron and item combination", requestTypeName),
-      REQUEST_TYPE, requestTypeName);
-  }
-
-  private static Result<RequestAndRelatedRecords> refuseWhenInvalidUserAndPatronGroup(
-    RequestAndRelatedRecords requestAndRelatedRecords) {
-
-    Request request = requestAndRelatedRecords.getRequest();
-    User requester = request.getRequester();
-
-    // TODO: Investigate whether the parameter used here is correct
-    // Should it be the userId for both of these failures?
-    if (requester == null) {
-      return failedValidation("A valid user and patron group are required. User is null", "userId", null);
-
-    } else if (requester.getPatronGroupId() == null) {
-      return failedValidation("A valid patron group is required. PatronGroup ID is null", "PatronGroupId", null);
-    } else {
-      return succeeded(requestAndRelatedRecords);
-    }
-  }
-
-  private static Result<RequestAndRelatedRecords> refuseWhenUserHasAlreadyRequestedItem(
-    RequestAndRelatedRecords request) {
-
-    Optional<Request> requestOptional = request.getRequestQueue().getRequests().stream()
-      .filter(it -> isTheSameRequester(request, it) && it.isOpen()).findFirst();
-
-    if (requestOptional.isPresent()) {
-      Map<String, String> parameters = new HashMap<>();
-      parameters.put("requesterId", request.getRequest().getUserId());
-      parameters.put("itemId", request.getRequest().getItemId());
-      parameters.put("requestId", requestOptional.get().getId());
-      String message = "This requester already has an open request for this item";
-      return failedValidation(new ValidationError(message, parameters));
-    } else {
-      return of(() -> request);
-    }
-  }
-
-  private static boolean isTheSameRequester(RequestAndRelatedRecords it, Request that) {
-    return Objects.equals(it.getUserId(), that.getUserId());
   }
 
   private CompletableFuture<Result<RequestAndRelatedRecords>> refuseWhenUserHasAlreadyBeenLoanedItem(
