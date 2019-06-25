f723ab87ecd508501b9fc15cbd33e1a116e4439b
Martin Tran <mtran@ebsco.com>
2019-06-25 16:29:17 -0400

    renamed and addressed PR comments

--- src/main/java/org/folio/circulation/domain/InstanceRequestItemsComparer.java
+++ src/main/java/org/folio/circulation/domain/InstanceRequestItemsComparer.java
@@ -10,7 +10,7 @@ import java.util.stream.Collectors;
 
 public class InstanceRequestItemsComparer {
 
-  private InstanceRequestItemsComparer(){}
+  private InstanceRequestItemsComparer() {}
 
   public static Map<Item, Integer> sortRequestQueues(Map<Item, Integer> itemsQueueLengthUnsortedMap,
                                                      Map<Item, DateTime> itemDueDateMap,
@@ -27,32 +27,32 @@ public class InstanceRequestItemsComparer {
                                                                           UUID destinationServicePointId) {
     // Sort the map
     return (q1Size, q2Size) -> {
-      int result = checkQueueSize(q1Size.getValue(), q2Size.getValue());
+      int result = compareQueueSize(q1Size.getValue(), q2Size.getValue());
 
       if (result == 0 && itemDueDateMap != null) {
         Item q1Item = q1Size.getKey();
         Item q2Item = q2Size.getKey();
 
-        result = checkDueDate(q1Item, q2Item, itemDueDateMap);
+        result = compareDueDate(q1Item, q2Item, itemDueDateMap);
         if (result == 0) {
-          result = checkItemServicePoint(q1Item, q2Item, destinationServicePointId);
+          result =  compareItemServicePoint(q1Item, q2Item, destinationServicePointId);
         }
       }
       return result;
     };
   }
 
-  private static int checkQueueSize(int queueSize1, int queueSize2){
+  private static int compareQueueSize(int queueSize1, int queueSize2){
     return queueSize1 - queueSize2;
   }
 
-  private static int checkDueDate(Item item1, Item item2, Map<Item, DateTime> itemDueDateMap) {
+  private static int compareDueDate(Item item1, Item item2, Map<Item, DateTime> itemDueDateMap) {
     DateTime q1ItemDueDate = itemDueDateMap.get(item1);
     DateTime q2ItemDueDate = itemDueDateMap.get(item2);
 
     if (q1ItemDueDate == null && q2ItemDueDate == null) {
       return 0;
-    } else if (q1ItemDueDate != null && q2ItemDueDate == null) {
+    } else if (q2ItemDueDate == null) {
       return -1;
     } else if (q1ItemDueDate == null) {
       return 1;
@@ -61,7 +61,7 @@ public class InstanceRequestItemsComparer {
     }
   }
 
-  private static int checkItemServicePoint(Item item1, Item item2,
+  private static int compareItemServicePoint(Item item1, Item item2,
                                            UUID destinationServicePointId) {
     if (destinationServicePointId == null)
       return 0;
