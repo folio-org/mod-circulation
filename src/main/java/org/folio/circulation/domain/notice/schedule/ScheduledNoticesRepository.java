5081e6789c3babe25eeb3cadffc42c4068f9bc31
Marc Johnson <marc@k-int.co.uk>
2019-06-26 19:46:44 +0100

    Use response interpreter when deleting scheduled notices CIRC-362

--- src/main/java/org/folio/circulation/domain/notice/schedule/ScheduledNoticesRepository.java
+++ src/main/java/org/folio/circulation/domain/notice/schedule/ScheduledNoticesRepository.java
@@ -1,22 +1,20 @@
 package org.folio.circulation.domain.notice.schedule;
 
+import static java.util.function.Function.identity;
 import static org.folio.circulation.domain.notice.schedule.JsonScheduledNoticeMapper.mapToJson;
 import static org.folio.circulation.support.CqlSortBy.ascending;
-import static org.folio.circulation.support.Result.failed;
-import static org.folio.circulation.support.Result.succeeded;
 import static org.folio.circulation.support.http.CommonResponseInterpreters.noContentRecordInterpreter;
 import static org.folio.circulation.support.http.ResponseMapping.flatMapUsingJson;
 import static org.folio.circulation.support.http.ResponseMapping.forwardOnFailure;
 
 import java.util.concurrent.CompletableFuture;
-import java.util.function.Function;
 
 import org.folio.circulation.domain.MultipleRecords;
 import org.folio.circulation.support.Clients;
 import org.folio.circulation.support.CollectionResourceClient;
 import org.folio.circulation.support.CqlQuery;
-import org.folio.circulation.support.ForwardOnFailure;
 import org.folio.circulation.support.Result;
+import org.folio.circulation.support.http.client.Response;
 import org.folio.circulation.support.http.client.ResponseInterpreter;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
@@ -50,6 +48,7 @@ public class ScheduledNoticesRepository {
 
   public CompletableFuture<Result<MultipleRecords<ScheduledNotice>>> findNoticesToSend(
     DateTime systemTime, int pageLimit) {
+
     return CqlQuery.lessThan("nextRunTime", systemTime.withZone(DateTimeZone.UTC))
       .map(cqlQuery -> cqlQuery.sortBy(ascending("nextRunTime")))
       .after(query -> findBy(query, pageLimit));
@@ -57,10 +56,12 @@ public class ScheduledNoticesRepository {
 
   private CompletableFuture<Result<MultipleRecords<ScheduledNotice>>> findBy(
     CqlQuery cqlQuery, int pageLimit) {
+
     return scheduledNoticesStorageClient.getMany(cqlQuery, pageLimit)
       .thenApply(r -> r.next(response ->
-        MultipleRecords.from(response, Function.identity(), "scheduledNotices")))
-      .thenApply(r -> r.next(records -> records.flatMapRecords(JsonScheduledNoticeMapper::mapFromJson)));
+        MultipleRecords.from(response, identity(), "scheduledNotices")))
+      .thenApply(r -> r.next(records -> records.flatMapRecords(
+        JsonScheduledNoticeMapper::mapFromJson)));
   }
 
   public CompletableFuture<Result<ScheduledNotice>> update(ScheduledNotice scheduledNotice) {
@@ -74,18 +75,16 @@ public class ScheduledNoticesRepository {
         .otherwise(forwardOnFailure())::apply);
   }
 
-  CompletableFuture<Result<Void>> deleteByLoanId(String loanId) {
+  CompletableFuture<Result<Response>> deleteByLoanId(String loanId) {
     return CqlQuery.exactMatch("loanId", loanId).after(this::deleteMany);
   }
 
-  private CompletableFuture<Result<Void>> deleteMany(CqlQuery cqlQuery) {
-    return scheduledNoticesStorageClient.deleteMany(cqlQuery).thenApply(
-      result -> result.next(response -> {
-        if (response.getStatusCode() == 204) {
-          return succeeded(null);
-        } else {
-          return failed(new ForwardOnFailure(response));
-        }
-      }));
+  private CompletableFuture<Result<Response>> deleteMany(CqlQuery cqlQuery) {
+    final ResponseInterpreter<Response> interpreter = new ResponseInterpreter<Response>()
+      .flatMapOn(204, Result::succeeded)
+      .otherwise(forwardOnFailure());
+
+    return scheduledNoticesStorageClient.deleteMany(cqlQuery)
+      .thenApply(responseResult -> responseResult.next(interpreter::apply));
   }
 }
