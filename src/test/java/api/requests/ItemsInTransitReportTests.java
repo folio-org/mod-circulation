c962a3153442280deb0ad96efc367890b28c351a
Svitlana_Zmiivska <Svitlana_Zmiivska@epam.com>
2019-11-01 18:18:27 +0200

    CIRC-518 change items files in ramls folder, refactor item in transit test

--- src/test/java/api/requests/ItemsInTransitReportTests.java
+++ src/test/java/api/requests/ItemsInTransitReportTests.java
@@ -4,6 +4,7 @@ import api.support.APITests;
 import api.support.builders.RequestBuilder;
 import api.support.http.InventoryItemResource;
 import api.support.http.ResourceClient;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import org.folio.circulation.domain.ItemStatus;
 import org.folio.circulation.support.http.client.IndividualResource;
@@ -11,6 +12,7 @@ import org.junit.Test;
 
 import java.net.MalformedURLException;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
@@ -21,8 +23,14 @@ import static org.junit.Assert.assertTrue;
 
 public class ItemsInTransitReportTests extends APITests {
 
+  private static final String LOCATION_NAME = "name";
+  private static final String LOCATION_CODE = "code";
+  private static final String LIBRARY = "library";
   private static final String STATUS_KEY = "status";
   private static final String BARCODE_KEY = "barcode";
+  private static final String TITLE = "title";
+  private static final String CONTRIBUTORS = "contributors";
+  private static final String DESTINATION_SERVICE_POINT = "inTransitDestinationServicePointId";
 
   @Test
   public void reportIsEmptyWhenThereAreNoItemsInTransit()
@@ -31,9 +39,9 @@ public class ItemsInTransitReportTests extends APITests {
     TimeoutException,
     ExecutionException {
 
-    List<JsonObject> rs = ResourceClient.forInventoryReport(client).getAll();
+    List<JsonObject> items = ResourceClient.forInventoryReport(client).getAll();
 
-    assertTrue(rs.isEmpty());
+    assertTrue(items.isEmpty());
   }
 
   @Test
@@ -44,6 +52,9 @@ public class ItemsInTransitReportTests extends APITests {
     ExecutionException {
 
     final InventoryItemResource smallAngryPlanet = itemsFixture.basedUponSmallAngryPlanet();
+    final JsonObject smallAngryPlanetInstance = smallAngryPlanet.getInstance().getJson();
+    final String contributors = String.valueOf(((JsonArray) smallAngryPlanetInstance
+      .getMap().get(CONTRIBUTORS)).getJsonObject(0).getMap().get("name"));
 
     // init for SP2
     final IndividualResource steve = usersFixture.steve();
@@ -64,12 +75,21 @@ public class ItemsInTransitReportTests extends APITests {
     // #3 check-in item in SP1
     loansFixture.checkInByBarcode(smallAngryPlanet);
 
-    // #5 get items report with in transit status in SP1
-    List<JsonObject> rs = ResourceClient.forInventoryReport(client).getAll();
-    JsonObject itemJson = rs.get(0);
+    // #4 get items report with in transit status
+    List<JsonObject> items = ResourceClient.forInventoryReport(client).getAll();
 
+    assertThat(items.size(), is(1));
+    JsonObject itemJson = items.get(0);
     assertThat(itemJson.getString(BARCODE_KEY), is(smallAngryPlanet.getBarcode()));
     assertThat(itemJson.getJsonObject(STATUS_KEY).getMap().get("name"),
       is(ItemStatus.IN_TRANSIT.getValue()));
+    assertThat(itemJson.getString(DESTINATION_SERVICE_POINT), is(String.valueOf(secondServicePointId)));
+    assertThat(itemJson.getString(TITLE), is(smallAngryPlanetInstance.getString(TITLE)));
+    assertThat(itemJson.getJsonArray(CONTRIBUTORS)
+      .getJsonObject(0).getMap().get("name"), is(contributors));
+    Map<String, String> actualLocation = (Map<String, String>) itemJson.getMap().get("location");
+    assertThat(actualLocation.get(LOCATION_NAME), is("3rd Floor"));
+    assertThat(actualLocation.get(LOCATION_CODE), is("NU/JC/DL/3F"));
+    assertThat(actualLocation.get(LIBRARY), is("Djanogly Learning Resource Centre"));
   }
 }
