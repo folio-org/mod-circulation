cc08858a04dd27970f09a04cc2496a150c2d568f
VRohach <Volodymyr_Rohach@epam.com>
2019-11-11 15:33:18 +0200

    CIRC-523: Maintain item status date even if item record is edited: Extra test removed.

--- src/test/java/api/item/ItemStatusApiTests.java
+++ src/test/java/api/item/ItemStatusApiTests.java
@@ -1,8 +1,6 @@
 package api.item;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.notNullValue;
 
@@ -38,36 +36,5 @@ public class ItemStatusApiTests extends APITests {
 
     assertThat(checkedOutItem.getJsonObject(ITEM_STATUS)
         .getString(ITEM_STATUS_DATE), is(notNullValue()));
-  }
-
-  @Test
-  public void itemStatusDateShouldNotChangedIfItemChanged() throws InterruptedException,
-    MalformedURLException,
-    TimeoutException,
-    ExecutionException {
-
-    IndividualResource item = itemsFixture.basedUponSmallAngryPlanet();
-    IndividualResource user = usersFixture.jessica();
-
-    loansFixture.checkOutByBarcode(item, user, new DateTime(DateTimeZone.UTC));
-
-    JsonObject checkedOutItem = itemsClient.get(item.getId()).getJson();
-
-    assertThat(checkedOutItem.getJsonObject(ITEM_STATUS)
-      .getString(ITEM_STATUS_DATE), is(notNullValue()));
-
-    String itemStatusDate = checkedOutItem.getJsonObject(ITEM_STATUS)
-      .getString(ITEM_STATUS_DATE);
-
-    JsonObject itemCopy = checkedOutItem.copy();
-    itemCopy.remove(ITEM_BARCODE);
-    itemCopy.put(ITEM_BARCODE, "1234567890");
-
-    itemsClient.replace(item.getId(), itemCopy);
-
-    JsonObject updatedItem = itemsClient.get(item.getId()).getJson();
-
-    assertEquals(updatedItem.getJsonObject(ITEM_STATUS).getString(ITEM_STATUS_DATE), itemStatusDate);
-    assertNotEquals(updatedItem.getString(ITEM_BARCODE), checkedOutItem.getString(ITEM_BARCODE));
   }
 }
