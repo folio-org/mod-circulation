1b622a1670c5b3a924d8e1e0fa060c02b99e1ff2
Dmytro Popov <dmytro_popov@epam.com>
2019-11-19 12:45:55 +0200

    CIRC-364: Add tests for anonymize closed loans setting of "Immediately"

---
+++ src/test/java/api/loans/anonymization/AnonymizeLoansImmediatelyAPITests.java
@@ -0,0 +1,223 @@
+package api.loans.anonymization;
+
+import static api.support.matchers.LoanMatchers.isAnonymized;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.junit.MatcherAssert.assertThat;
+
+import java.net.MalformedURLException;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+
+import api.support.builders.CheckOutByBarcodeRequestBuilder;
+import api.support.builders.LoanHistoryConfigurationBuilder;
+import org.folio.circulation.support.http.client.IndividualResource;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeUtils;
+import org.junit.Test;
+
+public class AnonymizeLoansImmediatelyAPITests extends LoanAnonymizationTests {
+
+  /**
+   * Scenario 1
+   *
+   *     Given:
+   *         An Anonymize closed loans setting of "Immediately after loan closes"
+   *         An Anonymize closed loans with associated fees/fines setting of "Immediately after fee/fine closes"
+   *         An open loan with an associated fee/fine
+   *     When the item in the loan is checked in
+   *     Then do not anonymize the loan
+   */
+  @Test
+  public void testOpenLoansWithOpenFeesAndFinesIsNotAnonymized()
+    throws InterruptedException, ExecutionException, TimeoutException, MalformedURLException {
+
+    LoanHistoryConfigurationBuilder loanHistoryConfig = new LoanHistoryConfigurationBuilder()
+      .loanCloseAnonymizeImmediately()
+      .feeFineCloseAnonymizeImmediately();
+    createConfiguration(loanHistoryConfig);
+
+    IndividualResource loanResource = loansFixture.checkOutByBarcode(new CheckOutByBarcodeRequestBuilder().forItem(item1)
+      .to(user)
+      .at(servicePoint.getId()));
+    UUID loanID = loanResource.getId();
+
+    createClosedAccountWithFeeFines(loanResource, DateTime.now());
+
+    anonymizeLoansInTenant();
+
+    assertThat(loansStorageClient.getById(loanID)
+      .getJson(), not(isAnonymized()));
+  }
+
+  /**
+   * Scenario 2
+   *
+   *     Given:
+   *         An Anonymize closed loans setting of "Immediately after loan closes"
+   *         An Anonymize closed loans with associated fees/fines setting of "Immediately after fee/fine closes"
+   *         A closed loan with an associated fee/fine
+   *     When all fees/fines associated with the loan are closed
+   *     Then anonymize the loan
+   */
+  @Test
+  public void testClosedLoansWithClosedFeesAndIsAnonymized()
+    throws InterruptedException, ExecutionException, TimeoutException, MalformedURLException {
+
+    LoanHistoryConfigurationBuilder loanHistoryConfig = new LoanHistoryConfigurationBuilder()
+      .loanCloseAnonymizeImmediately()
+      .feeFineCloseAnonymizeImmediately();
+    createConfiguration(loanHistoryConfig);
+
+    IndividualResource loanResource = loansFixture.checkOutByBarcode(new CheckOutByBarcodeRequestBuilder().forItem(item1)
+      .to(user)
+      .at(servicePoint.getId()));
+    UUID loanID = loanResource.getId();
+
+    createClosedAccountWithFeeFines(loanResource, DateTime.now());
+
+    loansFixture.checkInByBarcode(item1);
+
+    anonymizeLoansInTenant();
+
+    assertThat(loansStorageClient.getById(loanID)
+      .getJson(), isAnonymized());
+  }
+
+  /**
+   * Scenario 3
+   *
+   *     Given:
+   *         An Anonymize closed loans setting of "Immediately after loan closes"
+   *         An Anonymize closed loans with associated fees/fines setting of "Immediately after fee/fine closes"
+   *         A closed loan with an associated fee/fine
+   *     When all fees/fines associated with the loan are closed
+   *     Then anonymize the loan
+   */
+  @Test
+  public void testClosedLoansWithOpenFeesAndFinesIsNotAnonymized()
+    throws InterruptedException, ExecutionException, TimeoutException, MalformedURLException {
+
+    LoanHistoryConfigurationBuilder loanHistoryConfig = new LoanHistoryConfigurationBuilder()
+      .loanCloseAnonymizeImmediately()
+      .feeFineCloseAnonymizeNever();
+    createConfiguration(loanHistoryConfig);
+
+    IndividualResource loanResource = loansFixture.checkOutByBarcode(new CheckOutByBarcodeRequestBuilder().forItem(item1)
+      .to(user)
+      .at(servicePoint.getId()));
+    UUID loanID = loanResource.getId();
+
+    createOpenAccountWithFeeFines(loanResource);
+
+    loansFixture.checkInByBarcode(item1);
+
+    anonymizeLoansInTenant();
+
+    assertThat(loansStorageClient.getById(loanID).getJson(),
+      not(isAnonymized()));
+  }
+
+  /**
+   * Scenario 4
+   *
+   *     Given:
+   *         An Anonymize closed loans setting of "Immediately after loan closes"
+   *         An Anonymize closed loans with associated fees/fines setting of "Never"
+   *         An open loan with an associated fee/fine
+   *     When all fees/fines associated with the loan are closed
+   *     Then do not anonymize the loan
+   */
+  @Test
+  public void testClosedLoansWithClosedFeesAndFinesIsNotAnonymized()
+    throws InterruptedException, ExecutionException, TimeoutException, MalformedURLException {
+
+    LoanHistoryConfigurationBuilder loanHistoryConfig = new LoanHistoryConfigurationBuilder()
+      .loanCloseAnonymizeImmediately()
+      .feeFineCloseAnonymizeNever();
+    createConfiguration(loanHistoryConfig);
+
+    IndividualResource loanResource = loansFixture.checkOutByBarcode(new CheckOutByBarcodeRequestBuilder().forItem(item1)
+      .to(user)
+      .at(servicePoint.getId()));
+    UUID loanID = loanResource.getId();
+
+    createClosedAccountWithFeeFines(loanResource, DateTime.now());
+
+    loansFixture.checkInByBarcode(item1);
+
+    anonymizeLoansInTenant();
+
+    assertThat(loansStorageClient.getById(loanID).getJson(),
+      not(isAnonymized()));
+  }
+
+  /**
+   * Scenario 1.1
+   *
+   *     Given:
+   *         An Anonymize closed loans setting of "Immediately"
+   *         An Anonymize closed loans with associated fees/fines setting of "X interval after fee/fine closes"
+   *         An open loan with an associated fee/fine
+   *     When the item in the loan is checked in
+   *     Then do not anonymize the loan
+   */
+  @Test
+  public void testClosedLoansWithOpenFeesAndFinesIsNotAnonymizedWhenIntervalNotPassed()
+    throws InterruptedException, ExecutionException, TimeoutException, MalformedURLException {
+
+    LoanHistoryConfigurationBuilder loanHistoryConfig = new LoanHistoryConfigurationBuilder()
+      .loanCloseAnonymizeImmediately()
+      .feeFineCloseAnonymizeAfterXInterval(1, "minute");
+    createConfiguration(loanHistoryConfig);
+
+    IndividualResource loanResource = loansFixture.checkOutByBarcode(new CheckOutByBarcodeRequestBuilder().forItem(item1)
+      .to(user)
+      .at(servicePoint.getId()));
+    UUID loanID = loanResource.getId();
+
+    createClosedAccountWithFeeFines(loanResource, DateTime.now());
+
+    loansFixture.checkInByBarcode(item1);
+
+    anonymizeLoansInTenant();
+
+    assertThat(loansStorageClient.getById(loanID).getJson(),
+      not(isAnonymized()));
+  }
+
+  /**
+   * Scenario 1.2
+   *
+   *     Given:
+   *         An Anonymize closed loans setting of "Immediately"
+   *         An Anonymize closed loans with associated fees/fines setting of "X interval after fee/fine closes"
+   *         An open loan with an associated fee/fine
+   *     When the item in the loan is checked in
+   *     Then do not anonymize the loan
+   */
+  @Test
+  public void testClosedLoansWithOpenFeesAndFinesIsAnonymizedWhenIntervalPassed()
+    throws InterruptedException, ExecutionException, TimeoutException, MalformedURLException {
+
+    LoanHistoryConfigurationBuilder loanHistoryConfig = new LoanHistoryConfigurationBuilder()
+      .loanCloseAnonymizeImmediately()
+      .feeFineCloseAnonymizeAfterXInterval(1, "minute");
+    createConfiguration(loanHistoryConfig);
+
+    IndividualResource loanResource = loansFixture.checkOutByBarcode(new CheckOutByBarcodeRequestBuilder().forItem(item1)
+      .to(user)
+      .at(servicePoint.getId()));
+    UUID loanID = loanResource.getId();
+
+    createClosedAccountWithFeeFines(loanResource, DateTime.now());
+
+    loansFixture.checkInByBarcode(item1);
+
+    DateTimeUtils.setCurrentMillisOffset(ONE_MINUTE_AND_ONE);
+    anonymizeLoansInTenant();
+
+    assertThat(loansStorageClient.getById(loanID).getJson(),
+      isAnonymized());
+  }
+}
