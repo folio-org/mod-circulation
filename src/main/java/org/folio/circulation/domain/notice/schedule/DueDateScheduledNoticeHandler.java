70f987134fc402a348abc0dd706c329954513e68
Oleksandr Vidinieiev <Oleksandr_Vidinieiev@epam.com>
2019-12-04 22:02:51 +0200

    Delete notices for missing or incomplete loans
    (cherry picked from commit c2cc134b63e8a0baaa5ec46c4b5199d2f147c6e0)

--- src/main/java/org/folio/circulation/domain/notice/schedule/DueDateScheduledNoticeHandler.java
+++ src/main/java/org/folio/circulation/domain/notice/schedule/DueDateScheduledNoticeHandler.java
@@ -1,11 +1,14 @@
 package org.folio.circulation.domain.notice.schedule;
 
 import static java.util.concurrent.CompletableFuture.completedFuture;
+import static org.folio.circulation.support.Result.failed;
 import static org.folio.circulation.support.Result.succeeded;
 
+import java.lang.invoke.MethodHandles;
 import java.util.Collection;
 import java.util.concurrent.CompletableFuture;
 
+import org.apache.commons.lang3.StringUtils;
 import org.folio.circulation.domain.ConfigurationRepository;
 import org.folio.circulation.domain.Loan;
 import org.folio.circulation.domain.LoanAndRelatedRecords;
@@ -15,12 +18,23 @@ import org.folio.circulation.domain.notice.NoticeTiming;
 import org.folio.circulation.domain.notice.PatronNoticeService;
 import org.folio.circulation.domain.policy.LoanPolicyRepository;
 import org.folio.circulation.support.Clients;
+import org.folio.circulation.support.HttpFailure;
+import org.folio.circulation.support.RecordNotFoundFailure;
 import org.folio.circulation.support.Result;
 import org.joda.time.DateTime;
 
 import io.vertx.core.json.JsonObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DueDateScheduledNoticeHandler {
+  private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+  private static final String USER_REC_TYPE = "user";
+  private static final String ITEM_REC_TYPE = "item";
+  private static final String LOAN_REC_TYPE = "loan";
+  private static final String[] REQUIRED_REC_TYPES = {USER_REC_TYPE, ITEM_REC_TYPE, LOAN_REC_TYPE};
+  private static final String NOTICE_DELETION_MSG = "Deleting scheduled notice {} as referenced {} {} was not found";
 
   public static DueDateScheduledNoticeHandler using(Clients clients, DateTime systemTime) {
     return new DueDateScheduledNoticeHandler(
@@ -56,7 +70,7 @@ public class DueDateScheduledNoticeHandler {
   public CompletableFuture<Result<Collection<ScheduledNotice>>> handleNotices(Collection<ScheduledNotice> scheduledNotices) {
     CompletableFuture<Result<ScheduledNotice>> future = completedFuture(succeeded(null));
     for (ScheduledNotice scheduledNotice : scheduledNotices) {
-      future = future.thenCompose(r -> r.after(v -> handleNotice(scheduledNotice)));
+      future = future.thenCompose(r -> handleNotice(scheduledNotice));
     }
     return future.thenApply(r -> r.map(v -> scheduledNotices));
   }
@@ -70,12 +84,46 @@ public class DueDateScheduledNoticeHandler {
 
   private CompletableFuture<Result<ScheduledNotice>> handleDueDateNotice(ScheduledNotice notice) {
     return loanRepository.getById(notice.getLoanId())
+      .thenCompose(r -> deleteNoticeIfLoanIsMissingOrIncomplete(r, notice))
       .thenApply(r -> r.map(LoanAndRelatedRecords::new))
       .thenCompose(r -> r.after(loanPolicyRepository::lookupLoanPolicy))
-      .thenCompose(r -> r.combineAfter(configurationRepository::findTimeZoneConfiguration,
-        LoanAndRelatedRecords::withTimeZone))
+      .thenCompose(r -> r.combineAfter(configurationRepository::findTimeZoneConfiguration, LoanAndRelatedRecords::withTimeZone))
       .thenCompose(r -> r.after(records -> sendNotice(records, notice)))
-      .thenCompose(r -> r.after(relatedRecords -> updateNotice(relatedRecords, notice)));
+      .thenCompose(r -> r.after(relatedRecords -> updateNotice(relatedRecords, notice)))
+      .thenApply(r -> r.mapFailure(this::handleFailure));
+  }
+
+  private CompletableFuture<Result<Loan>> deleteNoticeIfLoanIsMissingOrIncomplete(
+      Result<Loan> result, ScheduledNotice notice) {
+
+    if (result.failed() && isRecordNotFoundFailureForType(result.cause(), LOAN_REC_TYPE)) {
+      return deleteInvalidNoticeAndFail(notice, LOAN_REC_TYPE, notice.getLoanId());
+    }
+
+    if (result.succeeded()) {
+      Loan loan = result.value();
+      if (loan.getItem().isNotFound()) {
+        return deleteInvalidNoticeAndFail(notice, ITEM_REC_TYPE, loan.getItemId());
+      }
+      if (loan.getUser() == null) {
+        return deleteInvalidNoticeAndFail(notice, USER_REC_TYPE, loan.getUserId());
+      }
+    }
+    return completedFuture(result);
+  }
+
+  private CompletableFuture<Result<Loan>> deleteInvalidNoticeAndFail(
+      ScheduledNotice notice, String missingRecordType, String missingRecordId) {
+    log.info(NOTICE_DELETION_MSG, notice.getId(), missingRecordType, missingRecordId);
+    return scheduledNoticesRepository.delete(notice)
+      .thenApply(r -> r.next(n -> failed(new RecordNotFoundFailure(missingRecordType, missingRecordId))));
+  }
+
+  private Result<ScheduledNotice> handleFailure(HttpFailure failure) {
+    if (isRecordNotFoundFailureForType(failure, REQUIRED_REC_TYPES)) {
+      return succeeded(null);
+    }
+    return failed(failure);
   }
 
   private CompletableFuture<Result<LoanAndRelatedRecords>> sendNotice(
@@ -136,4 +184,10 @@ public class DueDateScheduledNoticeHandler {
       noticeConfig.getTiming() == NoticeTiming.BEFORE &&
       notice.getNextRunTime().isAfter(loan.getDueDate());
   }
+
+  private boolean isRecordNotFoundFailureForType(HttpFailure failure, String... recordTypes) {
+    return failure instanceof RecordNotFoundFailure
+        && StringUtils.equalsAny(((RecordNotFoundFailure) failure).getRecordType(), recordTypes);
+  }
+  
 }
