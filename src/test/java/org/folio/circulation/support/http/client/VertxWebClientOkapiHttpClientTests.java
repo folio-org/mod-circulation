d12e4b724fbc13cbd2a3f19dd01e593b362f4726
Marc Johnson <marc@k-int.co.uk>
2019-12-10 22:49:04 +0000

    Fail when get using web client times out CIRC-308

--- src/test/java/org/folio/circulation/support/http/client/VertxWebClientOkapiHttpClientTests.java
+++ src/test/java/org/folio/circulation/support/http/client/VertxWebClientOkapiHttpClientTests.java
@@ -1,10 +1,12 @@
 package org.folio.circulation.support.http.client;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static com.github.tomakehurst.wiremock.client.WireMock.okJson;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static org.folio.circulation.support.http.client.VertxWebClientOkapiHttpClient.createClientUsing;
+import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -16,8 +18,10 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 import org.folio.circulation.support.Result;
+import org.folio.circulation.support.ServerErrorFailure;
 import org.folio.circulation.support.VertxAssistant;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
@@ -32,6 +36,13 @@ public class VertxWebClientOkapiHttpClientTests {
 
   @Rule
   public WireMockRule fakeWebServer = new WireMockRule(8081);
+  private final URL okapiUrl = new URL("http://okapi.com");
+  private final String tenantId = "test-tenant";
+  private final String token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJkaWt1X2FkbWluIiwidXNlcl9pZCI6ImFhMjZjYjg4LTc2YjEtNTQ1OS1hMjM1LWZjYTRmZDI3MGMyMyIsImlhdCI6MTU3NjAxMzY3MiwidGVuYW50IjoiZGlrdSJ9.oGCb0gDIdkXGlCiECvJHgQMXD3QKKW2vTh7PPCrpds8";
+  private final String userId = "aa26cb88-76b1-5459-a235-fca4fd270c23";
+  private final String requestId = "test-request-id";
+
+  public VertxWebClientOkapiHttpClientTests() throws MalformedURLException { }
 
   @BeforeClass
   public static void beforeAll() {
@@ -48,14 +59,8 @@ public class VertxWebClientOkapiHttpClientTests {
   }
 
   @Test
-  public void canGetJson() throws InterruptedException, ExecutionException,
-    TimeoutException, MalformedURLException {
-
-    final URL okapiUrl = new URL("http://okapi.com");
-    final String tenantId = "test-tenant";
-    final String token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJkaWt1X2FkbWluIiwidXNlcl9pZCI6ImFhMjZjYjg4LTc2YjEtNTQ1OS1hMjM1LWZjYTRmZDI3MGMyMyIsImlhdCI6MTU3NjAxMzY3MiwidGVuYW50IjoiZGlrdSJ9.oGCb0gDIdkXGlCiECvJHgQMXD3QKKW2vTh7PPCrpds8";
-    final String userId = "aa26cb88-76b1-5459-a235-fca4fd270c23";
-    final String requestId = "test-request-id";
+  public void canGetJson()
+    throws InterruptedException, ExecutionException, TimeoutException {
 
     final String locationResponseHeader = "/a-different-location";
 
@@ -80,5 +85,29 @@ public class VertxWebClientOkapiHttpClientTests {
     assertThat(response.getJson().getString("message"), is("hello"));
     assertThat(response.getContentType(), is("application/json"));
     assertThat(response.getHeader("location"), is(locationResponseHeader));
+  }
+
+  @Test
+  public void failsWhenGetTimesOut()
+    throws InterruptedException, ExecutionException, TimeoutException {
+
+    fakeWebServer.stubFor(get(urlEqualTo("/record"))
+      .withHeader("X-Okapi-Url", equalTo(okapiUrl.toString()))
+      .withHeader("X-Okapi-Tenant", equalTo(tenantId))
+      .withHeader("X-Okapi-Token", equalTo(token))
+      .withHeader("X-Okapi-User-Id", equalTo(userId))
+      .withHeader("X-Okapi-Request-Id", equalTo(requestId))
+      .willReturn(aResponse().withFixedDelay(7000)));
+
+    VertxWebClientOkapiHttpClient client =  createClientUsing(
+      vertxAssistant.createUsingVertx(Vertx::createHttpClient), okapiUrl,
+      tenantId, token, userId, requestId);
+
+    CompletableFuture<Result<Response>> getCompleted = client.get(fakeWebServer.url("/record"));
+
+    final Result<Response> responseResult = getCompleted.get(10, TimeUnit.SECONDS);
+
+    assertThat(responseResult.failed(), is(true));
+    Assert.assertThat(responseResult.cause(), is(instanceOf(ServerErrorFailure.class)));
   }
 }
