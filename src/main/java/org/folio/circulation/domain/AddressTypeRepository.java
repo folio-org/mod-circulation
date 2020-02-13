610b0eea925c8ee6764810f465f8f87ab912c1fa
Oleksandr Vidinieiev <Oleksandr_Vidinieiev@epam.com>
2020-02-13 18:08:46 +0200

    [CIRC-634] removed unused import

--- src/main/java/org/folio/circulation/domain/AddressTypeRepository.java
+++ src/main/java/org/folio/circulation/domain/AddressTypeRepository.java
@@ -4,7 +4,6 @@ import static org.folio.circulation.support.Result.ofAsync;
 import static org.folio.circulation.support.Result.succeeded;
 
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
