5dcc6b19ddaaef8735bef8f88e12e71bacab394a
Kevin Day <kday@library.tamu.edu>
2020-02-13 14:30:17 -0600

    CIRC-637: address SonarCloud Quality Gate issues

--- src/main/java/org/folio/circulation/domain/validation/LoanAndRelatedRecordsValidator.java
+++ src/main/java/org/folio/circulation/domain/validation/LoanAndRelatedRecordsValidator.java
@@ -1,8 +1,13 @@
 package org.folio.circulation.domain.validation;
 
+import static org.apache.commons.lang3.exception.ExceptionUtils.getStackTrace;
+import static org.folio.circulation.support.Result.failed;
 import static org.folio.circulation.support.Result.succeeded;
+import static org.folio.circulation.support.results.CommonFailures.failedDueToServerError;
 import static org.folio.circulation.support.ValidationErrorFailure.singleValidationError;
 
+import java.lang.invoke.MethodHandles;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 import org.folio.circulation.domain.Loan;
@@ -11,8 +16,11 @@ import org.folio.circulation.domain.LoanRepository;
 import org.folio.circulation.support.Result;
 import org.folio.circulation.support.ValidationErrorFailure;
 import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class LoanAndRelatedRecordsValidator {
+  private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
   private LoanAndRelatedRecordsValidator() {}
 
@@ -20,21 +28,23 @@ public class LoanAndRelatedRecordsValidator {
     return result.failWhen(
       r -> {
         Result<Loan> loanResult;
+        CompletableFuture<Result<Loan>> future = loanRepository.getById(r.getLoan().getId());
 
         try {
-          loanResult = loanRepository.getById(r.getLoan().getId()).get();
+          loanResult = future.get();
 
           if (loanResult.succeeded()) {
             return succeeded(isClaimedReturnedOnDueDateChanged(
               r.getLoan(), loanResult.value().getDueDate(), r.getLoan().getDueDate()));
           }
-        } catch (InterruptedException | ExecutionException e) {
-          e.printStackTrace();
+        } catch (ExecutionException | InterruptedException e) {
+          log.debug("Failed to fetch existing Loan for reason {} and stacktrace is {}", e.getMessage(), getStackTrace(e));
+          return failed(failedDueToServerError(e).cause());
         }
 
         return succeeded(false);
       },
-      r -> dueDateChangedFailedForClaimedReturned(r)
+      LoanAndRelatedRecordsValidator::dueDateChangedFailedForClaimedReturned
     );
   }
 
@@ -46,7 +56,7 @@ public class LoanAndRelatedRecordsValidator {
     return result.failWhen(
       r -> succeeded(isClaimedReturnedOnDueDateChanged(
         r.getLoan(), r.getLoan().getDueDate(), dueDate)),
-      r -> dueDateChangedFailedForClaimedReturned(r)
+      LoanAndRelatedRecordsValidator::dueDateChangedFailedForClaimedReturned
     );
   }
 
