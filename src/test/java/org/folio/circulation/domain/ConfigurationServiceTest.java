0fba61e11ae2fe6298e83569ea2aa5578bea4f35
Oleksandr Dekin <oleksandr_dekin@epam.com>
2020-03-16 18:51:10 +0200

    [CIRC-684] No exception handling for notice-session-expiration-by-timeout
    added tests

--- src/test/java/org/folio/circulation/domain/ConfigurationServiceTest.java
+++ src/test/java/org/folio/circulation/domain/ConfigurationServiceTest.java
@@ -8,6 +8,7 @@ import java.util.Collections;
 import api.support.builders.ConfigRecordBuilder;
 import api.support.builders.ConfigurationBuilder;
 import io.vertx.core.json.JsonObject;
+import java.util.List;
 import org.joda.time.DateTimeZone;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -15,6 +16,7 @@ import org.junit.Test;
 public class ConfigurationServiceTest {
 
   private static final String US_LOCALE = "en-US";
+  private static final String VALUE = "value";
 
   private static ConfigurationService service;
 
@@ -62,6 +64,39 @@ public class ConfigurationServiceTest {
     assertEquals(DateTimeZone.UTC, service.findDateTimeZone(jsonObject));
   }
 
+  @Test
+  public void shouldCorrectParseIfCheckoutTimeoutDurationIsInteger() {
+    JsonObject jsonConfig = new JsonObject()
+      .put("value", getJsonConfigWithCheckoutTimeoutDurationAsInteger());
+    List<Configuration> records = Collections.singletonList(new Configuration(jsonConfig));
+
+    Integer expectedSessionTimeout = service.findSessionTimeout(records);
+
+    assertEquals(expectedSessionTimeout, new Integer(1));
+  }
+
+  @Test
+  public void shouldCorrectParseIfCheckoutTimeoutDurationIsString() {
+    JsonObject jsonConfig = new JsonObject()
+      .put(VALUE, getJsonConfigWithCheckoutTimeoutDurationAsString());
+    List<Configuration> records = Collections.singletonList(new Configuration(jsonConfig));
+
+    Integer expectedSessionTimeout = service.findSessionTimeout(records);
+
+    assertEquals(expectedSessionTimeout, new Integer(1));
+  }
+
+  @Test
+  public void shouldCorrectParseIfCheckoutTimeoutDurationIsWrongString() {
+    JsonObject jsonConfig = new JsonObject()
+      .put("value", getJsonConfigWithCheckoutTimeoutDurationAsWrongValue());
+    List<Configuration> records = Collections.singletonList(new Configuration(jsonConfig));
+
+    Integer expectedSessionTimeout = service.findSessionTimeout(records);
+
+    assertEquals(expectedSessionTimeout, new Integer(3));
+  }
+
   private JsonObject getJsonObject(String timeZoneValue) {
     ConfigRecordBuilder config = new ConfigRecordBuilder(timeZoneValue);
     return new ConfigurationBuilder(Collections.singletonList(config)).create();
@@ -71,6 +106,27 @@ public class ConfigurationServiceTest {
     final JsonObject encodedValue = new JsonObject();
     write(encodedValue, "locale", US_LOCALE);
     write(encodedValue, "timezone", timezone);
+    return encodedValue.toString();
+  }
+
+  private String getJsonConfigWithCheckoutTimeoutDurationAsString() {
+    final JsonObject encodedValue = new JsonObject();
+    write(encodedValue, "checkoutTimeoutDuration", "1");
+    write(encodedValue, "checkoutTimeout", true);
+    return encodedValue.toString();
+  }
+
+  private String getJsonConfigWithCheckoutTimeoutDurationAsWrongValue() {
+    final JsonObject encodedValue = new JsonObject();
+    write(encodedValue, "checkoutTimeoutDuration", "test");
+    write(encodedValue, "checkoutTimeout", true);
+    return encodedValue.toString();
+  }
+
+  private String getJsonConfigWithCheckoutTimeoutDurationAsInteger() {
+    final JsonObject encodedValue = new JsonObject();
+    write(encodedValue, "checkoutTimeoutDuration", 1);
+    write(encodedValue, "checkoutTimeout", true);
     return encodedValue.toString();
   }
 }
