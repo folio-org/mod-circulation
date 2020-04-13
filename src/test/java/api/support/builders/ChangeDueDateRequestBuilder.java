bfb6bc4ef7491b7def7a581975e13287fd6bbfae
Jeremy Huff <jeremythuff@gmail.com>
2020-04-13 15:52:21 -0500

    Update src/test/java/api/support/builders/ChangeDueDateRequestBuilder.java
    Co-Authored-By: Marc Johnson <marc.johnson@k-int.com>

--- src/test/java/api/support/builders/ChangeDueDateRequestBuilder.java
+++ src/test/java/api/support/builders/ChangeDueDateRequestBuilder.java
@@ -17,8 +17,7 @@ public class ChangeDueDateRequestBuilder implements Builder {
     this(null, DateTime.now(DateTimeZone.UTC));
   }
 
-  private ChangeDueDateRequestBuilder(
-    String loanId, DateTime dueDate) {
+  private ChangeDueDateRequestBuilder(String loanId, DateTime dueDate) {
 
     this.dueDate = dueDate;
     this.loanId = loanId;
