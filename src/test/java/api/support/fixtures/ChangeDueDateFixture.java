7da6bedf40ca84a9940548b57f97f016bfb6334d
Marc Johnson <marc@k-int.co.uk>
2020-04-20 12:54:47 +0100

    Reformat method parameters in change due date fixture CIRC-647

--- src/test/java/api/support/fixtures/ChangeDueDateFixture.java
+++ src/test/java/api/support/fixtures/ChangeDueDateFixture.java
@@ -9,25 +9,19 @@ import api.support.RestAssuredClient;
 import api.support.builders.ChangeDueDateRequestBuilder;
 
 public class ChangeDueDateFixture {
-
   private final RestAssuredClient restAssuredClient;
 
   public ChangeDueDateFixture() {
     restAssuredClient = new RestAssuredClient(getOkapiHeadersFromContext());
   }
 
-  public Response changeDueDate(
-    ChangeDueDateRequestBuilder request) {
-
+  public Response changeDueDate(ChangeDueDateRequestBuilder request) {
     return restAssuredClient.post(request.create(),
       changeDueDateURL(request.getLoanId()), 204, "change-due-date-request");
   }
 
-  public Response attemptChangeDueDate(
-    ChangeDueDateRequestBuilder request) {
-
+  public Response attemptChangeDueDate(ChangeDueDateRequestBuilder request) {
     return restAssuredClient.post(request.create(),
         changeDueDateURL(request.getLoanId()), "change-due-date-request");
   }
-
 }
