dfec1285059ecc09cc356cb9720e43ff31aaafe7
Marc Johnson <marc@k-int.co.uk>
2020-05-03 17:02:23 +0100

    Use JSON response rather than result when anonymizing loans CIRC-732

--- src/main/java/org/folio/circulation/support/OkJsonResponseResult.java
+++
@@ -1,9 +0,0 @@
-package org.folio.circulation.support;
-
-import io.vertx.core.json.JsonObject;
-
-public class OkJsonResponseResult extends JsonResponseResult {
-  public OkJsonResponseResult(JsonObject body) {
-    super(200, body, null);
-  }
-}
