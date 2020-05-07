9c89fc26083be8f1dd138511f048dd102cd6d6b0
Marc Johnson <marc@k-int.co.uk>
2020-05-07 12:00:15 +0100

    Remove redundant constructor parameter CIRC-732

--- src/main/java/org/folio/circulation/support/http/server/NoContentResponse.java
+++ src/main/java/org/folio/circulation/support/http/server/NoContentResponse.java
@@ -3,19 +3,15 @@ package org.folio.circulation.support.http.server;
 import io.vertx.core.http.HttpServerResponse;
 
 public class NoContentResponse implements HttpResponse {
-  private final int statusCode;
-
   public static void noContent(HttpServerResponse response) {
-    new NoContentResponse(204).writeTo(response);
+    new NoContentResponse().writeTo(response);
   }
 
-  private NoContentResponse(int statusCode) {
-    this.statusCode = statusCode;
-  }
+  public NoContentResponse() { }
 
   @Override
   public void writeTo(HttpServerResponse response) {
-    response.setStatusCode(statusCode);
+    response.setStatusCode(204);
     response.end();
   }
 }
