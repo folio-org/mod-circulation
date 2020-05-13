2e355ef84925c585add73d45cc0e68799be71f63
Marc Johnson <marc@k-int.co.uk>
2020-05-13 09:54:46 +0100

    Extract method for lifting a value to a future result CIRC-717

--- src/main/java/org/folio/circulation/domain/OverdueFineCalculatorService.java
+++ src/main/java/org/folio/circulation/domain/OverdueFineCalculatorService.java
@@ -3,14 +3,12 @@ package org.folio.circulation.domain;
 import static java.util.concurrent.CompletableFuture.completedFuture;
 import static org.apache.commons.lang3.BooleanUtils.isFalse;
 import static org.folio.circulation.domain.OverdueFineCalculatorService.Scenario.CHECKIN;
-import static org.folio.circulation.support.Result.ofAsync;
 import static org.folio.circulation.support.Result.succeeded;
 import static org.folio.circulation.support.ResultBinding.mapResult;
 
 import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
-import java.util.function.Function;
 import java.util.function.Predicate;
 
 import org.apache.commons.lang3.ObjectUtils;
@@ -74,13 +72,9 @@ public class OverdueFineCalculatorService {
     CheckInProcessRecords records, String userId) {
 
     return shouldChargeOverdueFineOnCheckIn(records)
-      .thenCompose(r -> r.afterWhen(liftToFutureResult(),
+      .thenCompose(r -> r.afterWhen(ResultBinding.toFutureResult(),
           b -> createOverdueFineIfNecessary(records.getLoan(), CHECKIN, userId),
           b -> completedFuture(succeeded(null))));
-  }
-
-  private <T> Function<T, CompletableFuture<Result<T>>> liftToFutureResult() {
-    return b -> ofAsync(() -> b);
   }
 
   private CompletableFuture<Result<Boolean>> shouldChargeOverdueFineOnCheckIn(
