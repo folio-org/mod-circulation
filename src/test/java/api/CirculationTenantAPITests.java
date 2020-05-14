1c0ebbd1a0318aa6220df41cf117be5bc43b5164
Alexander Kurash <Oleksandr_Kurash@epam.com>
2020-05-14 15:29:35 +0300

    CIRC-734 Remove unnecessary imports

--- src/test/java/api/CirculationTenantAPITests.java
+++ src/test/java/api/CirculationTenantAPITests.java
@@ -4,13 +4,10 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 import org.folio.circulation.support.http.client.Response;
-import org.folio.util.pubsub.PubSubClientUtils;
 import org.junit.Test;
-import org.powermock.core.classloader.annotations.PrepareForTest;
 
 import api.support.APITests;
 
-@PrepareForTest(PubSubClientUtils.class)
 public class CirculationTenantAPITests extends APITests {
 
   @Test
