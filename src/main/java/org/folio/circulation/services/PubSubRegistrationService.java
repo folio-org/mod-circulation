a101d78424dd9d0e3250d357222faa30ff60b720
Bohdan Suprun <Bohdan_Suprun@epam.com>
2020-05-28 17:27:20 +0300

    CIRC-734: Fix parameters alignment.

--- src/main/java/org/folio/circulation/services/PubSubRegistrationService.java
+++ src/main/java/org/folio/circulation/services/PubSubRegistrationService.java
@@ -24,8 +24,8 @@ public class PubSubRegistrationService {
     throw new IllegalStateException();
   }
 
-  public static CompletableFuture<Boolean> registerModule(Map<String, String> headers,
-                                                          Vertx vertx) {
+  public static CompletableFuture<Boolean> registerModule(
+    Map<String, String> headers, Vertx vertx) {
 
     return PubSubClientUtils.registerModule(new OkapiConnectionParams(headers, vertx))
       .whenComplete((registrationAr, throwable) -> {
@@ -37,8 +37,8 @@ public class PubSubRegistrationService {
       });
   }
 
-  public static CompletableFuture<Boolean> unregisterModule(Map<String, String> headers,
-                                                            Vertx vertx) {
+  public static CompletableFuture<Boolean> unregisterModule(
+    Map<String, String> headers, Vertx vertx) {
 
     List<CompletableFuture<Boolean>> list = new ArrayList<>();
 
