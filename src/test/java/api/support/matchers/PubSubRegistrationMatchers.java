65da733111ae19290fc4f4973b3c3fad9640f357
Bohdan Suprun <Bohdan_Suprun@epam.com>
2020-05-29 11:24:40 +0300

    CIRC-743: Add subscribers assertion to tenant tests.

--- src/test/java/api/support/matchers/PubSubRegistrationMatchers.java
+++ src/test/java/api/support/matchers/PubSubRegistrationMatchers.java
@@ -1,9 +1,9 @@
 package api.support.matchers;
 
 import static com.jayway.jsonpath.matchers.JsonPathMatchers.hasJsonPath;
+import static org.folio.util.pubsub.PubSubClientUtils.constructModuleName;
 import static org.hamcrest.core.Is.is;
 
-import org.folio.util.pubsub.PubSubClientUtils;
 import org.hamcrest.Matcher;
 
 import io.vertx.core.json.JsonObject;
@@ -11,11 +11,19 @@ import io.vertx.core.json.JsonObject;
 public class PubSubRegistrationMatchers {
   public static Matcher<JsonObject> isValidPublishersRegistration() {
     return JsonObjectMatcher.allOfPaths(
-      hasJsonPath("moduleId", is(PubSubClientUtils.constructModuleName())),
+      hasJsonPath("moduleId", is(constructModuleName())),
       hasJsonPath("eventDescriptors[0].eventType", is("ITEM_CHECKED_OUT")),
       hasJsonPath("eventDescriptors[1].eventType", is("ITEM_CHECKED_IN")),
       hasJsonPath("eventDescriptors[2].eventType", is("ITEM_DECLARED_LOST")),
       hasJsonPath("eventDescriptors[3].eventType", is("LOAN_DUE_DATE_CHANGED"))
+    );
+  }
+
+  public static Matcher<JsonObject> isValidSubscribersRegistration() {
+    return JsonObjectMatcher.allOfPaths(
+      hasJsonPath("moduleId", is(constructModuleName())),
+      hasJsonPath("subscriptionDefinitions[0].eventType",
+        is("LOAN_RELATED_FEE_FINE_CLOSED"))
     );
   }
 }
