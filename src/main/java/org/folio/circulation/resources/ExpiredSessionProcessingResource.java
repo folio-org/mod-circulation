d579d3c826e69f4b502db71998624b9d380ee745
Roman_Barannyk <roman_barannyk@epam.com>
2020-06-09 15:01:22 +0300

    CIRC-760 refactoring according to code review

--- src/main/java/org/folio/circulation/resources/ExpiredSessionProcessingResource.java
+++ src/main/java/org/folio/circulation/resources/ExpiredSessionProcessingResource.java
@@ -2,7 +2,9 @@ package org.folio.circulation.resources;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
+import org.apache.commons.lang3.StringUtils;
 import org.folio.circulation.domain.ConfigurationRepository;
 import org.folio.circulation.domain.notice.session.ExpiredSession;
 import org.folio.circulation.domain.notice.session.PatronActionSessionService;
@@ -66,7 +68,11 @@ public class ExpiredSessionProcessingResource extends Resource {
   private CompletableFuture<Result<Void>> attemptEndSession(
     PatronActionSessionService patronSessionService, List<ExpiredSession> expiredSessions) {
 
-    if (expiredSessions.isEmpty()) {
+    List<ExpiredSession> existingExpiredSessions = expiredSessions.stream()
+      .filter(session -> StringUtils.isNotBlank(session.getPatronId()))
+      .collect(Collectors.toList());
+
+    if (existingExpiredSessions.isEmpty()) {
       return CompletableFuture.completedFuture(Result.succeeded(null));
     }
 
