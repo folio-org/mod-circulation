a3a57cfff7e7897be17d5bb71f5081d9b891adaf
jeremythuff <jeremythuff@gmail.com>
2020-06-24 17:36:37 -0500

    WIP creating notes

---
+++ src/main/java/org/folio/circulation/domain/Note.java
@@ -0,0 +1,44 @@
+package org.folio.circulation.domain;
+
+import static org.folio.circulation.support.JsonPropertyFetcher.getProperty;
+import static org.folio.circulation.support.JsonPropertyFetcher.getArrayProperty;
+
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+
+public class Note {
+  private final JsonObject representation;
+
+  public Note(JsonObject representation) {
+    this.representation = representation;
+  }
+
+  public static Note from(JsonObject representation) {
+    return new Note(representation);
+  }
+
+  String getId() {
+    return getProperty(representation, "id");
+  }
+
+  String getTypeId() {
+    return getProperty(representation, "typeId");
+  }
+
+  String getDomain() {
+    return getProperty(representation, "domain");
+  }
+
+  String getTitleString() {
+    return getProperty(representation, "title");
+  }
+
+  String getTitleContent() {
+    return getProperty(representation, "content");
+  }
+
+  JsonArray getLinks() {
+    return getArrayProperty(this.representation, "links");
+  }
+
+}
---
+++ src/main/java/org/folio/circulation/domain/NoteType.java
@@ -0,0 +1,27 @@
+package org.folio.circulation.domain;
+
+import static org.folio.circulation.support.JsonPropertyFetcher.getProperty;
+
+import io.vertx.core.json.JsonObject;
+
+public class NoteType {
+  
+  private final JsonObject representation;
+
+  public NoteType(JsonObject representation) {
+    this.representation = representation;
+  }
+
+  public static NoteType from(JsonObject representation) {
+    return new NoteType(representation);
+  }
+
+  public String getId() {
+    return getProperty(representation, "id");
+  }
+
+  public String getName() {
+    return getProperty(representation, "typeName");
+  }
+
+}
