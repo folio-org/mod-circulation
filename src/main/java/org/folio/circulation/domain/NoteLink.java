ec93b57838ed53976e4f7fdcb77e5b3423d05982
jeremythuff <jeremythuff@gmail.com>
2020-06-24 17:36:37 -0500

    WIP creating notes

---
+++ src/main/java/org/folio/circulation/domain/NoteLink.java
@@ -0,0 +1,19 @@
+package org.folio.circulation.domain;
+
+import io.vertx.core.json.JsonObject;
+
+public class NoteLink extends JsonObject {
+  String id;
+  String type;
+  
+  public NoteLink(String id, String type) {
+    super();
+    this.id = id;
+    this.type = type;
+  }
+
+  public static NoteLink from(String id, String type) {
+    return new NoteLink(id, type);
+  }
+  
+}
---
+++ src/main/java/org/folio/circulation/domain/NoteRepresentation.java
@@ -0,0 +1,65 @@
+package org.folio.circulation.domain;
+
+import java.util.Arrays;
+
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+
+public class NoteRepresentation extends JsonObject {
+  public NoteRepresentation(NoteRepresentationBuilder builder) {
+    super();
+
+    this.put("title", builder.title);
+    this.put("typeId", builder.typeId);
+    this.put("content", builder.content);
+    this.put("domain", builder.domain);
+    this.put("date", builder.date);
+    this.put("links", builder.links);
+
+  }
+
+  public static NoteRepresentationBuilder builder() {
+    return new NoteRepresentationBuilder();
+  }
+
+  public static class NoteRepresentationBuilder {
+    String title;
+    String typeId;
+    String domain;
+    String content;
+    String date;
+    JsonArray links;
+
+    public NoteRepresentationBuilder withTitle(String title) {
+      this.title = title;
+      return this;
+    }
+
+    public NoteRepresentationBuilder withTypeId(String typeId) {
+      this.typeId = typeId;
+      return this;
+    }
+
+    public NoteRepresentationBuilder withDomain(String domain) {
+      this.domain = domain;
+      return this;
+    }
+
+    public NoteRepresentationBuilder withContent(String content) {
+      this.content = content;
+      return this;
+    }
+
+    public NoteRepresentationBuilder withDate(String date) {
+      this.date = date;
+      return this;
+    }
+
+    public NoteRepresentationBuilder withLinks(NoteLink ...links) {
+      this.links = new JsonArray(Arrays.asList(links));
+      return this;
+    }
+
+  }
+
+}
