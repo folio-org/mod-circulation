ba60eedd6e1ef708de2130ef17bf49dff8cb26d7
jeremythuff <jeremythuff@gmail.com>
2020-06-29 10:16:09 -0500

    Generating note for items declared lost

--- src/main/java/org/folio/circulation/resources/DeclareLostResource.java
+++ src/main/java/org/folio/circulation/resources/DeclareLostResource.java
@@ -2,11 +2,22 @@ package org.folio.circulation.resources;
 
 import static org.folio.circulation.support.ValidationErrorFailure.singleValidationError;
 
+import java.time.LocalDateTime;
 import java.util.Objects;
+import java.util.Optional;
+import java.util.concurrent.ExecutionException;
 
 import org.folio.circulation.StoreLoanAndItem;
+import org.folio.circulation.domain.ItemStatus;
 import org.folio.circulation.domain.Loan;
 import org.folio.circulation.infrastructure.storage.loans.LoanRepository;
+import org.folio.circulation.domain.LoanRepository;
+import org.folio.circulation.domain.NoteLink;
+import org.folio.circulation.domain.NoteLinkType;
+import org.folio.circulation.domain.NoteRepresentation;
+import org.folio.circulation.domain.NoteType;
+import org.folio.circulation.domain.NoteTypesRepository;
+import org.folio.circulation.domain.NotesRepository;
 import org.folio.circulation.domain.representations.DeclareItemLostRequest;
 import org.folio.circulation.domain.validation.LoanValidator;
 import org.folio.circulation.services.EventPublisher;
@@ -22,14 +33,16 @@ import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 
 public class DeclareLostResource extends Resource {
+
+  private static final String NOTE_MESSAGE = "Claimed returned item marked lost";
+
   public DeclareLostResource(HttpClient client) {
     super(client);
   }
 
   @Override
   public void register(Router router) {
-    router.post("/circulation/loans/:id/declare-item-lost")
-      .handler(this::declareLost);
+    router.post("/circulation/loans/:id/declare-item-lost").handler(this::declareLost);
   }
 
   private void declareLost(RoutingContext routingContext) {
@@ -42,14 +55,42 @@ public class DeclareLostResource extends Resource {
     final LostItemFeeChargingService lostItemFeeService = new LostItemFeeChargingService(clients);
     final EventPublisher eventPublisher = new EventPublisher(routingContext);
 
-    validateDeclaredLostRequest(routingContext).after(request ->
-      loanRepository.getById(request.getLoanId())
+    final NotesRepository notesRepo = new NotesRepository(clients);
+    final NoteTypesRepository noteTypesRepo = new NoteTypesRepository(clients);
+
+    validateDeclaredLostRequest(routingContext).after(request -> loanRepository.getById(request.getLoanId())
         .thenApply(LoanValidator::refuseWhenLoanIsClosed)
-        .thenApply(this::refuseWhenItemIsAlreadyDeclaredLost)
-        .thenApply(loan -> declareItemLost(loan, request))
+        .thenApply(this::refuseWhenItemIsAlreadyDeclaredLost).thenApply(loan -> {
+          boolean hasBeenClaimedReturned = loan.value().hasItemWithStatus(ItemStatus.CLAIMED_RETURNED);
+
+          Result<Loan> loanResult = declareItemLost(loan, request);
+
+          if (hasBeenClaimedReturned) {
+            Optional<NoteType> noteType;
+            try {
+              noteType = noteTypesRepo.findBy("query=name==\"General note\"").get().value().getRecords().stream()
+                  .findFirst();
+              if(noteType.isPresent()) {
+                notesRepo.create(new NoteRepresentation(NoteRepresentation.builder()
+                  .withTitle(NOTE_MESSAGE)
+                  .withTypeId(noteType.get().getId())
+                  .withContent(NOTE_MESSAGE)
+                  .withDate(LocalDateTime.now().toString())
+                  .withLinks(
+                    NoteLink.from(loan.value().getUserId(), NoteLinkType.USER.getValue())
+                  )
+                ));
+              }
+            } catch (InterruptedException | ExecutionException e) {
+              e.printStackTrace();
+            }
+          }
+
+          return loanResult;
+        })
         .thenCompose(r -> r.after(storeLoanAndItem::updateLoanAndItemInStorage))
         .thenCompose(r -> r.after(loan -> lostItemFeeService
-          .chargeLostItemFees(loan, request, context.getUserId()))))
+        .chargeLostItemFees(loan, request, context.getUserId()))))
       .thenComposeAsync(r -> r.after(eventPublisher::publishDeclaredLostEvent))
       .thenApply(r -> r.toFixedValue(NoContentResponse::noContent))
       .thenAccept(context::writeResultToHttpResponse);
@@ -58,10 +99,9 @@ public class DeclareLostResource extends Resource {
   private Result<Loan> declareItemLost(Result<Loan> loanResult,
     DeclareItemLostRequest request) {
 
-    return loanResult.next(loan -> Result.of(() ->
-      loan.declareItemLost(
-        Objects.toString(request.getComment(), ""),
-        request.getDeclaredLostDateTime())));
+    return loanResult.next(loan -> Result.of(() -> loan.declareItemLost(
+      Objects.toString(request.getComment(), ""),
+      request.getDeclaredLostDateTime())));
   }
 
   private Result<DeclareItemLostRequest> validateDeclaredLostRequest(
