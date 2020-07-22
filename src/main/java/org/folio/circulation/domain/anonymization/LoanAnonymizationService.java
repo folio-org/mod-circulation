148cac76b62401f5e2c996719f3a7df66b6abce5
Marc Johnson <marc@k-int.co.uk>
2020-07-22 12:27:53 +0100

    Reformat asynchronous result chain for anonymizing loans CIRC-850

--- src/main/java/org/folio/circulation/domain/anonymization/LoanAnonymizationService.java
+++ src/main/java/org/folio/circulation/domain/anonymization/LoanAnonymizationService.java
@@ -5,6 +5,5 @@ import java.util.concurrent.CompletableFuture;
 import org.folio.circulation.support.Result;
 
 public interface LoanAnonymizationService {
-
   CompletableFuture<Result<LoanAnonymizationRecords>> anonymizeLoans();
 }
