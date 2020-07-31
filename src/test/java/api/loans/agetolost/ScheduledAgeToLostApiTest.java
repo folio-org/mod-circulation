b8532ed61d71385e8978b4d3a4cc5eab9166e919
Bohdan Suprun <Bohdan_Suprun@epam.com>
2020-07-31 15:26:14 +0300

    Make sure second age to lost does not update loan second time.

--- src/test/java/api/loans/agetolost/ScheduledAgeToLostApiTest.java
+++ src/test/java/api/loans/agetolost/ScheduledAgeToLostApiTest.java
@@ -6,12 +6,14 @@ import static api.support.matchers.ItemMatchers.isCheckedOut;
 import static api.support.matchers.ItemMatchers.isClaimedReturned;
 import static api.support.matchers.JsonObjectMatcher.hasJsonPath;
 import static org.hamcrest.CoreMatchers.allOf;
-import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.iterableWithSize;
 import static org.joda.time.DateTime.now;
 import static org.joda.time.DateTimeZone.UTC;
 
 import org.folio.circulation.support.http.client.IndividualResource;
+import org.hamcrest.Matcher;
 import org.joda.time.DateTime;
 import org.junit.Before;
 import org.junit.Test;
@@ -21,6 +23,7 @@ import api.support.MultipleJsonRecords;
 import api.support.builders.CheckOutByBarcodeRequestBuilder;
 import api.support.spring.SpringApiTest;
 import api.support.spring.clients.ScheduledJobClient;
+import io.vertx.core.json.JsonObject;
 import lombok.val;
 
 public class ScheduledAgeToLostApiTest extends SpringApiTest {
@@ -84,10 +87,21 @@ public class ScheduledAgeToLostApiTest extends SpringApiTest {
     scheduledAgeToLostClient.triggerJob();
 
     assertThat(itemsClient.get(overdueItem).getJson(), isAgedToLost());
-    assertThat(getLoanActions(), hasItem(allOf(
-      hasJsonPath("loan.status.name", "Open"),
-      hasJsonPath("loan.itemStatus", "Aged to lost")
-    )));
+    assertThat(getLoanActions(), hasAgedToLostAction());
+  }
+
+  @Test
+  public void shouldNotProcessAgedToLostItemSecondTime() {
+    scheduledAgeToLostClient.triggerJob();
+    scheduledAgeToLostClient.triggerJob();
+
+    assertThat(itemsClient.get(overdueItem).getJson(), isAgedToLost());
+    assertThat(getLoanActions(), hasAgedToLostAction());
+
+    val agedToLostActions = loanHistoryClient.getMany(
+      queryFromTemplate("loan.id==%s and loan.action==%s", overdueLoan.getId(), "itemAgedToLost"));
+
+    assertThat(agedToLostActions, iterableWithSize(1));
   }
 
   private DateTime getLoanOverdueDate() {
@@ -109,5 +123,13 @@ public class ScheduledAgeToLostApiTest extends SpringApiTest {
   private MultipleJsonRecords getLoanActions() {
     return loanHistoryClient
       .getMany(queryFromTemplate("loan.id==%s and operation==U", overdueLoan.getId()));
+  }
+
+  private Matcher<Iterable<? super JsonObject>> hasAgedToLostAction() {
+    return hasItem(allOf(
+      hasJsonPath("loan.status.name", "Open"),
+      hasJsonPath("loan.action", "itemAgedToLost"),
+      hasJsonPath("loan.itemStatus", "Aged to lost")
+    ));
   }
 }
