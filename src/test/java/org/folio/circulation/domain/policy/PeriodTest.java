048f606bf39e819010a610e7a797b9215dced554
Bohdan Suprun <Bohdan_Suprun@epam.com>
2020-07-31 15:47:40 +0300

    Add unit tests for period comparison.

--- src/test/java/org/folio/circulation/domain/policy/PeriodTest.java
+++ src/test/java/org/folio/circulation/domain/policy/PeriodTest.java
@@ -1,11 +1,19 @@
 package org.folio.circulation.domain.policy;
 
-import junitparams.JUnitParamsRunner;
-import junitparams.Parameters;
+import static org.folio.circulation.domain.policy.Period.days;
+import static org.folio.circulation.domain.policy.Period.months;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import lombok.val;
 
 @RunWith(JUnitParamsRunner.class)
 public class PeriodTest {
@@ -38,5 +46,31 @@ public class PeriodTest {
   public void toMinutesWithUnknownInterval() {
     Period period = Period.from(10, "Unknown interval");
     assertEquals(0, period.toMinutes());
+  }
+
+  @Test
+  public void periodsWithDifferentIntervalNamesIsNotEqual() {
+    val oneMonth = months(1);
+    val thirtyOneDay = days(31);
+
+    assertNotEquals(oneMonth, thirtyOneDay);
+  }
+
+  @Test
+  public void twoPeriodsAreEqual() {
+    val oneMonth = months(1);
+    val oneMonthCopy = months(1);
+
+    assertEquals(oneMonth, oneMonthCopy);
+    assertThat(oneMonth.compareTo(oneMonthCopy), is(0));
+  }
+
+  @Test
+  public void canComparePeriodsWithDifferentIntervals() {
+    val oneMonth = months(1);
+    val thirtyTwoDays = days(32);
+
+    assertThat(oneMonth.compareTo(thirtyTwoDays), equalTo(-1));
+    assertThat(thirtyTwoDays.compareTo(oneMonth), equalTo(1));
   }
 }
