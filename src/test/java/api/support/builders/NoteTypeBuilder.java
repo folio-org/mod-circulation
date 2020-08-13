eccbdddf02d1ac372c38e12fe77058d77414bfb3
Marc Johnson <marc@k-int.co.uk>
2020-08-13 15:30:30 +0100

    Use lombok to reduce code needed to define a note type bulder CIRC-742

--- src/test/java/api/support/builders/NoteTypeBuilder.java
+++ src/test/java/api/support/builders/NoteTypeBuilder.java
@@ -5,36 +5,22 @@ import static org.folio.circulation.support.JsonPropertyWriter.write;
 import java.util.UUID;
 
 import io.vertx.core.json.JsonObject;
+import lombok.AllArgsConstructor;
+import lombok.With;
 
+@With
+@AllArgsConstructor
 public class NoteTypeBuilder extends JsonBuilder implements Builder {
   private final UUID id;
   private final String typeName;
-
   public NoteTypeBuilder() {
-    this.id = null;
-    this.typeName = null;
+    this(null, null);
   }
-
-  public NoteTypeBuilder(UUID id, String typeName) {
-    this.id = id;
-    this.typeName = typeName;
-  }
-
   @Override
   public JsonObject create() {
     JsonObject object = new JsonObject();
-
     write(object, "id", id);
     write(object, "typeName", typeName);
-
     return object;
-  }
-
-  public NoteTypeBuilder withId(UUID id) {
-    return new NoteTypeBuilder(id, typeName);
-  }
-
-  public NoteTypeBuilder withTypeName(String typeName) {
-    return new NoteTypeBuilder(id, typeName);
   }
 }
