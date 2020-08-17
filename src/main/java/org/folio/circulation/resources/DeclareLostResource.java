b47ae4d8da27108b80e389a488c063ac890009d9
Marc Johnson <marc@k-int.co.uk>
2020-08-17 14:53:51 +0100

    Use separate path for when declaring a claimed returned item lost CIRC-742

--- src/main/java/org/folio/circulation/resources/DeclareLostResource.java
+++ src/main/java/org/folio/circulation/resources/DeclareLostResource.java
@@ -1,11 +1,14 @@
 package org.folio.circulation.resources;
 
 import static java.util.concurrent.CompletableFuture.completedFuture;
+import static org.apache.commons.lang.StringUtils.defaultIfBlank;
+import static org.folio.circulation.support.Result.ofAsync;
 import static org.folio.circulation.support.Result.succeeded;
 import static org.folio.circulation.support.ValidationErrorFailure.singleValidationError;
 
-import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.function.Function;
 
 import org.folio.circulation.StoreLoanAndItem;
 import org.folio.circulation.domain.Loan;
@@ -27,7 +30,6 @@ import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 
 public class DeclareLostResource extends Resource {
-  protected boolean isClaimedReturned;
 
   public DeclareLostResource(HttpClient client) {
     super(client);
@@ -42,33 +44,61 @@ public class DeclareLostResource extends Resource {
     final WebContext context = new WebContext(routingContext);
 
     final Clients clients = Clients.create(context, client);
-    final LoanRepository loanRepository = new LoanRepository(clients);
-    final ItemRepository itemRepository = new ItemRepository(clients, true, true, true);
-    final StoreLoanAndItem storeLoanAndItem = new StoreLoanAndItem(loanRepository, itemRepository);
-    final LostItemFeeChargingService lostItemFeeService = new LostItemFeeChargingService(clients);
     final EventPublisher eventPublisher = new EventPublisher(routingContext);
 
     validateDeclaredLostRequest(routingContext)
-      .after(request -> loanRepository.getById(request.getLoanId())
-      .thenApply(LoanValidator::refuseWhenLoanIsClosed)
-      .thenApply(this::refuseWhenItemIsAlreadyDeclaredLost)
-      .thenApply(this::setIsClaimedReturned)
-      .thenApply(loan -> declareItemLost(loan, request))
-      .thenCompose(r -> r.after(loan -> createNote(clients, loan, isClaimedReturned)))
-      .thenCompose(r -> r.after(storeLoanAndItem::updateLoanAndItemInStorage))
-      .thenCompose(r -> r.after(loan -> lostItemFeeService
-        .chargeLostItemFees(loan, request, context.getUserId()))))
+      .after(request -> declareItemLost(request, clients, context))
       .thenComposeAsync(r -> r.after(eventPublisher::publishDeclaredLostEvent))
       .thenApply(r -> r.toFixedValue(NoContentResponse::noContent))
       .thenAccept(context::writeResultToHttpResponse);
   }
 
-  private Result<Loan> declareItemLost(Result<Loan> loanResult,
-    DeclareItemLostRequest request) {
+  private CompletableFuture<Result<Loan>> declareItemLost(DeclareItemLostRequest request,
+    Clients clients, WebContext context) {
 
-    return loanResult.next(loan -> Result.of(() -> loan.declareItemLost(
-      Objects.toString(request.getComment(), ""),
-      request.getDeclaredLostDateTime())));
+    final LoanRepository loanRepository = new LoanRepository(clients);
+    final ItemRepository itemRepository = new ItemRepository(clients, true, true, true);
+    final StoreLoanAndItem storeLoanAndItem = new StoreLoanAndItem(loanRepository, itemRepository);
+    final LostItemFeeChargingService lostItemFeeService = new LostItemFeeChargingService(clients);
+
+    return loanRepository.getById(request.getLoanId())
+      .thenApply(LoanValidator::refuseWhenLoanIsClosed)
+      .thenApply(this::refuseWhenItemIsAlreadyDeclaredLost)
+      .thenCompose(declareItemLost(request, clients))
+      .thenCompose(r -> r.after(storeLoanAndItem::updateLoanAndItemInStorage))
+      .thenCompose(r -> r.after(loan -> lostItemFeeService
+        .chargeLostItemFees(loan, request, context.getUserId())));
+  }
+
+  private Function<Result<Loan>, CompletionStage<Result<Loan>>> declareItemLost(
+    DeclareItemLostRequest request, Clients clients) {
+
+    return r -> r.afterWhen(loan -> ofAsync(loan::isClaimedReturned),
+      loan -> declareItemLostWhenClaimedReturned(loan, request, clients),
+      loan -> declareItemLostWhenNotClaimedReturned(loan, request));
+  }
+
+  private CompletableFuture<Result<Loan>> declareItemLostWhenNotClaimedReturned(
+    Loan loan, DeclareItemLostRequest request) {
+
+    return ofAsync(() -> declareItemLost(loan, request));
+  }
+
+  private CompletableFuture<Result<Loan>> declareItemLostWhenClaimedReturned(
+    Loan loan, DeclareItemLostRequest request, Clients clients) {
+
+    final NotesRepository notesRepository = NotesRepository.createUsing(clients);
+    final NoteCreator creator = new NoteCreator(notesRepository);
+
+    return ofAsync(() -> declareItemLost(loan, request))
+      .thenCompose(r -> r.after(l -> creator.createNote(loan.getUserId(),
+        "Claimed returned item marked declared lost")))
+      .thenCompose(r -> r.after(note -> completedFuture(succeeded(loan))));
+  }
+
+  private Loan declareItemLost(Loan loan, DeclareItemLostRequest request) {
+    return loan.declareItemLost(defaultIfBlank(request.getComment(), ""),
+      request.getDeclaredLostDateTime());
   }
 
   private Result<DeclareItemLostRequest> validateDeclaredLostRequest(
@@ -83,23 +113,5 @@ public class DeclareLostResource extends Resource {
       loan -> Result.succeeded(loan.getItem().isDeclaredLost()),
       loan -> singleValidationError("The item is already declared lost",
         "itemId", loan.getItemId()));
-  }
-
-  protected  Result<Loan> setIsClaimedReturned(Result<Loan> loanResult) {
-    loanResult.map(loan -> isClaimedReturned = loan.isClaimedReturned());
-    return loanResult;
-  }
-
-  protected CompletableFuture<Result<Loan>> createNote(Clients clients, Loan loan, boolean isClaimedReturned) {
-    final NotesRepository notesRepository = NotesRepository.createUsing(clients);
-    final NoteCreator creator = new NoteCreator(notesRepository);
-
-    if (isClaimedReturned) {
-      return creator.createNote(loan.getUserId(), "Claimed returned item marked declared lost")
-        .thenCompose(r -> r.after(note -> completedFuture(succeeded(loan))));
-    }
-    else {
-      return completedFuture(succeeded(loan));
-    }
   }
 }
