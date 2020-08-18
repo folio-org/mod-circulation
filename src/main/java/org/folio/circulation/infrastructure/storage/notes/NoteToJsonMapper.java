d2170d22d38a2157954c10757ff867171a6cc2c2
Marc Johnson <marc@k-int.co.uk>
2020-08-18 14:54:11 +0100

    Use writer to convert note to JSON CIRC-742

--- src/main/java/org/folio/circulation/infrastructure/storage/notes/NoteToJsonMapper.java
+++ src/main/java/org/folio/circulation/infrastructure/storage/notes/NoteToJsonMapper.java
@@ -1,11 +1,11 @@
 package org.folio.circulation.infrastructure.storage.notes;
 
 import static org.folio.circulation.support.JsonPropertyFetcher.getArrayProperty;
+import static org.folio.circulation.support.JsonPropertyWriter.write;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.apache.commons.lang3.StringUtils;
 import org.folio.circulation.domain.NoteLink;
 import org.folio.circulation.domain.notes.Note;
 import org.folio.circulation.support.JsonArrayHelper;
@@ -14,38 +14,34 @@ import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 
 public class NoteToJsonMapper {
-
   private static NoteLink noteLinkFrom(JsonObject jsonObject) {
     return new NoteLink(jsonObject.getString("id"), jsonObject.getString("type"));
   }
 
   public static JsonObject toJson(Note note) {
-    JsonObject jsonObject = new JsonObject();
+    JsonObject json = new JsonObject();
 
     List<JsonObject> jsonLinks = note.getLinks().stream()
       .map(NoteToJsonMapper::toJson)
       .collect(Collectors.toList());
 
-    if (StringUtils.isNotBlank(note.getId())) {
-      jsonObject.put("id", note.getId());
-    }
+    write(json,"id", note.getId());
+    write(json, "typeId", note.getTypeId());
+    write(json, "domain", note.getDomain());
+    write(json, "title", note.getTitle());
+    write(json, "content", note.getContent());
+    write(json, "links", new JsonArray(jsonLinks));
 
-    jsonObject.put("typeId", note.getTypeId());
-    jsonObject.put("domain", note.getDomain());
-    jsonObject.put("title", note.getTitle());
-    jsonObject.put("content", note.getContent());
-    jsonObject.put("links", jsonLinks);
-
-    return jsonObject;
+    return json;
   }
 
   public static JsonObject toJson(NoteLink link) {
-    JsonObject jsonLink = new JsonObject();
+    JsonObject json = new JsonObject();
 
-    jsonLink.put("id", link.getId());
-    jsonLink.put("type", link.getType());
+    write(json,"id", link.getId());
+    write(json, "type", link.getType());
 
-    return jsonLink;
+    return json;
   }
 
   public Note noteFrom(JsonObject jsonObject) {
