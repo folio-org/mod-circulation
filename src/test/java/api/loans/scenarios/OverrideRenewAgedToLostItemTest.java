6976649a637bdd9b59c26ee48b7356526cc0439a
Bohdan Suprun <Bohdan_Suprun@epam.com>
2020-09-10 17:56:07 +0300

    CIRC-844: Rename base class for aged to lost fee refunds.

---
+++ src/test/java/api/loans/scenarios/OverrideRenewAgedToLostItemTest.java
@@ -0,0 +1,22 @@
+package api.loans.scenarios;
+
+import org.joda.time.DateTime;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import api.support.fixtures.AgeToLostFixture;
+import api.support.fixtures.OverrideRenewalFixture;
+
+public class OverrideRenewAgedToLostItemTest extends RefundAgedToLostFeesTestBase {
+  @Autowired
+  private OverrideRenewalFixture overrideRenewalFixture;
+
+  @Override
+  protected void performActionThatRequiresRefund(AgeToLostFixture.AgeToLostResult result,
+     DateTime actionDate) {
+
+    mockClockManagerToReturnFixedDateTime(actionDate);
+
+    overrideRenewalFixture.overrideRenewalByBarcode(result.getLoan(),
+      servicePointsFixture.cd1().getId());
+  }
+}
