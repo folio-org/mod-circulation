3305816e77932db58bcca06944c8095d9a27b2bf
Bohdan Suprun <Bohdan_Suprun@epam.com>
2020-09-16 16:47:22 +0300

    CIRC-920: Add location repository test.

---
+++ src/test/java/org/folio/circulation/infrastructure/storage/inventory/LocationRepositoryTest.java
@@ -0,0 +1,27 @@
+package org.folio.circulation.infrastructure.storage.inventory;
+
+import static org.folio.circulation.infrastructure.storage.inventory.LocationRepository.using;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+
+import org.folio.circulation.support.Clients;
+import org.folio.circulation.support.ServerErrorFailure;
+import org.folio.circulation.support.results.Result;
+import org.junit.Test;
+
+import lombok.val;
+
+public class LocationRepositoryTest {
+
+  @Test
+  public void shouldReturnNullWhenLocationIdIsNull() {
+    final LocationRepository repository = using(mock(Clients.class));
+
+    val result = repository.fetchLocationById(null)
+      .getNow(Result.failed(new ServerErrorFailure("Error")));
+
+    assertTrue(result.succeeded());
+    assertNull(result.value());
+  }
+}
