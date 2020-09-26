1a93db6de39818096d1ddc09fd39093652e61b16
khandramai <Viachaslau_Khandramai@epam.com>
2020-09-26 12:56:08 +0300

    MODAUD-20 # Improvements/refactoring

--- src/main/java/org/folio/circulation/domain/representations/logs/CirculationCheckInCheckOutLogEventMapper.java
+++ src/main/java/org/folio/circulation/domain/representations/logs/CirculationCheckInCheckOutLogEventMapper.java
@@ -1,6 +1,9 @@
 package org.folio.circulation.domain.representations.logs;
 
 import static java.util.Optional.ofNullable;
+import static org.folio.circulation.domain.representations.logs.LogEventPayloadField.*;
+import static org.folio.circulation.domain.representations.logs.LogEventPayloadType.CHECK_IN;
+import static org.folio.circulation.domain.representations.logs.LogEventPayloadType.CHECK_OUT;
 import static org.folio.circulation.support.json.JsonPropertyWriter.write;
 
 import io.vertx.core.json.JsonArray;
@@ -14,64 +17,49 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class CirculationCheckInCheckOutLogEventMapper {
-
-  public static final String CHECK_IN = "CHECK_IN_EVENT";
-  public static final String CHECK_OUT = "CHECK_OUT_EVENT";
-
-  public static final String PL_LOG_EVENT_TYPE = "logEventType";
-  public static final String CLAIMED_RETURNED_RESOLUTION = "claimedReturnedResolution";
-  public static final String PL_SERVICE_POINT_ID = "servicePointId";
-  public static final String PL_REQUESTS = "requests";
-  public static final String PL_ITEM_ID = "itemId";
-  public static final String PL_ITEM_BARCODE = "itemBarcode";
-  public static final String PL_ITEM_STATUS_NAME = "itemStatusName";
-  public static final String PL_IS_CLAIMED_RETURNED = "isClaimedReturned";
-  public static final String PL_DESTINATION_SERVICE_POINT = "destinationServicePoint";
   public static final String ITEM_SOURCE = "source";
-  public static final String PL_SOURCE = "source";
-  public static final String PL_LOAN_ID = "loanId";
-  public static final String PL_IS_LOAN_CLOSED = "isLoanClosed";
-  public static final String PL_SYSTEM_RETURN_DATE = "systemReturnDate";
-  public static final String PL_RETURN_DATE = "returnDate";
-  public static final String PL_DUE_DATE = "dueDate";
-  public static final String PL_USER_ID = "userId";
-  public static final String PL_USER_BARCODE = "userBarcode";
-  public static final String PL_PROXY_BARCODE = "proxyBarcode";
-  public static final String PL_ID = "id";
-  public static final String PL_OLD_REQUEST_STATUS = "oldRequestStatus";
-  public static final String PL_NEW_REQUEST_STATUS = "newRequestStatus";
-  public static final String PL_PICK_UP_SERVICE_POINT = "pickUpServicePoint";
-  public static final String PL_REQUEST_TYPE = "requestType";
-  public static final String PL_ADDRESS_TYPE = "addressType";
 
+  private CirculationCheckInCheckOutLogEventMapper() {
+  }
+
+  /**
+   * This method returns {@link JsonObject} for check-in log event payload
+   * @param checkInContext check-in flow context {@link CheckInContext}
+   * @return check-in log event payload
+   */
   public static JsonObject mapToCheckInLogEventJson(CheckInContext checkInContext) {
     JsonObject logEventPayload = new JsonObject();
 
-    write(logEventPayload, PL_LOG_EVENT_TYPE, CHECK_IN);
-    write(logEventPayload, PL_SERVICE_POINT_ID, checkInContext.getCheckInServicePointId());
+    write(logEventPayload, LOG_EVENT_TYPE.value(), CHECK_IN.value());
+    write(logEventPayload, SERVICE_POINT_ID.value(), checkInContext.getCheckInServicePointId());
 
     populateLoanData(checkInContext, logEventPayload);
     populateItemData(checkInContext, logEventPayload);
 
     ofNullable(checkInContext.getCheckInRequest())
       .flatMap(checkInRequest -> ofNullable(checkInRequest.getClaimedReturnedResolution()))
-      .ifPresent(resolution -> write(logEventPayload, CLAIMED_RETURNED_RESOLUTION, resolution.getValue()));
+      .ifPresent(resolution -> write(logEventPayload, CLAIMED_RETURNED_RESOLUTION.value(), resolution.getValue()));
 
-    write(logEventPayload, PL_REQUESTS, getUpdatedRequests(checkInContext));
+    write(logEventPayload, REQUESTS.value(), getUpdatedRequests(checkInContext));
 
     return logEventPayload;
   }
 
+  /**
+   * This method returns {@link JsonObject} for check-out log event payload
+   * @param loanAndRelatedRecords check-out flow context {@link LoanAndRelatedRecords}
+   * @return check-out log event payload
+   */
   public static JsonObject mapToCheckOutLogEventJson(LoanAndRelatedRecords loanAndRelatedRecords) {
     JsonObject logEventPayload = new JsonObject();
 
-    write(logEventPayload, PL_LOG_EVENT_TYPE, CHECK_OUT);
-    write(logEventPayload, PL_SERVICE_POINT_ID, loanAndRelatedRecords.getLoan().getCheckoutServicePointId());
+    write(logEventPayload, LOG_EVENT_TYPE.value(), CHECK_OUT.value());
+    write(logEventPayload, SERVICE_POINT_ID.value(), loanAndRelatedRecords.getLoan().getCheckoutServicePointId());
 
     populateLoanData(loanAndRelatedRecords, logEventPayload);
     populateItemData(loanAndRelatedRecords, logEventPayload);
 
-    write(logEventPayload, PL_REQUESTS, getUpdatedRequests(loanAndRelatedRecords));
+    write(logEventPayload, REQUESTS.value(), getUpdatedRequests(loanAndRelatedRecords));
 
     return logEventPayload;
   }
@@ -79,25 +67,25 @@ public class CirculationCheckInCheckOutLogEventMapper {
   private static void populateItemData(CheckInContext checkInContext, JsonObject logEventPayload) {
     ofNullable(checkInContext.getItem())
       .ifPresent(item -> {
-        write(logEventPayload, PL_ITEM_ID, item.getItemId());
-        write(logEventPayload, PL_ITEM_BARCODE, item.getBarcode());
-        write(logEventPayload, PL_ITEM_STATUS_NAME, item.getStatusName());
-        write(logEventPayload, PL_IS_CLAIMED_RETURNED, item.isClaimedReturned());
+        write(logEventPayload, ITEM_ID.value(), item.getItemId());
+        write(logEventPayload, ITEM_BARCODE.value(), item.getBarcode());
+        write(logEventPayload, ITEM_STATUS_NAME.value(), item.getStatusName());
+        write(logEventPayload, IS_CLAIMED_RETURNED.value(), item.isClaimedReturned());
         ofNullable(item.getInTransitDestinationServicePoint())
-          .ifPresent(sp -> write(logEventPayload, PL_DESTINATION_SERVICE_POINT, sp.getName()));
+          .ifPresent(sp -> write(logEventPayload, DESTINATION_SERVICE_POINT.value(), sp.getName()));
         ofNullable(item.getMaterialType())
-          .ifPresent(mt -> write(logEventPayload, PL_SOURCE, mt.getString(ITEM_SOURCE)));
+          .ifPresent(mt -> write(logEventPayload, SOURCE.value(), mt.getString(ITEM_SOURCE)));
       });
   }
 
   private static void populateItemData(LoanAndRelatedRecords loanAndRelatedRecords, JsonObject logEventPayload) {
     ofNullable(loanAndRelatedRecords.getLoan().getItem())
       .ifPresent(item -> {
-        write(logEventPayload, PL_ITEM_ID, item.getItemId());
-        write(logEventPayload, PL_ITEM_BARCODE, item.getBarcode());
-        write(logEventPayload, PL_ITEM_STATUS_NAME, item.getStatusName());
+        write(logEventPayload, ITEM_ID.value(), item.getItemId());
+        write(logEventPayload, ITEM_BARCODE.value(), item.getBarcode());
+        write(logEventPayload, ITEM_STATUS_NAME.value(), item.getStatusName());
         ofNullable(item.getMaterialType())
-          .ifPresent(mt -> write(logEventPayload, PL_SOURCE, mt.getString(ITEM_SOURCE)));
+          .ifPresent(mt -> write(logEventPayload, SOURCE.value(), mt.getString(ITEM_SOURCE)));
       });
   }
 
@@ -112,18 +100,18 @@ public class CirculationCheckInCheckOutLogEventMapper {
   private static void populateLoanData(Loan checkInCheckOutLoan, JsonObject logEventPayload) {
     ofNullable(checkInCheckOutLoan)
       .ifPresent(loan -> {
-        write(logEventPayload, PL_LOAN_ID, loan.getId());
-        write(logEventPayload, PL_IS_LOAN_CLOSED, loan.isClosed());
-        write(logEventPayload, PL_SYSTEM_RETURN_DATE, loan.getSystemReturnDate());
-        write(logEventPayload, PL_RETURN_DATE, loan.getReturnDate());
-        write(logEventPayload, PL_DUE_DATE, loan.getDueDate());
+        write(logEventPayload, LOAN_ID.value(), loan.getId());
+        write(logEventPayload, IS_LOAN_CLOSED.value(), loan.isClosed());
+        write(logEventPayload, SYSTEM_RETURN_DATE.value(), loan.getSystemReturnDate());
+        write(logEventPayload, RETURN_DATE.value(), loan.getReturnDate());
+        write(logEventPayload, DUE_DATE.value(), loan.getDueDate());
         ofNullable(loan.getUser())
           .ifPresent(user -> {
-            write(logEventPayload, PL_USER_ID, user.getId());
-            write(logEventPayload, PL_USER_BARCODE, user.getBarcode());
+            write(logEventPayload, USER_ID.value(), user.getId());
+            write(logEventPayload, USER_BARCODE.value(), user.getBarcode());
           });
         ofNullable(loan.getProxy())
-          .ifPresent(proxy -> write(logEventPayload, PL_PROXY_BARCODE, proxy.getBarcode()));
+          .ifPresent(proxy -> write(logEventPayload, PROXY_BARCODE.value(), proxy.getBarcode()));
       });
   }
 
@@ -139,13 +127,12 @@ public class CirculationCheckInCheckOutLogEventMapper {
     return new JsonArray(updatedRequestPairs.stream()
       .map(request -> {
         JsonObject requestPayload = new JsonObject();
-        write(requestPayload, PL_ID, request.getUpdated().getId());
-        write(requestPayload, PL_OLD_REQUEST_STATUS, request.getOriginal().getStatus().getValue());
-        write(requestPayload, PL_NEW_REQUEST_STATUS, request.getUpdated().getStatus().getValue());
-        ofNullable(request.getUpdated().getPickupServicePoint()).ifPresent(sp -> write(requestPayload, PL_PICK_UP_SERVICE_POINT, sp.getName()));
-        ofNullable(request.getUpdated().getRequestType()).ifPresent(rt -> write(requestPayload, PL_REQUEST_TYPE, rt.getValue()));
-        ofNullable(request.getUpdated().getAddressType()).ifPresent(at -> write(requestPayload, PL_ADDRESS_TYPE, at.getName()));
-
+        write(requestPayload, REQUEST_ID.value(), request.getUpdated().getId());
+        write(requestPayload, OLD_REQUEST_STATUS.value(), request.getOriginal().getStatus().getValue());
+        write(requestPayload, NEW_REQUEST_STATUS.value(), request.getUpdated().getStatus().getValue());
+        ofNullable(request.getUpdated().getPickupServicePoint()).ifPresent(sp -> write(requestPayload, PICK_UP_SERVICE_POINT.value(), sp.getName()));
+        ofNullable(request.getUpdated().getRequestType()).ifPresent(rt -> write(requestPayload, REQUEST_TYPE.value(), rt.getValue()));
+        ofNullable(request.getUpdated().getAddressType()).ifPresent(at -> write(requestPayload, REQUEST_ADDRESS_TYPE.value(), at.getName()));
         return requestPayload;
       }).collect(Collectors.toList())
     );
---
+++ src/main/java/org/folio/circulation/domain/representations/logs/LogEventPayloadField.java
@@ -0,0 +1,41 @@
+package org.folio.circulation.domain.representations.logs;
+
+
+public enum LogEventPayloadField {
+
+  LOG_EVENT_TYPE("logEventType"),
+  CLAIMED_RETURNED_RESOLUTION("claimedReturnedResolution"),
+  SERVICE_POINT_ID("servicePointId"),
+  REQUESTS("requests"),
+  ITEM_ID("itemId"),
+  ITEM_BARCODE("itemBarcode"),
+  ITEM_STATUS_NAME("itemStatusName"),
+  IS_CLAIMED_RETURNED("isClaimedReturned"),
+  DESTINATION_SERVICE_POINT("destinationServicePoint"),
+  SOURCE("source"),
+  LOAN_ID("loanId"),
+  IS_LOAN_CLOSED("isLoanClosed"),
+  SYSTEM_RETURN_DATE("systemReturnDate"),
+  RETURN_DATE("returnDate"),
+  DUE_DATE("dueDate"),
+  USER_ID("userId"),
+  USER_BARCODE("userBarcode"),
+  PROXY_BARCODE("proxyBarcode"),
+  REQUEST_ID("id"),
+  OLD_REQUEST_STATUS("oldRequestStatus"),
+  NEW_REQUEST_STATUS("newRequestStatus"),
+  PICK_UP_SERVICE_POINT("pickUpServicePoint"),
+  REQUEST_TYPE("requestType"),
+  REQUEST_ADDRESS_TYPE("addressType");
+
+  private final String value;
+
+  LogEventPayloadField(String value) {
+    this.value = value;
+  }
+
+  public String value() {
+    return this.value;
+  }
+
+}
