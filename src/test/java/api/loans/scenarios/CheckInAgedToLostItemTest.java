2e77aa071cb147efd3f97a8c1733e2a7bf3a4a9b
Bohdan Suprun <Bohdan_Suprun@epam.com>
2020-10-07 11:17:52 +0300

    CIRC-934: Allow refund for Lost and paid items that was Aged to lost before.

--- src/test/java/api/loans/scenarios/CheckInAgedToLostItemTest.java
+++ src/test/java/api/loans/scenarios/CheckInAgedToLostItemTest.java
@@ -1,6 +1,13 @@
 package api.loans.scenarios;
 
+import static api.support.matchers.AccountMatchers.isRefundedFully;
+import static api.support.matchers.ItemMatchers.isLostAndPaid;
+import static api.support.matchers.LoanAccountMatcher.hasLostItemFee;
+import static api.support.matchers.LoanAccountMatcher.hasLostItemProcessingFee;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 import org.joda.time.DateTime;
+import org.junit.Test;
 
 import api.support.builders.CheckInByBarcodeRequestBuilder;
 import api.support.fixtures.AgeToLostFixture;
@@ -16,5 +23,30 @@ public class CheckInAgedToLostItemTest extends RefundAgedToLostFeesTestBase {
       .on(actionDate)
       .at(servicePointsFixture.cd1())
       .forItem(result.getItem()));
+  }
+
+  @Test
+  public void canRefundFeesForLostAndPaidItemsThatWasAgedToLost() {
+    final double processingFee = 12.99;
+    final double itemFee = 12.99;
+
+    final var policy = lostItemFeePoliciesFixture.ageToLostAfterOneMinutePolicy()
+      .withName("canRefundFeesForLostAndPaidItemsThatWasAgedToLost")
+      .chargeProcessingFeeWhenAgedToLost(processingFee)
+      .withSetCost(itemFee)
+      .withNoFeeRefundInterval();
+
+    final var result = ageToLostFixture.createLoanAgeToLostAndChargeFees(policy);
+
+    feeFineAccountFixture.payLostItemFee(result.getLoanId());
+    feeFineAccountFixture.payLostItemProcessingFee(result.getLoanId());
+    eventSubscribersFixture.publishLoanRelatedFeeFineClosedEvent(result.getLoanId());
+
+    assertThat(itemsClient.get(result.getItem()).getJson(), isLostAndPaid());
+
+    checkInFixture.checkInByBarcode(result.getItem());
+
+    assertThat(result.getLoan(), hasLostItemFee(isRefundedFully(itemFee)));
+    assertThat(result.getLoan(), hasLostItemProcessingFee(isRefundedFully(processingFee)));
   }
 }
