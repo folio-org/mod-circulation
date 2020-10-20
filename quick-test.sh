1a35006d05ecce8d4e2298b2a7dda4ae2d4d511c
Marc Johnson <marc@k-int.co.uk>
2020-10-20 15:57:55 +0100

    Easily execute a single test CIRC-959

--- quick-test.sh
+++ quick-test.sh
@@ -3,4 +3,12 @@
 #remove log output
 rm tests.log
 
-mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent test | tee -a tests.log
+if [ -z "$1" ]
+  then
+    mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent test | tee -a tests.log
+  else
+    echo "Using test argument: $1"
+    mvn -Dtest=$1 clean org.jacoco:jacoco-maven-plugin:prepare-agent test | tee -a tests.log
+fi
+
+
