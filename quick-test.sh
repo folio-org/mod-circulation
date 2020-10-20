f0b02979c8438047f00a52992568f1fa3f1c2744
Marc Johnson <marc@k-int.co.uk>
2020-10-20 16:36:39 +0100

    Extract variables to reduce duplicaion in quick test script CIRC-959

--- quick-test.sh
+++ quick-test.sh
@@ -3,12 +3,15 @@
 #remove log output
 rm tests.log
 
+mvnTargets="clean org.jacoco:jacoco-maven-plugin:prepare-agent test"
+teeCommand="tee -a tests.log"
+
 if [ -z "$1" ]
   then
-    mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent test | tee -a tests.log
+    mvn $mvnTargets | $teeCommand
   else
     echo "Using test argument: $1"
-    mvn -Dtest=$1 clean org.jacoco:jacoco-maven-plugin:prepare-agent test | tee -a tests.log
+    mvn -Dtest=$1 $mvnTargets | $teeCommand
 fi
 
 
