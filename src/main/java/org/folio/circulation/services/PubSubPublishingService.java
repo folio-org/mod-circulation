66dd5113792e60cc1db22c3610deb43d1c92858e
Bohdan Suprun <Bohdan_Suprun@epam.com>
2020-10-28 20:15:14 +0200

    Revert pub-sub changes.

--- src/main/java/org/folio/circulation/services/PubSubPublishingService.java
+++ src/main/java/org/folio/circulation/services/PubSubPublishingService.java
@@ -1,32 +1,36 @@
 package org.folio.circulation.services;
 
-import io.vertx.core.logging.Logger;
-import io.vertx.core.logging.LoggerFactory;
-import io.vertx.ext.web.RoutingContext;
-import org.folio.circulation.support.http.server.WebContext;
-import org.folio.rest.jaxrs.model.Event;
-import org.folio.rest.jaxrs.model.EventMetadata;
-import org.folio.rest.util.OkapiConnectionParams;
-import org.folio.util.pubsub.PubSubClientUtils;
+import static org.folio.rest.util.OkapiConnectionParams.OKAPI_TENANT_HEADER;
 
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
-import static org.folio.rest.util.OkapiConnectionParams.OKAPI_TENANT_HEADER;
-import static org.folio.rest.util.OkapiConnectionParams.OKAPI_TOKEN_HEADER;
-import static org.folio.rest.util.OkapiConnectionParams.OKAPI_URL_HEADER;
+import org.folio.HttpStatus;
+import org.folio.circulation.support.http.server.WebContext;
+import org.folio.rest.client.PubsubClient;
+import org.folio.rest.jaxrs.model.Event;
+import org.folio.rest.jaxrs.model.EventMetadata;
+import org.folio.util.pubsub.PubSubClientUtils;
+import org.folio.util.pubsub.exceptions.EventSendingException;
+
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import io.vertx.ext.web.RoutingContext;
 
 public class PubSubPublishingService {
   private static final Logger logger = LoggerFactory.getLogger(PubSubPublishingService.class);
 
   private final Map<String, String> okapiHeaders;
+  private final PubsubClient pubSubClient;
 
   public PubSubPublishingService(RoutingContext routingContext) {
     this(new WebContext(routingContext));
   }
 
   public PubSubPublishingService(WebContext context) {
+    this.pubSubClient = new PubsubClient(context.getOkapiLocation(), context.getTenantId(),
+      context.getOkapiToken());
     this.okapiHeaders = context.getHeaders();
   }
 
@@ -41,12 +45,8 @@ public class PubSubPublishingService {
         .withEventTTL(1));
 
     final CompletableFuture<Boolean> publishResult = new CompletableFuture<>();
-    OkapiConnectionParams params = new OkapiConnectionParams();
-    params.setOkapiUrl(okapiHeaders.get(OKAPI_URL_HEADER));
-    params.setTenantId(okapiHeaders.get(OKAPI_TENANT_HEADER));
-    params.setToken(okapiHeaders.get(OKAPI_TOKEN_HEADER));
 
-    PubSubClientUtils.sendEventMessage(event, params)
+    sendEventMessage(event)
       .whenComplete((result, throwable) -> {
         if (Boolean.TRUE.equals(result)) {
           logger.debug("Event published successfully. ID: {}, type: {}, payload: {}",
@@ -65,5 +65,38 @@ public class PubSubPublishingService {
       });
 
     return publishResult;
+  }
+
+  /**
+   * Method that publishes event to PubSub and includes response body in the exception message in
+   * case of any response status other than 204.
+   *
+   * @param eventMessage Event to be published
+   */
+  private CompletableFuture<Boolean> sendEventMessage(Event eventMessage) {
+    CompletableFuture<Boolean> result = new CompletableFuture<>();
+
+    try {
+      pubSubClient.postPubsubPublish(eventMessage, ar -> {
+        if (ar.statusCode() == HttpStatus.HTTP_NO_CONTENT.toInt()) {
+          result.complete(true);
+        } else {
+          ar.bodyHandler(body -> {
+            EventSendingException exception = new EventSendingException(String.format(
+              "Error during publishing Event Message in PubSub. " +
+                "Status code: %s . Status message: %s . Response body: %s",
+              ar.statusCode(), ar.statusMessage(), body.toString()));
+            logger.error(exception);
+            result.completeExceptionally(exception);
+          });
+        }
+      });
+      return result;
+    } catch (Exception ex) {
+      logger.error("Error during sending event message to PubSub, event {}", ex,
+        eventMessage.getEventPayload());
+      result.completeExceptionally(ex);
+      return result;
+    }
   }
 }
