70c52c8618e46b994980daedcec58e3dac894a54
Alexander Kurash <Oleksandr_Kurash@epam.com>
2020-10-29 16:52:07 +0200

    CIRC-969 Run sendEventMessage on vertx context
    (cherry picked from commit 741ed23a1cfa4e15366ba23e1a9b69733d141161)

--- src/main/java/org/folio/circulation/services/PubSubPublishingService.java
+++ src/main/java/org/folio/circulation/services/PubSubPublishingService.java
@@ -1,19 +1,21 @@
 package org.folio.circulation.services;
 
 import static org.folio.rest.util.OkapiConnectionParams.OKAPI_TENANT_HEADER;
+import static org.folio.rest.util.OkapiConnectionParams.OKAPI_TOKEN_HEADER;
+import static org.folio.rest.util.OkapiConnectionParams.OKAPI_URL_HEADER;
 
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
-import org.folio.HttpStatus;
 import org.folio.circulation.support.http.server.WebContext;
-import org.folio.rest.client.PubsubClient;
 import org.folio.rest.jaxrs.model.Event;
 import org.folio.rest.jaxrs.model.EventMetadata;
+import org.folio.rest.util.OkapiConnectionParams;
 import org.folio.util.pubsub.PubSubClientUtils;
-import org.folio.util.pubsub.exceptions.EventSendingException;
 
+import io.vertx.core.Context;
+import io.vertx.core.Vertx;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import io.vertx.ext.web.RoutingContext;
@@ -22,16 +24,15 @@ public class PubSubPublishingService {
   private static final Logger logger = LoggerFactory.getLogger(PubSubPublishingService.class);
 
   private final Map<String, String> okapiHeaders;
-  private final PubsubClient pubSubClient;
+  private final Context vertxContext;
 
   public PubSubPublishingService(RoutingContext routingContext) {
     this(new WebContext(routingContext));
   }
 
   public PubSubPublishingService(WebContext context) {
-    this.pubSubClient = new PubsubClient(context.getOkapiLocation(), context.getTenantId(),
-      context.getOkapiToken());
     this.okapiHeaders = context.getHeaders();
+    vertxContext = Vertx.currentContext();
   }
 
   public CompletableFuture<Boolean> publishEvent(String eventType, String payload) {
@@ -45,8 +46,12 @@ public class PubSubPublishingService {
         .withEventTTL(1));
 
     final CompletableFuture<Boolean> publishResult = new CompletableFuture<>();
+    OkapiConnectionParams params = new OkapiConnectionParams();
+    params.setOkapiUrl(okapiHeaders.get(OKAPI_URL_HEADER));
+    params.setTenantId(okapiHeaders.get(OKAPI_TENANT_HEADER));
+    params.setToken(okapiHeaders.get(OKAPI_TOKEN_HEADER));
 
-    sendEventMessage(event)
+    vertxContext.runOnContext(v -> PubSubClientUtils.sendEventMessage(event, params)
       .whenComplete((result, throwable) -> {
         if (Boolean.TRUE.equals(result)) {
           logger.debug("Event published successfully. ID: {}, type: {}, payload: {}",
@@ -56,56 +61,14 @@ public class PubSubPublishingService {
           logger.error("Failed to publish event. ID: {}, type: {}, payload: {}", throwable,
             event.getId(), event.getEventType(), event.getEventPayload());
 
-          if (throwable != null && throwable.getMessage() != null &&
-            throwable.getMessage().toLowerCase().contains(
-            "there is no subscribers registered for event type")) {
-
-            publishResult.complete(true);
-          }
-          else {
+          if (throwable == null) {
+            publishResult.complete(false);
+          } else {
             publishResult.completeExceptionally(throwable);
           }
         }
-      });
+      }));
 
     return publishResult;
-  }
-
-  public PubsubClient getPubSubClient() {
-    return pubSubClient;
-  }
-
-  /**
-   * Method that publishes event to PubSub and includes response body in the exception message in
-   * case of any response status other than 204.
-   *
-   * @param eventMessage Event to be published
-   * @return
-   */
-  private CompletableFuture<Boolean> sendEventMessage(Event eventMessage) {
-    CompletableFuture<Boolean> result = new CompletableFuture<>();
-
-    try {
-      getPubSubClient().postPubsubPublish(eventMessage, ar -> {
-        if (ar.statusCode() == HttpStatus.HTTP_NO_CONTENT.toInt()) {
-          result.complete(true);
-        } else {
-          ar.bodyHandler(body -> {
-            EventSendingException exception = new EventSendingException(String.format(
-            "Error during publishing Event Message in PubSub. " +
-              "Status code: %s . Status message: %s . Response body: %s",
-            ar.statusCode(), ar.statusMessage(), body.toString()));
-            logger.error(exception);
-            result.completeExceptionally(exception);
-          });
-        }
-      });
-      return result;
-    } catch (Exception ex) {
-      logger.error("Error during sending event message to PubSub, event {}", ex,
-        eventMessage.getEventPayload());
-      result.completeExceptionally(ex);
-      return result;
-    }
   }
 }
