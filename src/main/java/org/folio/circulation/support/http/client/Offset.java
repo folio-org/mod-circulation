d23ddb2b68a6e6eac8ba913b2da90eb0ff11d2d0
Bohdan Suprun <Bohdan_Suprun@epam.com>
2020-10-30 13:46:51 +0200

    Disallow calculate next page offset with zero and null page limit.

--- src/main/java/org/folio/circulation/support/http/client/Offset.java
+++ src/main/java/org/folio/circulation/support/http/client/Offset.java
@@ -31,6 +31,10 @@ public class Offset implements QueryParameter {
   }
 
   public Offset nextPage(PageLimit pageLimit) {
+    if (pageLimit == null || pageLimit.getLimit() == 0) {
+      throw new IllegalArgumentException("Page limit must be non null and greater than 0");
+    }
+
     return offset(getOffset() + pageLimit.getLimit());
   }
 }
