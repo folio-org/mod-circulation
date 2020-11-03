ccd4033fb8a0764122fe09905c54311766962e3a
Bohdan Suprun <Bohdan_Suprun@epam.com>
2020-11-03 20:49:12 +0200

    Refactor CqlPredicate.

--- src/test/java/api/support/fakes/CqlPredicate.java
+++ src/test/java/api/support/fakes/CqlPredicate.java
@@ -1,6 +1,9 @@
 package api.support.fakes;
 
 import static org.folio.circulation.support.json.JsonPropertyFetcher.getValueByPath;
+import static org.z3950.zing.cql.CQLBoolean.AND;
+import static org.z3950.zing.cql.CQLBoolean.NOT;
+import static org.z3950.zing.cql.CQLBoolean.OR;
 
 import java.util.Map;
 import java.util.Objects;
@@ -20,8 +23,8 @@ public final class CqlPredicate implements Predicate<JsonObject> {
   private static final String MATCH_ALL_RECORDS_INDEX = "cql.allRecords";
   public static final String MATCH_ALL_RECORDS = MATCH_ALL_RECORDS_INDEX + "=1";
 
-  private static final Map<CQLBoolean, CqlLogicalOperator> supportedLogicalOperators = initLogicalOperators();
-  private static final Map<String, CqlBinaryOperator> supportedOperators = initOperatorsTable();
+  private static final Map<CQLBoolean, CqlLogicalOperator> logicalOperators = initLogicalOperators();
+  private static final Map<String, CqlBinaryOperator> binaryOperators = initBinaryOperators();
   private final CQLNode entryNode;
 
   @SneakyThrows
@@ -39,15 +42,15 @@ public final class CqlPredicate implements Predicate<JsonObject> {
   private boolean evaluate(JsonObject json, CQLNode node) {
     if (isTerm(node)) {
       return evaluateTerm(json, (CQLTermNode) node);
-    } else if (isLogicalNode(node)) {
+    } else if (isLogicalOperation(node)) {
       return evaluateLogicalOperation(json, (CQLBooleanNode) node);
     } else {
       throw new IllegalArgumentException("Undefined node type: " + node.getClass().getName());
     }
   }
 
-  private boolean isLogicalNode(CQLNode node) {
-    return node instanceof CQLBooleanNode;
+  private boolean isTerm(CQLNode node) {
+    return node instanceof CQLTermNode;
   }
 
   private boolean evaluateTerm(JsonObject json, CQLTermNode node) {
@@ -55,26 +58,26 @@ public final class CqlPredicate implements Predicate<JsonObject> {
       return true;
     }
 
-    final CqlBinaryOperator cqlBinaryOperator = supportedOperators.get(node.getRelation().getBase());
+    final CqlBinaryOperator cqlBinaryOperator = binaryOperators.get(node.getRelation().getBase());
     return cqlBinaryOperator.apply(json, node.getIndex(), node.getTerm());
   }
 
+  private boolean isLogicalOperation(CQLNode node) {
+    return node instanceof CQLBooleanNode;
+  }
+
   private boolean evaluateLogicalOperation(JsonObject json, CQLBooleanNode node) {
-    final CqlLogicalOperator operator = supportedLogicalOperators.get(node.getOperator());
+    final CqlLogicalOperator operator = logicalOperators.get(node.getOperator());
 
     return operator.apply(evaluate(json, node.getRightOperand()),
       evaluate(json, node.getLeftOperand()));
-  }
-
-  private boolean isTerm(CQLNode node) {
-    return node instanceof CQLTermNode;
   }
 
   private boolean isMatchAllRecordsTerm(CQLTermNode node) {
     return MATCH_ALL_RECORDS_INDEX.equals(node.getIndex());
   }
 
-  private static Map<String, CqlBinaryOperator> initOperatorsTable() {
+  private static Map<String, CqlBinaryOperator> initBinaryOperators() {
     return Map.of(
       "==", operator(Objects::equals),
       "=", operator((expected, actual) -> actual.contains(expected)),
@@ -85,24 +88,23 @@ public final class CqlPredicate implements Predicate<JsonObject> {
       ">=", operator((expected, actual) -> actual.compareTo(expected) >= 0));
   }
 
-  private static CqlBinaryOperator operator(
-    BiFunction<String, String, Boolean> compareOperator) {
-    return (json, propertyName, expectedPropertyValue) -> {
-      final Object actualPropertyValue = getValueByPath(json, propertyName.split("\\."));
+  private static CqlBinaryOperator operator(BiFunction<String, String, Boolean> compareOperator) {
+    return (json, propertyName, expectedValue) -> {
+      final Object actualValue = getValueByPath(json, propertyName.split("\\."));
 
-      if (actualPropertyValue == null) {
+      if (actualValue == null) {
         return false;
       }
 
-      return compareOperator.apply(expectedPropertyValue, actualPropertyValue.toString());
+      return compareOperator.apply(expectedValue, actualValue.toString());
     };
   }
 
   private static Map<CQLBoolean, CqlLogicalOperator> initLogicalOperators() {
     return Map.of(
-      CQLBoolean.AND, (left, right) -> left && right,
-      CQLBoolean.OR, (left, right) -> left || right,
-      CQLBoolean.NOT, (left, right) -> left && !right);
+      AND, (left, right) -> left && right,
+      OR, (left, right) -> left || right,
+      NOT, (left, right) -> left && !right);
   }
 
   @FunctionalInterface
