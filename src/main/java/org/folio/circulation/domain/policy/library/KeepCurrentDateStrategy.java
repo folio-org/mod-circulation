0636934cc199cdc6a70ea76af6ae032302045b7c
Bohdan Suprun <Bohdan_Suprun@epam.com>
2020-11-17 12:32:35 +0200

    CIRC-1013: Use withZoneRetainFields method instead.

--- src/main/java/org/folio/circulation/domain/policy/library/KeepCurrentDateStrategy.java
+++ src/main/java/org/folio/circulation/domain/policy/library/KeepCurrentDateStrategy.java
@@ -9,7 +9,6 @@ import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 
 public class KeepCurrentDateStrategy implements ClosedLibraryStrategy {
-
   private final DateTimeZone zone;
 
   public KeepCurrentDateStrategy(DateTimeZone zone) {
@@ -18,11 +17,6 @@ public class KeepCurrentDateStrategy implements ClosedLibraryStrategy {
 
   @Override
   public Result<DateTime> calculateDueDate(DateTime requestedDate, AdjacentOpeningDays openingDays) {
-    final DateTime dueDate = requestedDate.withZone(zone)
-      // Always keep the requested date
-      .withDate(requestedDate.toLocalDate())
-      .withTime(END_OF_A_DAY);
-
-    return succeeded(dueDate);
+    return succeeded(requestedDate.withZoneRetainFields(zone).withTime(END_OF_A_DAY));
   }
 }
