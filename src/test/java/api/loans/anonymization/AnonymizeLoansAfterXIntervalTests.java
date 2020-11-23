d66a6da5d9f616949ab4e52734e0d8d7fe29617a
Marc Johnson <marc@k-int.co.uk>
2020-11-23 12:03:41 +0000

    Remove unused variable in anonymization API tests CIRC-1025

--- src/test/java/api/loans/anonymization/AnonymizeLoansAfterXIntervalTests.java
+++ src/test/java/api/loans/anonymization/AnonymizeLoansAfterXIntervalTests.java
@@ -2,8 +2,8 @@ package api.loans.anonymization;
 
 import static api.support.PubsubPublisherTestUtils.assertThatPublishedLoanLogRecordEventsAreValid;
 import static api.support.PubsubPublisherTestUtils.getPublishedLogRecordEvents;
-import static api.support.matchers.LoanMatchers.isOpen;
 import static api.support.matchers.LoanMatchers.isAnonymized;
+import static api.support.matchers.LoanMatchers.isOpen;
 import static org.folio.circulation.domain.representations.logs.LogEventType.LOAN;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasItems;
@@ -13,18 +13,16 @@ import static org.hamcrest.Matchers.not;
 import static org.joda.time.DateTime.now;
 import static org.joda.time.DateTimeZone.UTC;
 
-import java.util.List;
 import java.util.UUID;
 
-import api.support.fakes.FakePubSub;
-import io.vertx.core.json.JsonObject;
 import org.folio.circulation.domain.representations.anonymization.LoanAnonymizationAPIResponse;
-import api.support.http.IndividualResource;
 import org.joda.time.DateTime;
 import org.junit.Test;
 
 import api.support.builders.CheckOutByBarcodeRequestBuilder;
 import api.support.builders.LoanHistoryConfigurationBuilder;
+import api.support.fakes.FakePubSub;
+import api.support.http.IndividualResource;
 import api.support.http.ItemResource;
 
 public class AnonymizeLoansAfterXIntervalTests extends LoanAnonymizationTests {
@@ -400,7 +398,6 @@ public class AnonymizeLoansAfterXIntervalTests extends LoanAnonymizationTests {
     assertThat(firstAnonymization.getAnonymizedLoans().size(), is(2));
     assertThat(loansStorageClient.getById(firstLoan.getId()).getJson(), isAnonymized());
     assertThat(loansStorageClient.getById(secondLoan.getId()).getJson(), isAnonymized());
-    List<JsonObject> events = getPublishedLogRecordEvents(LOAN.value());
     assertThat(getPublishedLogRecordEvents(LOAN.value(), "Anonymize").size(), is(2));
     assertThatPublishedLoanLogRecordEventsAreValid();
     FakePubSub.clearPublishedEvents();
