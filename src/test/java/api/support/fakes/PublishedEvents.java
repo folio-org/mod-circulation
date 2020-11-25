4375169532825f7c0df8f3ba16f4b1e380118edb
Marc Johnson <marc@k-int.co.uk>
2020-11-25 22:07:25 +0000

    Reduce duplication in finding events by type CIRC-1025

--- src/test/java/api/support/fakes/PublishedEvents.java
+++ src/test/java/api/support/fakes/PublishedEvents.java
@@ -10,21 +10,16 @@ public class PublishedEvents extends ArrayList<JsonObject> {
   public static Predicate<JsonObject> byEventType(String eventType) {
     return evt -> eventType.equalsIgnoreCase(evt.getString("eventType"));
   }
-
-  public JsonObject getEventByType(String eventType) {
-    return filterEventsByType(eventType)
-      .findFirst().orElse(new JsonObject());
-  }
-
-  public Stream<JsonObject> filterEventsByType(String eventType) {
-    return filter(byEventType(eventType));
-  }
-
+  
   public Stream<JsonObject> filter(Predicate<JsonObject> predicate) {
     return stream().filter(predicate);
   }
 
   public JsonObject findFirst(Predicate<JsonObject> predicate) {
     return filter(predicate).findFirst().orElse(new JsonObject());
+  }
+
+  public JsonObject getEventByType(String eventType) {
+    return findFirst(byEventType(eventType));
   }
 }
