9a604cc5dfb7130e624b7f566b5c4fa1dd2948a5
Illia Dolzhenko <Illia_Dolzhenko@epam.com>
2020-11-30 11:34:19 +0200

    CIRC-925 Add null check

--- src/main/java/org/folio/circulation/resources/LoanNoticeSender.java
+++ src/main/java/org/folio/circulation/resources/LoanNoticeSender.java
@@ -46,6 +46,10 @@ public class LoanNoticeSender {
   }
 
   public CompletableFuture<Result<LoanAndRelatedRecords>> sendManualDueDateChangeNotice(LoanAndRelatedRecords records) {
+    if (records.getLoan() == null || records.getLoan().getUser() == null) {
+      log.info("ManualDueDateNotice was not sent. Record doesn't have a valid loan or user.");
+      return ofAsync(() -> records);
+    }
     return loanPolicyRepository.lookupLoanPolicy(records)
       .thenApply(r -> r.next(recordsWithPolicy -> {
         sendLoanNotice(recordsWithPolicy, NoticeEventType.MANUAL_DUE_DATE_CHANGE);
