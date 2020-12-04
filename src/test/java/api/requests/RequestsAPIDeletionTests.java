4d6cb21c69bfdcd6a1b99887a5c5d3b0e92d6920
Marc Johnson <marc@k-int.co.uk>
2020-12-04 16:24:50 +0000

    Emphasise the deletion of a single request CIRC-1025
    By moving it to the top of the class, because it is the most common scenario

--- src/test/java/api/requests/RequestsAPIDeletionTests.java
+++ src/test/java/api/requests/RequestsAPIDeletionTests.java
@@ -5,8 +5,6 @@ import static java.net.HttpURLConnection.HTTP_OK;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
-import java.util.UUID;
-
 import org.junit.Test;
 
 import api.support.APITests;
@@ -15,6 +13,33 @@ import api.support.builders.RequestBuilder;
 import api.support.http.ItemResource;
 
 public class RequestsAPIDeletionTests extends APITests {
+  @Test
+  public void canDeleteARequestFromTheQueue() {
+    final var nod = itemsFixture.basedUponNod();
+
+    checkOutFixture.checkOutByBarcode(nod);
+
+    final var firstRequestId = requestsFixture.place(requestFor(nod)).getId();
+    final var secondRequestId = requestsFixture.place(requestFor(nod)).getId();
+    final var thirdRequestId = requestsFixture.place(requestFor(nod)).getId();
+
+    requestsFixture.deleteRequest(secondRequestId);
+
+    assertThat(requestsFixture.getById(firstRequestId).getStatusCode(),
+      is(HTTP_OK));
+
+    assertThat(requestsFixture.getById(secondRequestId).getStatusCode(),
+      is(HTTP_NOT_FOUND));
+
+    assertThat(requestsFixture.getById(thirdRequestId).getStatusCode(),
+      is(HTTP_OK));
+
+    MultipleJsonRecords allRequests = requestsFixture.getAllRequests();
+
+    assertThat(allRequests.size(), is(2));
+    assertThat(allRequests.totalRecords(), is(2));
+  }
+
   @Test
   public void canDeleteAllRequests() {
     final ItemResource nod = itemsFixture.basedUponNod();
@@ -35,37 +60,6 @@ public class RequestsAPIDeletionTests extends APITests {
 
     assertThat(allRequests.size(), is(0));
     assertThat(allRequests.totalRecords(), is(0));
-  }
-
-  @Test
-  public void canDeleteAnIndividualRequest() {
-    final ItemResource nod = itemsFixture.basedUponNod();
-    final ItemResource smallAngryPlanet = itemsFixture.basedUponSmallAngryPlanet();
-    final ItemResource temeraire = itemsFixture.basedUponTemeraire();
-
-    checkOutFixture.checkOutByBarcode(nod);
-    checkOutFixture.checkOutByBarcode(smallAngryPlanet);
-    checkOutFixture.checkOutByBarcode(temeraire);
-
-    final UUID firstRequestId = requestsFixture.place(requestFor(nod)).getId();
-    final UUID secondRequestId = requestsFixture.place(requestFor(smallAngryPlanet)).getId();
-    final UUID thirdRequestId = requestsFixture.place(requestFor(temeraire)).getId();
-
-    requestsFixture.deleteRequest(secondRequestId);
-
-    assertThat(requestsFixture.getById(firstRequestId).getStatusCode(),
-      is(HTTP_OK));
-
-    assertThat(requestsFixture.getById(secondRequestId).getStatusCode(),
-      is(HTTP_NOT_FOUND));
-
-    assertThat(requestsFixture.getById(thirdRequestId).getStatusCode(),
-      is(HTTP_OK));
-
-    MultipleJsonRecords allRequests = requestsFixture.getAllRequests();
-
-    assertThat(allRequests.size(), is(2));
-    assertThat(allRequests.totalRecords(), is(2));
   }
 
   private RequestBuilder requestFor(ItemResource item) {
