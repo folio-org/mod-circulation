e9992aeef4356a545fd0b3a34b6ee86a9349c9c3
Marc Johnson <marc@k-int.co.uk>
2020-12-25 09:21:58 +0000

    Use implicit types for local variables in request collection resource CIRC-1066

--- src/main/java/org/folio/circulation/resources/RequestCollectionResource.java
+++ src/main/java/org/folio/circulation/resources/RequestCollectionResource.java
@@ -1,43 +1,42 @@
 package org.folio.circulation.resources;
 
 import static org.folio.circulation.domain.representations.RequestProperties.PROXY_USER_ID;
-import static org.folio.circulation.support.json.JsonPropertyWriter.write;
 import static org.folio.circulation.support.ValidationErrorFailure.singleValidationError;
 import static org.folio.circulation.support.fetching.RecordFetching.findWithCqlQuery;
+import static org.folio.circulation.support.json.JsonPropertyWriter.write;
 import static org.folio.circulation.support.results.MappingFunctions.toFixedValue;
 
-import org.folio.circulation.infrastructure.storage.AutomatedPatronBlocksRepository;
-import org.folio.circulation.infrastructure.storage.ConfigurationRepository;
 import org.folio.circulation.domain.CreateRequestRepositories;
 import org.folio.circulation.domain.CreateRequestService;
-import org.folio.circulation.infrastructure.storage.loans.LoanRepository;
 import org.folio.circulation.domain.MoveRequestProcessAdapter;
 import org.folio.circulation.domain.MoveRequestService;
 import org.folio.circulation.domain.RequestAndRelatedRecords;
-import org.folio.circulation.infrastructure.storage.requests.RequestQueueRepository;
-import org.folio.circulation.infrastructure.storage.requests.RequestRepository;
 import org.folio.circulation.domain.RequestRepresentation;
 import org.folio.circulation.domain.RequestType;
-import org.folio.circulation.infrastructure.storage.ServicePointRepository;
 import org.folio.circulation.domain.UpdateItem;
 import org.folio.circulation.domain.UpdateLoan;
 import org.folio.circulation.domain.UpdateRequestQueue;
 import org.folio.circulation.domain.UpdateRequestService;
 import org.folio.circulation.domain.UpdateUponRequest;
 import org.folio.circulation.domain.UserManualBlock;
-import org.folio.circulation.infrastructure.storage.users.UserRepository;
 import org.folio.circulation.domain.notice.schedule.RequestScheduledNoticeService;
-import org.folio.circulation.infrastructure.storage.loans.LoanPolicyRepository;
-import org.folio.circulation.infrastructure.storage.requests.RequestPolicyRepository;
 import org.folio.circulation.domain.validation.ClosedRequestValidator;
 import org.folio.circulation.domain.validation.ProxyRelationshipValidator;
 import org.folio.circulation.domain.validation.RequestLoanValidator;
 import org.folio.circulation.domain.validation.ServicePointPickupLocationValidator;
 import org.folio.circulation.domain.validation.UserManualBlocksValidator;
+import org.folio.circulation.infrastructure.storage.AutomatedPatronBlocksRepository;
+import org.folio.circulation.infrastructure.storage.ConfigurationRepository;
+import org.folio.circulation.infrastructure.storage.ServicePointRepository;
+import org.folio.circulation.infrastructure.storage.inventory.ItemRepository;
+import org.folio.circulation.infrastructure.storage.loans.LoanPolicyRepository;
+import org.folio.circulation.infrastructure.storage.loans.LoanRepository;
+import org.folio.circulation.infrastructure.storage.requests.RequestPolicyRepository;
+import org.folio.circulation.infrastructure.storage.requests.RequestQueueRepository;
+import org.folio.circulation.infrastructure.storage.requests.RequestRepository;
+import org.folio.circulation.infrastructure.storage.users.UserRepository;
 import org.folio.circulation.services.EventPublisher;
 import org.folio.circulation.support.Clients;
-import org.folio.circulation.support.FindWithCqlQuery;
-import org.folio.circulation.infrastructure.storage.inventory.ItemRepository;
 import org.folio.circulation.support.http.server.JsonHttpResponse;
 import org.folio.circulation.support.http.server.NoContentResponse;
 import org.folio.circulation.support.http.server.WebContext;
@@ -60,28 +59,26 @@ public class RequestCollectionResource extends CollectionResource {
 
   @Override
   void create(RoutingContext routingContext) {
-    final WebContext context = new WebContext(routingContext);
+    final var context = new WebContext(routingContext);
 
-    JsonObject representation = routingContext.getBodyAsJson();
+    final var representation = routingContext.getBodyAsJson();
 
-    final Clients clients = Clients.create(context, client);
-    final EventPublisher eventPublisher = new EventPublisher(routingContext);
+    final var clients = Clients.create(context, client);
+    final var eventPublisher = new EventPublisher(routingContext);
 
-    final UserRepository userRepository = new UserRepository(clients);
-    final LoanRepository loanRepository = new LoanRepository(clients);
-    final LoanPolicyRepository loanPolicyRepository = new LoanPolicyRepository(clients);
-    final RequestNoticeSender requestNoticeSender = RequestNoticeSender.using(clients);
-    final ConfigurationRepository configurationRepository = new ConfigurationRepository(clients);
-    final FindWithCqlQuery<UserManualBlock> userManualBlocksValidator
-      = findWithCqlQuery(clients.userManualBlocksStorageClient(),
-        "manualblocks", UserManualBlock::from);
+    final var userRepository = new UserRepository(clients);
+    final var loanRepository = new LoanRepository(clients);
+    final var loanPolicyRepository = new LoanPolicyRepository(clients);
+    final var requestNoticeSender = RequestNoticeSender.using(clients);
+    final var configurationRepository = new ConfigurationRepository(clients);
+    final var userManualBlocksValidator = findWithCqlQuery(clients.userManualBlocksStorageClient(),
+      "manualblocks", UserManualBlock::from);
 
-    final UpdateUponRequest updateUponRequest = new UpdateUponRequest(
-        new UpdateItem(clients),
-        new UpdateLoan(clients, loanRepository, loanPolicyRepository),
-        UpdateRequestQueue.using(clients));
+    final var updateUponRequest = new UpdateUponRequest(new UpdateItem(clients),
+      new UpdateLoan(clients, loanRepository, loanPolicyRepository),
+      UpdateRequestQueue.using(clients));
 
-    final CreateRequestService createRequestService = new CreateRequestService(
+    final var createRequestService = new CreateRequestService(
       new CreateRequestRepositories(RequestRepository.using(clients),
         new RequestPolicyRepository(clients), configurationRepository,
         new AutomatedPatronBlocksRepository(clients)),
@@ -89,14 +86,14 @@ public class RequestCollectionResource extends CollectionResource {
       requestNoticeSender, new UserManualBlocksValidator(userManualBlocksValidator),
       eventPublisher);
 
-    final RequestFromRepresentationService requestFromRepresentationService =
-      new RequestFromRepresentationService(new ItemRepository(clients, true, true, true),
-        RequestQueueRepository.using(clients), userRepository, loanRepository,
-        new ServicePointRepository(clients),
-        createProxyRelationshipValidator(representation, clients),
-        new ServicePointPickupLocationValidator());
+    final var requestFromRepresentationService = new RequestFromRepresentationService(
+      new ItemRepository(clients, true, true, true),
+      RequestQueueRepository.using(clients), userRepository, loanRepository,
+      new ServicePointRepository(clients),
+      createProxyRelationshipValidator(representation, clients),
+      new ServicePointPickupLocationValidator());
 
-    final RequestScheduledNoticeService scheduledNoticeService = RequestScheduledNoticeService.using(clients);
+    final var scheduledNoticeService = RequestScheduledNoticeService.using(clients);
 
     requestFromRepresentationService.getRequestFrom(representation)
       .thenComposeAsync(r -> r.after(createRequestService::createRequest))
@@ -111,31 +108,29 @@ public class RequestCollectionResource extends CollectionResource {
 
   @Override
   void replace(RoutingContext routingContext) {
-    JsonObject representation = routingContext.getBodyAsJson();
+    final var representation = routingContext.getBodyAsJson();
+
     write(representation, "id", getRequestId(routingContext));
 
-    final WebContext context = new WebContext(routingContext);
-    final Clients clients = Clients.create(context, client);
+    final var context = new WebContext(routingContext);
+    final var clients = Clients.create(context, client);
 
-    final RequestRepository requestRepository = RequestRepository.using(clients);
-    final UpdateRequestQueue updateRequestQueue = UpdateRequestQueue.using(clients);
-    final LoanRepository loanRepository = new LoanRepository(clients);
-    final LoanPolicyRepository loanPolicyRepository = new LoanPolicyRepository(clients);
-    final EventPublisher eventPublisher = new EventPublisher(routingContext);
-    final RequestNoticeSender requestNoticeSender = RequestNoticeSender.using(clients);
-    final ConfigurationRepository configurationRepository = new ConfigurationRepository(clients);
-    final FindWithCqlQuery<UserManualBlock> userManualBlocksValidator
-        = findWithCqlQuery(clients.userManualBlocksStorageClient(),
-        "manualblocks", UserManualBlock::from);
+    final var requestRepository = RequestRepository.using(clients);
+    final var updateRequestQueue = UpdateRequestQueue.using(clients);
+    final var loanRepository = new LoanRepository(clients);
+    final var loanPolicyRepository = new LoanPolicyRepository(clients);
+    final var eventPublisher = new EventPublisher(routingContext);
+    final var requestNoticeSender = RequestNoticeSender.using(clients);
+    final var configurationRepository = new ConfigurationRepository(clients);
+    final var userManualBlocksValidator = findWithCqlQuery(clients.userManualBlocksStorageClient(),
+      "manualblocks", UserManualBlock::from);
 
-    final UpdateItem updateItem = new UpdateItem(clients);
+    final var updateItem = new UpdateItem(clients);
 
-    final UpdateUponRequest updateUponRequest = new UpdateUponRequest(
-        updateItem,
-        new UpdateLoan(clients, loanRepository, loanPolicyRepository),
-        updateRequestQueue);
+    final var updateUponRequest = new UpdateUponRequest(updateItem,
+      new UpdateLoan(clients, loanRepository, loanPolicyRepository), updateRequestQueue);
 
-    final CreateRequestService createRequestService = new CreateRequestService(
+    final var createRequestService = new CreateRequestService(
       new CreateRequestRepositories(requestRepository,
         new RequestPolicyRepository(clients), configurationRepository,
         new AutomatedPatronBlocksRepository(clients)),
@@ -143,19 +138,18 @@ public class RequestCollectionResource extends CollectionResource {
       requestNoticeSender, new UserManualBlocksValidator(userManualBlocksValidator),
       eventPublisher);
 
-    final UpdateRequestService updateRequestService = new UpdateRequestService(
-      requestRepository, updateRequestQueue, new ClosedRequestValidator(requestRepository),
+    final var updateRequestService = new UpdateRequestService(requestRepository,
+      updateRequestQueue, new ClosedRequestValidator(requestRepository),
       requestNoticeSender, updateItem, eventPublisher);
 
-    final RequestFromRepresentationService requestFromRepresentationService =
-      new RequestFromRepresentationService(new ItemRepository(clients, true, true, true),
-        RequestQueueRepository.using(clients), new UserRepository(clients),
-        loanRepository, new ServicePointRepository(clients),
-        createProxyRelationshipValidator(representation, clients),
-        new ServicePointPickupLocationValidator());
+    final var requestFromRepresentationService = new RequestFromRepresentationService(
+      new ItemRepository(clients, true, true, true),
+      RequestQueueRepository.using(clients), new UserRepository(clients),
+      loanRepository, new ServicePointRepository(clients),
+      createProxyRelationshipValidator(representation, clients),
+      new ServicePointPickupLocationValidator());
 
-    final RequestScheduledNoticeService requestScheduledNoticeService =
-      RequestScheduledNoticeService.using(clients);
+    final var requestScheduledNoticeService = RequestScheduledNoticeService.using(clients);
 
     requestFromRepresentationService.getRequestFrom(representation)
       .thenComposeAsync(r -> r.afterWhen(requestRepository::exists,
@@ -170,12 +164,12 @@ public class RequestCollectionResource extends CollectionResource {
 
   @Override
   void get(RoutingContext routingContext) {
-    WebContext context = new WebContext(routingContext);
-    Clients clients = Clients.create(context, client);
+    final var context = new WebContext(routingContext);
+    final var clients = Clients.create(context, client);
 
-    final RequestRepository requestRepository = RequestRepository.using(clients);
+    final var requestRepository = RequestRepository.using(clients);
 
-    String id = getRequestId(routingContext);
+    final var id = getRequestId(routingContext);
 
     requestRepository.getById(id)
       .thenApply(r -> r.map(new RequestRepresentation()::extendedRepresentation))
@@ -185,16 +179,15 @@ public class RequestCollectionResource extends CollectionResource {
 
   @Override
   void delete(RoutingContext routingContext) {
-    WebContext context = new WebContext(routingContext);
-    Clients clients = Clients.create(context, client);
+    final var context = new WebContext(routingContext);
+    final var clients = Clients.create(context, client);
 
-    String id = getRequestId(routingContext);
+    final var id = getRequestId(routingContext);
 
-    final RequestRepository requestRepository = RequestRepository.using(clients);
+    final var requestRepository = RequestRepository.using(clients);
 
-    final UpdateRequestQueue updateRequestQueue = new UpdateRequestQueue(
-      RequestQueueRepository.using(clients), requestRepository,
-      new ServicePointRepository(clients), new ConfigurationRepository(clients));
+    final var updateRequestQueue = new UpdateRequestQueue(RequestQueueRepository.using(clients),
+      requestRepository, new ServicePointRepository(clients), new ConfigurationRepository(clients));
 
     requestRepository.getById(id)
       .thenComposeAsync(r -> r.after(requestRepository::delete))
@@ -205,11 +198,11 @@ public class RequestCollectionResource extends CollectionResource {
 
   @Override
   void getMany(RoutingContext routingContext) {
-    WebContext context = new WebContext(routingContext);
-    Clients clients = Clients.create(context, client);
+    final var context = new WebContext(routingContext);
+    final var clients = Clients.create(context, client);
 
-    final RequestRepository requestRepository = RequestRepository.using(clients);
-    final RequestRepresentation requestRepresentation = new RequestRepresentation();
+    final var requestRepository = RequestRepository.using(clients);
+    final var requestRepresentation = new RequestRepresentation();
 
     requestRepository.findBy(routingContext.request().query())
       .thenApply(r -> r.map(requests ->
@@ -220,8 +213,8 @@ public class RequestCollectionResource extends CollectionResource {
 
   @Override
   void empty(RoutingContext routingContext) {
-    WebContext context = new WebContext(routingContext);
-    Clients clients = Clients.create(context, client);
+    final var context = new WebContext(routingContext);
+    final var clients = Clients.create(context, client);
 
     clients.requestsStorage().delete()
       .thenApply(r -> r.map(toFixedValue(NoContentResponse::noContent)))
@@ -229,32 +222,31 @@ public class RequestCollectionResource extends CollectionResource {
   }
 
   void move(RoutingContext routingContext) {
-    WebContext context = new WebContext(routingContext);
-    Clients clients = Clients.create(context, client);
+    final var context = new WebContext(routingContext);
+    final var clients = Clients.create(context, client);
 
-    JsonObject representation = routingContext.getBodyAsJson();
+    final var representation = routingContext.getBodyAsJson();
 
-    String id = getRequestId(routingContext);
+    final var id = getRequestId(routingContext);
 
-    final RequestRepository requestRepository = RequestRepository.using(clients);
-    final RequestQueueRepository requestQueueRepository = RequestQueueRepository.using(clients);
+    final var requestRepository = RequestRepository.using(clients);
+    final var requestQueueRepository = RequestQueueRepository.using(clients);
 
-    final ItemRepository itemRepository = new ItemRepository(clients, true, true, true);
-    final LoanRepository loanRepository = new LoanRepository(clients);
-    final LoanPolicyRepository loanPolicyRepository = new LoanPolicyRepository(clients);
-    final ConfigurationRepository configurationRepository = new ConfigurationRepository(clients);
+    final var itemRepository = new ItemRepository(clients, true, true, true);
+    final var loanRepository = new LoanRepository(clients);
+    final var loanPolicyRepository = new LoanPolicyRepository(clients);
+    final var configurationRepository = new ConfigurationRepository(clients);
 
-    final UpdateUponRequest updateUponRequest = new UpdateUponRequest(
-      new UpdateItem(clients), new UpdateLoan(clients, loanRepository, loanPolicyRepository),
+    final var updateUponRequest = new UpdateUponRequest(new UpdateItem(clients),
+      new UpdateLoan(clients, loanRepository, loanPolicyRepository),
       UpdateRequestQueue.using(clients));
 
-    final MoveRequestProcessAdapter moveRequestProcessAdapter =
-      new MoveRequestProcessAdapter(itemRepository, loanRepository, requestRepository,
-        requestQueueRepository);
+    final var moveRequestProcessAdapter = new MoveRequestProcessAdapter(itemRepository,
+      loanRepository, requestRepository, requestQueueRepository);
 
-    final EventPublisher eventPublisher = new EventPublisher(routingContext);
+    final var eventPublisher = new EventPublisher(routingContext);
 
-    final MoveRequestService moveRequestService = new MoveRequestService(
+    final var moveRequestService = new MoveRequestService(
       requestRepository, new RequestPolicyRepository(clients),
       updateUponRequest, moveRequestProcessAdapter, new RequestLoanValidator(loanRepository),
       RequestNoticeSender.using(clients), configurationRepository, eventPublisher);
@@ -274,9 +266,8 @@ public class RequestCollectionResource extends CollectionResource {
   private RequestAndRelatedRecords asMove(RequestAndRelatedRecords requestAndRelatedRecords,
     JsonObject representation) {
 
-    String originalItemId = requestAndRelatedRecords.getItemId();
-
-    String destinationItemId = representation.getString("destinationItemId");
+    final var originalItemId = requestAndRelatedRecords.getItemId();
+    final var destinationItemId = representation.getString("destinationItemId");
 
     if (representation.containsKey("requestType")) {
       RequestType requestType = RequestType.from(representation.getString("requestType"));
