9ecab924fd20e94b8d6b01b6306514d489a97c4d
Alexander Kurash <alexander.kurash@gmail.com>
2021-03-26 13:09:55 +0200

    CIRC-1106 Adapt old checkout override tests to cover new overriding functionality
    * CIRC-1106 Add check out override tests
    
    * CIRC-1106 Fix tests
    
    * CIRC-1106 Validate comment for all overrides
    
    * CIRC-1106 Rename test
    
    (cherry picked from commit 0494e333367ddf54e18c0c766258e145149c884a)

---
+++ src/test/java/api/support/builders/CheckOutBlockOverrides.java
@@ -0,0 +1,37 @@
+package api.support.builders;
+
+import io.vertx.core.json.JsonObject;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.With;
+
+@NoArgsConstructor(force = true)
+@AllArgsConstructor
+@With
+public class CheckOutBlockOverrides extends JsonBuilder implements Builder {
+  private final JsonObject itemNotLoanableBlockOverride;
+  private final JsonObject patronBlockOverride;
+  private final JsonObject itemLimitBlockOverride;
+  private final String comment;
+
+  @Override
+  public JsonObject create() {
+    JsonObject blockOverrides = new JsonObject();
+
+    if (itemNotLoanableBlockOverride != null) {
+      put(blockOverrides, "itemNotLoanableBlock", itemNotLoanableBlockOverride);
+    }
+
+    if (patronBlockOverride != null) {
+      put(blockOverrides, "patronBlock", patronBlockOverride);
+    }
+
+    if (itemLimitBlockOverride != null) {
+      put(blockOverrides, "itemLimitBlock", itemLimitBlockOverride);
+    }
+
+    put(blockOverrides, "comment", comment);
+
+    return blockOverrides;
+  }
+}
