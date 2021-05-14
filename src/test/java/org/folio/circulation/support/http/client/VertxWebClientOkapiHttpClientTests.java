ae0769c1c2d36215b26e69517dfdb1dad4b094e6
Kyle Felker <felkerk@gvsu.edu>
2021-05-14 13:38:24 -0400

    fixing VertxWebClientOkapiHttpClientTests

--- src/test/java/org/folio/circulation/support/http/client/VertxWebClientOkapiHttpClientTests.java
+++ src/test/java/org/folio/circulation/support/http/client/VertxWebClientOkapiHttpClientTests.java
@@ -50,7 +50,7 @@ public class VertxWebClientOkapiHttpClientTests {
   private static VertxAssistant vertxAssistant;
 
   @Rule
-  public WireMockRule fakeWebServer = new WireMockRule();
+  public WireMockRule fakeWebServer = new WireMockRule(8081);
   private final URL okapiUrl = new URL("http://okapi.com");
   private final String tenantId = "test-tenant";
   private final String token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJkaWt1X2FkbWluIiwidXNlcl9pZCI6ImFhMjZjYjg4LTc2YjEtNTQ1OS1hMjM1LWZjYTRmZDI3MGMyMyIsImlhdCI6MTU3NjAxMzY3MiwidGVuYW50IjoiZGlrdSJ9.oGCb0gDIdkXGlCiECvJHgQMXD3QKKW2vTh7PPCrpds8";
