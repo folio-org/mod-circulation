8497b186659555367136a62d3be0b3d10febfec1
Marc Johnson <marc@k-int.co.uk>
2021-07-21 15:49:01 +0100

    Inline factory method for creating anonymizer for loans in tenant CIRC-1178

--- src/main/java/org/folio/circulation/domain/anonymization/LoanAnonymization.java
+++ src/main/java/org/folio/circulation/domain/anonymization/LoanAnonymization.java
@@ -2,38 +2,8 @@ package org.folio.circulation.domain.anonymization;
 
 import static org.folio.circulation.support.http.client.PageLimit.limit;
 
-import java.lang.invoke.MethodHandles;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.folio.circulation.domain.anonymization.config.LoanAnonymizationConfiguration;
-import org.folio.circulation.domain.anonymization.service.AnonymizationCheckersService;
-import org.folio.circulation.infrastructure.storage.loans.AnonymizeStorageLoansRepository;
-import org.folio.circulation.services.EventPublisher;
 import org.folio.circulation.support.http.client.PageLimit;
 
 public class LoanAnonymization {
-  private final Logger log = LogManager.getLogger(MethodHandles.lookup().lookupClass());
-
   public static final PageLimit FETCH_LOANS_PAGE_LIMIT = limit(5000);
-
-  private final AnonymizeStorageLoansRepository anonymizeStorageLoansRepository;
-  private final EventPublisher eventPublisher;
-
-  public LoanAnonymization(AnonymizeStorageLoansRepository anonymizeStorageLoansRepository,
-    EventPublisher eventPublisher) {
-
-    this.anonymizeStorageLoansRepository = anonymizeStorageLoansRepository;
-    this.eventPublisher = eventPublisher;
-  }
-
-  public LoanAnonymizationService byCurrentTenant(LoanAnonymizationConfiguration config) {
-    log.info("Initializing loan anonymization for current tenant");
-
-    final var anonymizationCheckersService = new AnonymizationCheckersService(config);
-
-    return new DefaultLoanAnonymizationService(anonymizationCheckersService,
-      anonymizeStorageLoansRepository, eventPublisher);
-  }
-
 }
