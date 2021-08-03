983cf79f0a4710370bbf91a66dfa872be2eab17c
Marc Johnson <marc@k-int.co.uk>
2021-08-03 14:59:28 +0100

    Warn if number of loans to check is invalid CIRC-1178

--- src/main/java/org/folio/Environment.java
+++ src/main/java/org/folio/Environment.java
@@ -1,10 +1,36 @@
 package org.folio;
 
+import static java.lang.Integer.parseInt;
+import static org.apache.commons.lang.StringUtils.isBlank;
+
+import java.lang.invoke.MethodHandles;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Environment {
+  private static final Logger log = LogManager.getLogger(MethodHandles.lookup().lookupClass());
+
   private Environment() { }
 
   public static int getScheduledAnonymizationNumberOfLoansToCheck() {
-    return Integer.parseInt(System.getenv()
-      .getOrDefault("SCHEDULED_ANONYMIZATION_NUMBER_OF_LOANS_TO_CHECK", "50000"));
+    return getVariable("SCHEDULED_ANONYMIZATION_NUMBER_OF_LOANS_TO_CHECK", 50000);
+  }
+
+  private static int getVariable(String key, int defaultValue) {
+    final var variable = System.getenv().get(key);
+
+    if (isBlank(variable)) {
+      return defaultValue;
+    }
+
+    try {
+      return parseInt(variable);
+    }
+    catch(Exception e) {
+      log.warn("Invalid value for '{}': '{}' ", key, variable);
+
+      return defaultValue;
+    }
   }
 }
