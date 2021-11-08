2ed7a307b06bd0014575728bc3f0c310066537d3
Kyle Felker <felkerk@gvsu.edu>
2021-11-08 10:50:59 -0500

    Making requested changes in code review

--- src/main/java/org/folio/circulation/resources/renewal/RenewalResource.java
+++ src/main/java/org/folio/circulation/resources/renewal/RenewalResource.java
@@ -179,7 +179,7 @@ public abstract class RenewalResource extends Resource {
         RenewalContext::withTimeZone))
       .thenComposeAsync(r -> r.after(context -> renew(context, clients, errorHandler)))
       .thenApply(r -> r.next(errorHandler::failWithValidationErrors))
-      .thenApply(this::unsetDateTruncationFlagIfNoOpenRecallsInQueue)
+      .thenApply(r -> r.map(this::unsetDueDateChangedByRecallIfNoOpenRecallsInQueue))
       .thenComposeAsync(r -> r.after(storeLoanAndItem::updateLoanAndItemInStorage))
       .thenComposeAsync(r -> r.after(context -> processFeesFines(context, clients)))
       .thenApplyAsync(r -> r.next(feeFineNoticesService::scheduleOverdueFineNotices))
@@ -191,24 +191,18 @@ public abstract class RenewalResource extends Resource {
       .thenAccept(webContext::writeResultToHttpResponse);
   }
 
-  private Result<RenewalContext> unsetDateTruncationFlagIfNoOpenRecallsInQueue(
-    Result<RenewalContext> renewalContext) {
-    if (renewalContext.failed()) {
+  private RenewalContext unsetDueDateChangedByRecallIfNoOpenRecallsInQueue(
+    RenewalContext renewalContext) {
+
+    Loan loan = renewalContext.getLoan();
+    RequestQueue queue = renewalContext.getRequestQueue();
+
+    if (loan.wasDueDateChangedByRecall() && !queue.hasOpenRecalls()) {
+      return renewalContext.withLoan(loan.unsetDueDateChangedByRecall());
+    }
+    else {
       return renewalContext;
     }
-
-    Loan loan = renewalContext.value().getLoan();
-    RequestQueue queue = renewalContext.value().getRequestQueue();
-
-    if (loan.wasDueDateChangedByRecall() && !hasOpenRecalls(queue)) {
-      loan.unsetDueDateChangedByRecall();
-    }
-    return renewalContext;
-  }
-
-  private Boolean hasOpenRecalls(RequestQueue queue) {
-    return queue.getRequests().stream()
-        .anyMatch(request -> request.getRequestType() == RequestType.RECALL && request.isNotYetFilled());
   }
 
   private CompletableFuture<Result<RenewalContext>> processFeesFines(RenewalContext renewalContext,
--- src/test/java/api/loans/scenarios/ChangeDueDateAPITests.java
+++ src/test/java/api/loans/scenarios/ChangeDueDateAPITests.java
@@ -69,7 +69,6 @@ import api.support.http.ItemResource;
 import io.vertx.core.json.JsonObject;
 
 class ChangeDueDateAPITests extends APITests {
-  private static final Logger log = LogManager.getLogger(MethodHandles.lookup().lookupClass());
   private ItemResource item;
   private IndividualResource loan;
   private DateTime dueDate;
@@ -398,7 +397,7 @@ class ChangeDueDateAPITests extends APITests {
     JsonObject dueDateChangedLoan = loansClient.getById(initialLoan.getId()).getJson();
 
     assertThat(dueDateChangedLoan.getBoolean("dueDateChangedByRecall"), equalTo(false));
-    assertThat("due date is not updated",
+    assertThat("due date should be provided new due date",
     dueDateChangedLoan.getString("dueDate"), isEquivalentTo(newDueDate));
   }
 
