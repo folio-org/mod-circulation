ee4d17bddc698ff8e4fa21a08ff2d970fdc16334
Marc Johnson <marc@k-int.co.uk>
2021-11-25 20:21:09 +0000

    Generate accessors for service point CIRC-1369

--- src/main/java/org/folio/circulation/domain/ServicePoint.java
+++ src/main/java/org/folio/circulation/domain/ServicePoint.java
@@ -1,47 +1,15 @@
 package org.folio.circulation.domain;
 
-import lombok.AllArgsConstructor;
+import lombok.Value;
 
-@AllArgsConstructor
+@Value
 public class ServicePoint {
-  private final String id;
-  private final String name;
-  private final String code;
-  private final boolean pickupLocation;
-  private final String discoveryDisplayName;
-  private final String description;
-  private final Integer shelvingLagTime;
-  private final TimePeriod holdShelfExpiryPeriod;
-
-  public boolean isPickupLocation() {
-    return pickupLocation;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public String getId() {
-    return id;
-  }
-
-  public String getCode() {
-    return code;
-  }
-
-  public String getDiscoveryDisplayName() {
-    return discoveryDisplayName;
-  }
-
-  public String getDescription() {
-    return description;
-  }
-
-  public Integer getShelvingLagTime() {
-    return shelvingLagTime;
-  }
-
-  public TimePeriod getHoldShelfExpiryPeriod() {
-    return holdShelfExpiryPeriod;
-  }
+  String id;
+  String name;
+  String code;
+  boolean pickupLocation;
+  String discoveryDisplayName;
+  String description;
+  Integer shelvingLagTime;
+  TimePeriod holdShelfExpiryPeriod;
 }
