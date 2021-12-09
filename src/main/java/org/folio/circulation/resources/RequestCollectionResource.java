469820757c91971397cb196bbe4e5d2e3815e02d
Marc Johnson <marc@k-int.co.uk>
2021-12-09 20:58:48 +0000

    Extract loan repository dependency for event publisher CIRC-1418

--- src/main/java/org/folio/circulation/resources/RequestCollectionResource.java
+++ src/main/java/org/folio/circulation/resources/RequestCollectionResource.java
@@ -115,7 +115,7 @@ public class RequestCollectionResource extends CollectionResource {
       representation))
       .flatMapFuture(createRequestService::createRequest)
       .onSuccess(scheduledNoticeService::scheduleRequestNotices)
-      .onSuccess(records -> eventPublisher.publishDueDateChangedEvent(records, clients))
+      .onSuccess(records -> eventPublisher.publishDueDateChangedEvent(records, loanRepository))
       .map(RequestAndRelatedRecords::getRequest)
       .map(new RequestRepresentation()::extendedRepresentation)
       .map(JsonHttpResponse::created)
@@ -173,7 +173,7 @@ public class RequestCollectionResource extends CollectionResource {
       representation))
       .flatMapFuture(when(requestRepository::exists, updateRequestService::replaceRequest,
         createRequestService::createRequest))
-      .flatMapFuture(records -> eventPublisher.publishDueDateChangedEvent(records, clients))
+      .flatMapFuture(records -> eventPublisher.publishDueDateChangedEvent(records, loanRepository))
       .map(requestScheduledNoticeService::rescheduleRequestNotices)
       .map(toFixedValue(NoContentResponse::noContent))
       .onComplete(context::write, context::write);
@@ -278,7 +278,7 @@ public class RequestCollectionResource extends CollectionResource {
       .map(RequestAndRelatedRecords::new)
       .map(request -> asMove(request, representation))
       .flatMapFuture(move -> moveRequestService.moveRequest(move, move.getOriginalRequest()))
-      .onSuccess(records -> eventPublisher.publishDueDateChangedEvent(records, clients))
+      .onSuccess(records -> eventPublisher.publishDueDateChangedEvent(records, loanRepository))
       .map(RequestAndRelatedRecords::getRequest)
       .map(new RequestRepresentation()::extendedRepresentation)
       .map(JsonHttpResponse::ok)
