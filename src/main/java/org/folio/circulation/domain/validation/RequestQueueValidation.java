7d66e450f7159b65830a110a0eb5a719954e69fb
Alexander Kurash <alexander.kurash@gmail.com>
2021-12-09 13:54:10 +0200

    CIRC-1298 Consider title-level requests during request queue management (#977)
    * CIRC-1296 Use enum for trl feature status
    
    * CIRC-1296 Use enum for trl feature status
    
    * CIRC-1298 Initial implementation
    
    * CIRC-1298 Change validation for unified queue
    
    * CIRC-1298 Fix reorder tests
    
    * CIRC-1298 Unified queue sanity check tests
    
    * CIRC-1298 Fix log events test
    
    * CIRC-1298 Change number of expected events
    
    * CIRC-1298 Refactor URIs, resource class
    
    * CIRC-1298 Change queue URIs in tests
    
    * CIRC-1298 Fix routing
    
    * CIRC-1298 Fix queue URI in tests
    
    * CIRC-1298 Increment major interface version
    
    * CIRC-1298 Remove comment

--- src/main/java/org/folio/circulation/domain/validation/RequestQueueValidation.java
+++ src/main/java/org/folio/circulation/domain/validation/RequestQueueValidation.java
@@ -1,11 +1,13 @@
 package org.folio.circulation.domain.validation;
 
 import static org.folio.circulation.support.ValidationErrorFailure.singleValidationError;
+import static org.folio.circulation.support.results.Result.succeeded;
 
 import java.util.Comparator;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.folio.circulation.domain.Request;
 import org.folio.circulation.domain.reorder.ReorderRequest;
@@ -20,44 +22,97 @@ public class RequestQueueValidation {
     throw new UnsupportedOperationException("Do not instantiate");
   }
 
-  public static Result<ReorderRequestContext> queueFoundForItem(
+  public static Result<ReorderRequestContext> queueIsFound(
     Result<ReorderRequestContext> result) {
+
     return result.failWhen(
-      r -> Result.succeeded(r.getRequestQueue().getRequests().isEmpty()),
-      r -> new RecordNotFoundFailure("Item", r.getItemId())
-    );
+      r -> succeeded(r.getRequestQueue().getRequests().isEmpty()),
+      r -> new RecordNotFoundFailure(r.isQueueForInstance() ? "Instance" : "Item",
+        r.getIdParamValue()));
   }
 
   /**
    * Verifies that provided reordered queue has exact the same requests that currently present
    * in the DB. No new requests added to neither reorderedQueue nor actual queue in DB.
+   * Used for both item and instance queues.
    *
    * @param result - Context.
    * @return New result, failed when the validation has not been passed.
    */
   public static Result<ReorderRequestContext> queueIsConsistent(Result<ReorderRequestContext> result) {
     return result.failWhen(
-      context -> Result.succeeded(isQueueInconsistent(context)),
+      context -> succeeded(isQueueInconsistent(context)),
       context -> singleValidationError(
-        "There is inconsistency between provided reordered queue and item queue.",
+        "There is inconsistency between provided reordered queue and existing queue.",
         null, null)
     );
   }
 
-  public static Result<ReorderRequestContext> fulfillingRequestHasFirstPosition(
+  public static Result<ReorderRequestContext> fulfillingRequestsPositioning(
+    Result<ReorderRequestContext> result) {
+
+    if (result.failed()) {
+      return result;
+    }
+
+    return result.value().isQueueForInstance()
+      ? fulfillingRequestsHaveTopPositions(result)
+      : fulfillingRequestHasFirstPosition(result);
+  }
+
+  /**
+   * Makes sure that all requests that are in the process of fulfilment are always at the top of
+   * the unified queue (TLR feature enabled).
+   *
+   * @param result - Context
+   * @return New result, failed if validation has failed
+   */
+  private static Result<ReorderRequestContext> fulfillingRequestsHaveTopPositions(
+    Result<ReorderRequestContext> result) {
+    return validateRequestsAtTopPositions(result, RequestHelper::isRequestBeganFulfillment,
+      "Requests can not be displaced from top positions when fulfillment begun.");
+  }
+
+  private static Result<ReorderRequestContext> fulfillingRequestHasFirstPosition(
     Result<ReorderRequestContext> result) {
     return validateRequestAtFirstPosition(result, RequestHelper::isRequestBeganFulfillment,
       "Requests can not be displaced from position 1 when fulfillment begun.");
   }
 
-  public static Result<ReorderRequestContext> pageRequestHasFirstPosition(Result<ReorderRequestContext> result) {
+  public static Result<ReorderRequestContext> pageRequestsPositioning(
+    Result<ReorderRequestContext> result) {
+
+    if (result.failed()) {
+      return result;
+    }
+
+    return result.value().isQueueForInstance()
+      ? pageRequestsHaveTopPositions(result)
+      : pageRequestHasFirstPosition(result);
+  }
+
+  /**
+   * Makes sure that all Page requests are always at the top of the unified queue (TLR feature
+   * enabled).
+   *
+   * @param result - Context
+   * @return New result, failed if validation has failed
+   */
+  private static Result<ReorderRequestContext> pageRequestsHaveTopPositions(
+    Result<ReorderRequestContext> result) {
+
+    return validateRequestsAtTopPositions(result, RequestHelper::isPageRequest,
+      "Page requests can not be displaced from top positions.");
+  }
+
+  private static Result<ReorderRequestContext> pageRequestHasFirstPosition(Result<ReorderRequestContext> result) {
     return validateRequestAtFirstPosition(result, RequestHelper::isPageRequest,
       "Page requests can not be displaced from position 1.");
   }
 
   /**
-   * Verifies that newPositions has sequential order, i.e. 1, 2, 3, 4
-   * but not 1, 2, 3, 40.
+   * Verifies that newPositions has sequential order, i.e. 1, 2, 3, 4 but not 1, 2, 3, 40.
+   * Used for both item and instance queues.
    *
    * @param result - Context object.
    * @return New Result, failed if validation have not been passed.
@@ -73,13 +128,13 @@ public class RequestQueueValidation {
         int expectedCurrentPosition = 1;
         for (ReorderRequest reorderRequest : sortedReorderedQueue) {
           if (reorderRequest.getNewPosition() != expectedCurrentPosition) {
-            return Result.succeeded(true);
+            return succeeded(true);
           }
 
           expectedCurrentPosition++;
         }
 
-        return Result.succeeded(false);
+        return succeeded(false);
       },
       r -> singleValidationError("Positions must have sequential order.", "newPosition", null)
     );
@@ -117,9 +172,31 @@ public class RequestQueueValidation {
               && reorderRequest.getNewPosition() != 1;
           });
 
-        return Result.succeeded(notAtFirstPosition);
+        return succeeded(notAtFirstPosition);
       },
       r -> singleValidationError(onFailureMessage, "newPosition", null)
     );
   }
+
+  private static Result<ReorderRequestContext> validateRequestsAtTopPositions(
+    Result<ReorderRequestContext> result,
+    Predicate<Request> requestTypePredicate,
+    String onFailureMessage) {
+
+    return result.failWhen(context -> {
+        List<Integer> newPositions = context.getReorderRequestToRequestMap()
+          .entrySet()
+          .stream()
+          .filter(entry -> requestTypePredicate.test(entry.getValue()))
+          .map(entry -> entry.getKey().getNewPosition())
+          .sorted()
+          .collect(Collectors.toList());
+
+        return succeeded(IntStream.range(0, newPositions.size())
+          .anyMatch(i -> newPositions.get(i) != i + 1));
+      },
+      r -> singleValidationError(onFailureMessage, "newPosition", null)
+    );
+  }
+
 }
