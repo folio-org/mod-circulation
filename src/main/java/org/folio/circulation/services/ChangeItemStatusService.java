702dbcb2a92e6e6a56865de27d05359b61477cdc
Marc Johnson <marc@k-int.co.uk>
2021-12-10 11:44:31 +0000

    Extract repository dependencies for change item status service CIRC-1418

--- src/main/java/org/folio/circulation/services/ChangeItemStatusService.java
+++ src/main/java/org/folio/circulation/services/ChangeItemStatusService.java
@@ -6,21 +6,18 @@ import org.folio.circulation.StoreLoanAndItem;
 import org.folio.circulation.domain.Loan;
 import org.folio.circulation.domain.representations.ChangeItemStatusRequest;
 import org.folio.circulation.domain.validation.LoanValidator;
-import org.folio.circulation.infrastructure.storage.inventory.ItemRepository;
 import org.folio.circulation.infrastructure.storage.loans.LoanRepository;
-import org.folio.circulation.infrastructure.storage.users.UserRepository;
-import org.folio.circulation.support.Clients;
 import org.folio.circulation.support.results.Result;
 
 public class ChangeItemStatusService {
   private final LoanRepository loanRepository;
   private final StoreLoanAndItem storeLoanAndItem;
 
-  public ChangeItemStatusService(Clients clients) {
-    final var itemRepository = new ItemRepository(clients);
-    final var userRepository = new UserRepository(clients);
-    this.loanRepository = new LoanRepository(clients, itemRepository, userRepository);
-      this.storeLoanAndItem = new StoreLoanAndItem(loanRepository, itemRepository);
+  public ChangeItemStatusService(LoanRepository loanRepository,
+    StoreLoanAndItem storeLoanAndItem) {
+
+    this.loanRepository = loanRepository;
+    this.storeLoanAndItem = storeLoanAndItem;
   }
 
   public <T extends ChangeItemStatusRequest> CompletableFuture<Result<Loan>> getOpenLoan(T request) {
