133ad14d7090a57b5dedab5ccfbd1b2cadb88977
Sherzod Nurjonov <jd.sherzod@gmail.com>
2021-12-23 15:08:22 +0500

    conflicts resolved

--- src/main/java/org/folio/circulation/infrastructure/storage/inventory/InstanceRepository.java
+++ src/main/java/org/folio/circulation/infrastructure/storage/inventory/InstanceRepository.java
@@ -1,15 +1,30 @@
 package org.folio.circulation.infrastructure.storage.inventory;
 
-import java.util.concurrent.CompletableFuture;
+import static java.util.concurrent.CompletableFuture.completedFuture;
+import static org.folio.circulation.support.fetching.RecordFetching.findWithMultipleCqlIndexValues;
+import static org.folio.circulation.support.results.Result.succeeded;
 
+import java.lang.invoke.MethodHandles;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.folio.circulation.domain.Instance;
+import org.folio.circulation.domain.MultipleRecords;
 import org.folio.circulation.domain.Request;
 import org.folio.circulation.support.Clients;
 import org.folio.circulation.support.CollectionResourceClient;
+import org.folio.circulation.support.FindWithMultipleCqlIndexValues;
 import org.folio.circulation.support.SingleRecordFetcher;
 import org.folio.circulation.support.results.Result;
 
 public class InstanceRepository {
+  private static final Logger log = LogManager.getLogger(MethodHandles.lookup().lookupClass());
   private final CollectionResourceClient instancesClient;
 
   public InstanceRepository(Clients clients) {
@@ -24,5 +39,53 @@ public class InstanceRepository {
     return SingleRecordFetcher.jsonOrNull(instancesClient, "instance")
       .fetch(instanceId)
       .thenApply(r -> r.map(Instance::from));
+  }
+
+  public CompletableFuture<Result<MultipleRecords<Request>>> findInstancesForRequests(MultipleRecords<Request> multipleRequests) {
+    Collection<Request> requests = multipleRequests.getRecords();
+    final List<String> instanceIdsToFetch = requests.stream()
+      .filter(Objects::nonNull)
+      .map(Request::getInstanceId)
+      .filter(Objects::nonNull)
+      .distinct()
+      .collect(Collectors.toList());
+
+    if (instanceIdsToFetch.isEmpty()) {
+      log.info("No instance ids to query");
+      return completedFuture(succeeded(multipleRequests));
+    }
+
+    final FindWithMultipleCqlIndexValues<Instance> fetcher = createInstancesFetcher();
+
+    return fetcher.findByIds(instanceIdsToFetch)
+      .thenApply(multipleInstancesResult -> multipleInstancesResult.next(
+        multipleInstances -> {
+          List<Request> newRequestList = new ArrayList<>();
+          Collection<Instance> instanceCollection = multipleInstances.getRecords();
+          for (Request request : requests) {
+            Request newRequest = null;
+            boolean foundInstance = false;
+            for (Instance instance : instanceCollection) {
+              if (request.getInstanceId() != null && request.getInstanceId().equals(instance.getId())) {
+                newRequest = request.withInstance(instance);
+                foundInstance = true;
+                break;
+              }
+            }
+            if (!foundInstance) {
+              log.info("No instance (out of {}) found for request {} (instanceId {})",
+                instanceCollection.size(), request.getId(), request.getInstanceId());
+              newRequest = request;
+            }
+            newRequestList.add(newRequest);
+          }
+          return succeeded(new MultipleRecords<>(newRequestList,
+            multipleRequests.getTotalRecords()));
+        }
+      ));
+  }
+
+  private FindWithMultipleCqlIndexValues<Instance> createInstancesFetcher() {
+    return findWithMultipleCqlIndexValues(instancesClient, "instances", Instance::from);
   }
 }
