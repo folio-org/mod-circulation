0fb2269042dbe5e595f984391d19cbe08b5f891e
Alexander Kurash <Oleksandr_Kurash@epam.com>
2022-01-04 22:12:07 +0200

    CIRC-1338 Add a placeholder for in-transit-report

---
+++ src/main/java/org/folio/circulation/services/ItemsInTransitReportService.java
@@ -0,0 +1,106 @@
+package org.folio.circulation.services;
+
+import static java.util.concurrent.CompletableFuture.completedFuture;
+import static org.folio.circulation.support.results.Result.succeeded;
+
+import java.util.concurrent.CompletableFuture;
+
+import org.folio.circulation.infrastructure.storage.ServicePointRepository;
+import org.folio.circulation.infrastructure.storage.inventory.ItemReportRepository;
+import org.folio.circulation.infrastructure.storage.inventory.ItemRepository;
+import org.folio.circulation.infrastructure.storage.users.PatronGroupRepository;
+import org.folio.circulation.infrastructure.storage.users.UserRepository;
+import org.folio.circulation.services.support.ItemsInTransitReportContext;
+import org.folio.circulation.support.GetManyRecordsClient;
+import org.folio.circulation.support.results.Result;
+
+import io.vertx.core.json.JsonObject;
+import lombok.AllArgsConstructor;
+
+/**
+ * Placeholder class which will replace items-in-transit report logic when implemented
+ */
+@AllArgsConstructor
+public class ItemsInTransitReportService {
+  private ItemReportRepository itemReportRepository;
+  private GetManyRecordsClient loansStorageClient;
+  private ServicePointRepository servicePointRepository;
+  private GetManyRecordsClient requestsStorageClient;
+  private ItemRepository itemRepository;
+  private UserRepository userRepository;
+  private PatronGroupRepository patronGroupRepository;
+
+  public CompletableFuture<Result<JsonObject>> buildReport() {
+    return completedFuture(succeeded(new ItemsInTransitReportContext()))
+      .thenCompose(this::fetchItems)
+      .thenCompose(this::fetchHoldingsRecords)
+      .thenCompose(this::fetchInstances)
+      .thenCompose(this::fetchLocations)
+      .thenCompose(this::fetchMaterialTypes)
+      .thenCompose(this::fetchLoanTypes)
+      .thenCompose(this::fetchLoans)
+      .thenCompose(this::fetchRequests)
+      .thenCompose(this::fetchServicePoints)
+      .thenApply(this::mapToJsonObject);
+  }
+
+  private Result<JsonObject> mapToJsonObject(Result<ItemsInTransitReportContext> context) {
+
+    return succeeded(new JsonObject());
+  }
+
+  private CompletableFuture<Result<ItemsInTransitReportContext>> fetchItems(
+    Result<ItemsInTransitReportContext> context) {
+
+    return completedFuture(context);
+  }
+
+  private CompletableFuture<Result<ItemsInTransitReportContext>> fetchHoldingsRecords(
+    Result<ItemsInTransitReportContext> context) {
+
+    return completedFuture(context);
+  }
+
+  private CompletableFuture<Result<ItemsInTransitReportContext>> fetchInstances(
+    Result<ItemsInTransitReportContext> context) {
+
+    return completedFuture(context);
+  }
+
+  private CompletableFuture<Result<ItemsInTransitReportContext>> fetchLocations(
+    Result<ItemsInTransitReportContext> context) {
+
+    return completedFuture(context);
+  }
+
+  private CompletableFuture<Result<ItemsInTransitReportContext>> fetchMaterialTypes(
+    Result<ItemsInTransitReportContext> context) {
+
+    return completedFuture(context);
+  }
+
+  private CompletableFuture<Result<ItemsInTransitReportContext>> fetchLoanTypes(
+    Result<ItemsInTransitReportContext> context) {
+
+    return completedFuture(context);
+  }
+
+  private CompletableFuture<Result<ItemsInTransitReportContext>> fetchLoans(
+    Result<ItemsInTransitReportContext> context) {
+
+    return completedFuture(context);
+  }
+
+  private CompletableFuture<Result<ItemsInTransitReportContext>> fetchRequests(
+    Result<ItemsInTransitReportContext> context) {
+
+    return completedFuture(context);
+  }
+
+  // Needs to fetch all service points for items, loans and requests
+  private CompletableFuture<Result<ItemsInTransitReportContext>> fetchServicePoints(
+    Result<ItemsInTransitReportContext> context) {
+
+    return completedFuture(context);
+  }
+}
