d96114ffc5d058d69c963097e347978739ffea05
Alexander Kurash <alexander.kurash@gmail.com>
2022-01-12 12:34:37 +0200

    CIRC-1339 Items in transit report performance - fetch items (#1029)

--- src/test/java/org/folio/circulation/services/ItemsInTransitReportServiceTest.java
+++ src/test/java/org/folio/circulation/services/ItemsInTransitReportServiceTest.java
@@ -1,11 +1,23 @@
 package org.folio.circulation.services;
 
+import static java.util.concurrent.CompletableFuture.completedFuture;
+import static org.folio.circulation.support.results.Result.succeeded;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
+import org.folio.circulation.domain.Item;
+import org.folio.circulation.domain.ItemsReportFetcher;
+import org.folio.circulation.domain.MultipleRecords;
+import org.folio.circulation.infrastructure.storage.inventory.ItemReportRepository;
 import org.folio.circulation.support.results.Result;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import io.vertx.core.json.JsonObject;
 
@@ -14,11 +26,25 @@ import io.vertx.core.json.JsonObject;
  * Once the feature is implemented, ItemsInTransitReportService will replace the existing
  * items-in-transit report logic and will be covered by existing API tests.
  */
+@ExtendWith(MockitoExtension.class)
 class ItemsInTransitReportServiceTest {
+  @Mock
+  ItemReportRepository itemReportRepository;
+
+  @Mock
+  ItemsReportFetcher itemsReportFetcher;
+
   @Test
   void itemsInTransitReportServiceTest() {
-    ItemsInTransitReportService service = new ItemsInTransitReportService(null, null, null, null,
-      null, null, null);
+    when(itemReportRepository.getAllItemsByField(any(), any()))
+      .thenReturn(completedFuture(succeeded(itemsReportFetcher)));
+
+    when(itemsReportFetcher.getResultListOfItems())
+      .thenReturn(List.of(succeeded(new MultipleRecords<>(
+        List.of(Item.from(new JsonObject())), 1))));
+
+    ItemsInTransitReportService service = new ItemsInTransitReportService(itemReportRepository,
+      null, null, null, null, null, null);
     CompletableFuture<Result<JsonObject>> report = service.buildReport();
     assertNotNull(report);
   }
