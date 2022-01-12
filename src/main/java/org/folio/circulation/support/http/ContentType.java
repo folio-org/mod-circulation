3ec768cfc861ffba0c689faf624c007282a3805f
Julian Ladisch <julianladisch@users.noreply.github.com>
2022-01-12 18:16:34 +0100

    CIRC-1414: Remove org.apache.httpcomponents:httpclient-*
    pom.xml adds org.apache.httpcomponents:httpclient-osgi and org.apache.httpcomponents:httpclient-cache as runtime dependencies.
    
    They result in 1.2 MB of code in the final mod-circulation.jar because of indirect dependencies:
    
    * org.apache.httpcomponents:fluent-hc:jar:4.5.8
    * org.apache.httpcomponents:httpclient-cache:jar:4.5.8
    * org.apache.httpcomponents:httpclient-osgi:jar:4.5.8
    * org.apache.httpcomponents:httpclient:jar:4.5.8
    * org.apache.httpcomponents:httpcore:jar:4.4.11
    * org.apache.httpcomponents:httpmime:jar:4.5.8
    
    These libraries are deprecated: "Users of HttpCore 4.x are strongly encouraged to migrate to HttpCore 5.x" https://hc.apache.org/status.html
    
    The libraries are needed for 6 String constants only.
    
    To avoid bloating the final jar these 6 String contstants are taken from other sources and the org.apache.httpcomponents are removed.

---
+++ src/main/java/org/folio/circulation/support/http/ContentType.java
@@ -0,0 +1,10 @@
+package org.folio.circulation.support.http;
+
+/**
+ * Strings for HTTP content-type header.
+ */
+public class ContentType {
+  public static final String CONTENT_TYPE = "Content-Type";
+  public static final String APPLICATION_JSON = "application/json; charset=UTF-8";
+  public static final String TEXT_PLAIN = "text/plain; ISO_8859_1";
+}
