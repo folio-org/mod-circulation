ae66c315d488764a8637d341a22fe4d82fe049c1
Julian Ladisch <julianladisch@users.noreply.github.com>
2022-01-13 19:58:01 +0100

    Fix Sonar utility class warnings

--- src/main/java/org/folio/circulation/support/http/ContentType.java
+++ src/main/java/org/folio/circulation/support/http/ContentType.java
@@ -3,8 +3,12 @@ package org.folio.circulation.support.http;
 /**
  * Strings for HTTP content-type header.
  */
-public class ContentType {
+public final class ContentType {
   public static final String CONTENT_TYPE = "Content-Type";
   public static final String APPLICATION_JSON = "application/json; charset=UTF-8";
   public static final String TEXT_PLAIN = "text/plain; ISO_8859_1";
+
+  private ContentType() {
+    throw new UnsupportedOperationException("Can't instantiate utility class");
+  }
 }
