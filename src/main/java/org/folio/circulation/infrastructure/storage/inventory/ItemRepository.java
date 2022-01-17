d5147e2503196ef9ee448ef09b300c94877a7f3c
Yusuf Murodov <yusuf_murodov@epam.com>
2022-01-17 18:44:25 +0500

    renamed holding const variable

--- src/main/java/org/folio/circulation/infrastructure/storage/inventory/ItemRepository.java
+++ src/main/java/org/folio/circulation/infrastructure/storage/inventory/ItemRepository.java
@@ -72,7 +72,7 @@ public class ItemRepository {
   private final boolean fetchLoanType;
 
   private static final String ITEMS_COLLECTION_PROPERTY_NAME = "items";
-  private static final String HOLDINGS_RECORDS_PROPERTY_NAME = "holdingsRecords";
+  private static final String HOLDINGS_COLLECTION_PROPERTY_NAME = "holdingsRecords";
 
   public ItemRepository(org.folio.circulation.support.Clients clients,
     boolean fetchLocation, boolean fetchMaterialType, boolean fetchLoanType) {
@@ -285,7 +285,7 @@ public class ItemRepository {
         .collect(Collectors.toList());
 
       final var fetcher
-        = findWithMultipleCqlIndexValues(holdingsClient, HOLDINGS_RECORDS_PROPERTY_NAME,
+        = findWithMultipleCqlIndexValues(holdingsClient, HOLDINGS_COLLECTION_PROPERTY_NAME,
         identity());
 
       final var mapper = new HoldingsMapper();
@@ -405,7 +405,7 @@ public class ItemRepository {
     Collection<String> ids) {
 
     return findWithMultipleCqlIndexValues(holdingsClient,
-      HOLDINGS_RECORDS_PROPERTY_NAME, identity())
+      HOLDINGS_COLLECTION_PROPERTY_NAME, identity())
       .findByIds(ids)
       .thenApply(r -> r.map(
         multipleRecords -> multipleRecords.mapRecords(new HoldingsMapper()::toDomain)));
