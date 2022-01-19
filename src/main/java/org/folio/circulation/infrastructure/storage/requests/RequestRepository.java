2ef3be9e8f46afd6b75983b4d5b9ac34f7fdb4a0
OleksandrVidinieiev <56632770+OleksandrVidinieiev@users.noreply.github.com>
2022-01-19 15:27:08 +0200

    [CIRC-1347] Integrate ItemsInTransitReportService into ItemsInTransitResourse (#1042)
    * [CIRC-1347] Integrate ItemsInTransitReportService into ItemsInTransitResource
    
    * [CIRC-1347] Fetch patron groups
    
    * CIRC-1347 Fix implementation and tests
    
    * CIRC-1347 Sort entries by check-in service point
    
    * CIRC-1347 Exctract getInstanceId method
    
    * CIRC-1347 Refactor report builder
    
    * CIRC-1347 Refactor context
    
    Co-authored-by: Alexander Kurash <Oleksandr_Kurash@epam.com>
    Co-authored-by: Alexander Kurash <alexander.kurash@gmail.com>

--- src/main/java/org/folio/circulation/infrastructure/storage/requests/RequestRepository.java
+++ src/main/java/org/folio/circulation/infrastructure/storage/requests/RequestRepository.java
@@ -2,6 +2,7 @@ package org.folio.circulation.infrastructure.storage.requests;
 
 import static java.util.Objects.isNull;
 import static org.folio.circulation.domain.RequestStatus.openStates;
+import static org.folio.circulation.support.CqlSortBy.ascending;
 import static org.folio.circulation.support.fetching.RecordFetching.findWithMultipleCqlIndexValues;
 import static org.folio.circulation.support.http.client.CqlQuery.exactMatchAny;
 import static org.folio.circulation.support.results.Result.failed;
@@ -51,6 +52,20 @@ public class RequestRepository {
   private final LoanRepository loanRepository;
   private final ServicePointRepository servicePointRepository;
   private final PatronGroupRepository patronGroupRepository;
+
+  /**
+   * Public constructor to avoid creating repositories twice
+   */
+  public RequestRepository(org.folio.circulation.support.Clients clients,
+    ItemRepository itemRepository, UserRepository userRepository, LoanRepository loanRepository,
+    ServicePointRepository servicePointRepository, PatronGroupRepository patronGroupRepository) {
+
+    this(new Clients(clients.requestsStorage(), clients.requestsBatchStorage(),
+        clients.cancellationReasonStorage()), itemRepository,
+      userRepository, loanRepository,
+      servicePointRepository,
+      patronGroupRepository);
+  }
 
   private RequestRepository(Clients clients, ItemRepository itemRepository,
     UserRepository userRepository, LoanRepository loanRepository,
@@ -149,8 +164,11 @@ public class RequestRepository {
   public CompletableFuture<Result<MultipleRecords<Request>>> findOpenRequestsByItemIds(
     Collection<String> itemIds) {
 
+    Result<CqlQuery> query = exactMatchAny("status", openStates())
+      .map(q -> q.sortBy(ascending("position")));
+
     return findWithMultipleCqlIndexValues(requestsStorageClient, "requests", Request::from)
-      .findByIdIndexAndQuery(itemIds, "itemId", exactMatchAny("status", openStates()));
+      .findByIdIndexAndQuery(itemIds, "itemId", query);
   }
 
   public CompletableFuture<Result<Request>> update(Request request) {
