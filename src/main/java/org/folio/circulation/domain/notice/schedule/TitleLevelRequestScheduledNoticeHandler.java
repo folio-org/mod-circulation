de0d1b768fd78eaf67ad8e0059e23643f6e7bea8
Marc Johnson <marc@k-int.co.uk>
2022-03-02 12:02:34 +0200

    Remove item repository dependency in scheduled notice handlers CIRC-1418
    (cherry picked from commit bc65808bf79990cdcdcb4097754d24aa040ca47f)

--- src/main/java/org/folio/circulation/domain/notice/schedule/TitleLevelRequestScheduledNoticeHandler.java
+++ src/main/java/org/folio/circulation/domain/notice/schedule/TitleLevelRequestScheduledNoticeHandler.java
@@ -5,13 +5,17 @@ import static org.folio.circulation.support.results.ResultBinding.mapResult;
 
 import java.util.concurrent.CompletableFuture;
 
+import org.folio.circulation.infrastructure.storage.loans.LoanRepository;
+import org.folio.circulation.infrastructure.storage.requests.RequestRepository;
 import org.folio.circulation.support.Clients;
 import org.folio.circulation.support.results.Result;
 
 public class TitleLevelRequestScheduledNoticeHandler extends RequestScheduledNoticeHandler {
 
-  public TitleLevelRequestScheduledNoticeHandler(Clients clients) {
-    super(clients);
+  public TitleLevelRequestScheduledNoticeHandler(Clients clients,
+    RequestRepository requestRepository, LoanRepository loanRepository) {
+
+    super(clients, loanRepository, requestRepository);
   }
 
   @Override
