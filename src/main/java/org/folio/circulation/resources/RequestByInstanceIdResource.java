e39f4b6d08aef40f291945225529886e652e3bdf
Mykyta_Varenyk <Mykyta_Varenyk@epam.com>
2022-04-08 17:21:48 +0300

    Refactor

--- src/main/java/org/folio/circulation/resources/RequestByInstanceIdResource.java
+++ src/main/java/org/folio/circulation/resources/RequestByInstanceIdResource.java
@@ -269,12 +269,11 @@ public class RequestByInstanceIdResource extends Resource {
     JsonObject requestBody, ItemByInstanceIdFinder itemFinder,
     LoanRepository loanRepository, RequestQueueRepository requestQueueRepository) {
 
-    return succeeded(requestBody.put(REQUEST_LEVEL, "Item"))
-      .after(body -> RequestByInstanceIdRequest.from(body)
+    return RequestByInstanceIdRequest.from(requestBody.put(REQUEST_LEVEL, RequestLevel.ITEM.getValue()))
       .map(InstanceRequestRelatedRecords::new)
       .after(instanceRequest -> getPotentialItems(itemFinder, instanceRequest,
         loanRepository, requestQueueRepository))
-      .thenApply( r -> r.next(RequestByInstanceIdResource::instanceToItemRequests)));
+      .thenApply( r -> r.next(RequestByInstanceIdResource::instanceToItemRequests));
   }
 
   private CompletableFuture<Result<RequestAndRelatedRecords>> placeRequests(
