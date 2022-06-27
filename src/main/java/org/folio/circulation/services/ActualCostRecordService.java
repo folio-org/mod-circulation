e237ffdfb1a5a1a922e34f250297c08c61d387ae
alexanderkurash <alexander.kurash@gmail.com>
2022-06-27 23:17:23 +0300

    CIRC-1554 Remove unused fields

--- src/main/java/org/folio/circulation/services/ActualCostRecordService.java
+++ src/main/java/org/folio/circulation/services/ActualCostRecordService.java
@@ -6,21 +6,10 @@ import static org.folio.circulation.support.results.Result.succeeded;
 
 import java.util.concurrent.CompletableFuture;
 
-import org.folio.circulation.infrastructure.storage.ActualCostRecordRepository;
-import org.folio.circulation.infrastructure.storage.inventory.LocationRepository;
 import org.folio.circulation.services.agedtolost.LoanToChargeFees;
 import org.folio.circulation.support.results.Result;
 
 public class ActualCostRecordService {
-  private final ActualCostRecordRepository actualCostRecordRepository;
-  private final LocationRepository locationRepository;
-
-  public ActualCostRecordService(ActualCostRecordRepository actualCostRecordRepository,
-    LocationRepository locationRepository) {
-    this.actualCostRecordRepository = actualCostRecordRepository;
-    this.locationRepository = locationRepository;
-  }
-
   public CompletableFuture<Result<ReferenceDataContext>> createIfNecessaryForDeclaredLostItem(
     ReferenceDataContext referenceDataContext) {
 
