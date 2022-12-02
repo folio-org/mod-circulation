c4acfd8d4182de156bf71d1e7fb755ff61282389
Arin_Suryavanshi <arin_suryavanshi@epam.com>
2022-12-02 15:25:29 +0530

    [CIRC-1658]: unit tests added for holdShelfExpiryDate

---
+++ src/test/java/org/folio/circulation/domain/policy/library/DetermineClosedLibraryStrategyForHoldShelfExpirationDateTests.java
@@ -0,0 +1,48 @@
+package org.folio.circulation.domain.policy.library;
+
+import api.support.builders.ServicePointBuilder;
+import io.vertx.core.json.JsonObject;
+import org.folio.circulation.domain.TimePeriod;
+import org.folio.circulation.domain.policy.ExpirationDateManagement;
+import org.junit.Assert;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.time.Duration;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.Boolean.TRUE;
+import static java.time.ZoneOffset.UTC;
+
+public class DetermineClosedLibraryStrategyForHoldShelfExpirationDateTests {
+  private ClosedLibraryStrategy closedLibraryStrategy;
+
+  @ParameterizedTest
+  @MethodSource("testDetermineClosedLibraryStrategyHoldShelfParameters")
+  void testDetermineClosedLibraryStrategyForHoldShelf(ExpirationDateManagement expirationDateManagement, Class<?> expectedClass) {
+    JsonObject representation = new ServicePointBuilder("Circ Desk 2", "cd2",
+      "Circulation Desk -- Back Entrance").withPickupLocation(TRUE)
+      .withHoldShelfExpriyPeriod(6, "Days")
+      .withHoldShelfExpirationDateManagement(expirationDateManagement.getValue())
+      .create();
+
+    ZonedDateTime startDate = ZonedDateTime.of(2019, 1, 1, 0, 0, 0, 0, UTC);
+    Duration duration = new TimePeriod(6, "Days").getIntervalDuration();
+    closedLibraryStrategy = ClosedLibraryStrategyUtils
+      .determineClosedLibraryStrategyForHoldShelfExpirationDate(expirationDateManagement, startDate, UTC, duration);
+
+    Assert.assertEquals(expectedClass, closedLibraryStrategy.getClass());
+  }
+
+  private static List<Object[]> testDetermineClosedLibraryStrategyHoldShelfParameters() {
+    List<Object[]> data = new ArrayList<>();
+    data.add(new Object[]{ExpirationDateManagement.KEEP_THE_CURRENT_DUE_DATE, KeepCurrentDateStrategy.class});
+    data.add(new Object[]{ExpirationDateManagement.MOVE_TO_THE_END_OF_THE_PREVIOUS_OPEN_DAY, EndOfPreviousDayStrategy.class});
+    data.add(new Object[]{ExpirationDateManagement.MOVE_TO_THE_END_OF_THE_NEXT_OPEN_DAY, EndOfNextOpenDayStrategy.class});
+    data.add(new Object[]{ExpirationDateManagement.MOVE_TO_END_OF_CURRENT_SERVICE_POINT_HOURS, EndOfCurrentHoursStrategy.class});
+    data.add(new Object[]{ExpirationDateManagement.MOVE_TO_BEGINNING_OF_NEXT_OPEN_SERVICE_POINT_HOURS, BeginningOfNextOpenHoursStrategy.class});
+    return data;
+  }
+}
