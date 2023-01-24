191dcab4aec0f76735bfbf0a09aad984cb71fb56
Gurleen Kaur <gurleen_kaur1@epam.com>
2023-01-24 14:00:51 +0530

    [CIRC-1578] - Code cleanup.

--- src/main/java/org/folio/circulation/domain/representations/CheckInByBarcodeResponse.java
+++ src/main/java/org/folio/circulation/domain/representations/CheckInByBarcodeResponse.java
@@ -3,17 +3,10 @@ package org.folio.circulation.domain.representations;
 import static org.folio.circulation.domain.notice.TemplateContextUtil.createCheckInContext;
 import static org.folio.circulation.support.json.JsonPropertyWriter.write;
 import static org.folio.circulation.support.http.server.JsonHttpResponse.ok;
-
 import org.folio.circulation.domain.CheckInContext;
 import org.folio.circulation.domain.LoanRepresentation;
-import org.folio.circulation.domain.Request;
-import org.folio.circulation.domain.User;
-import org.folio.circulation.infrastructure.storage.users.PatronGroupRepository;
 import org.folio.circulation.support.http.server.HttpResponse;
-
 import io.vertx.core.json.JsonObject;
-
-import java.util.concurrent.ExecutionException;
 
 public class CheckInByBarcodeResponse {
   private final CheckInContext context;
