7874b4ea5d75967ac7e556b8346cdfd49fbd8739
Julian Ladisch <julianladisch@users.noreply.github.com>
2023-02-16 22:18:02 +0100

    CIRC-1731 Use public AnonymizeLoansRepresentation::from method for test

--- src/test/java/org/folio/circulation/domain/representations/anonymization/AnonymizeLoansRepresentationTest.java
+++ src/test/java/org/folio/circulation/domain/representations/anonymization/AnonymizeLoansRepresentationTest.java
@@ -2,37 +2,44 @@ package org.folio.circulation.domain.representations.anonymization;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.oneOf;
 
-import java.util.Collection;
-import java.util.List;
+import io.vertx.core.json.JsonArray;
+import java.util.Collections;
 import java.util.Map;
+import java.util.Set;
+import org.folio.circulation.domain.anonymization.LoanAnonymizationRecords;
+import org.folio.circulation.support.results.Result;
 import org.junit.jupiter.api.Test;
 
 class AnonymizeLoansRepresentationTest {
 
   @Test
-  void mapToErrors() {
-    Map<String, Collection<String>> multiMap = Map.of(
-        "foo", List.of("foo1", "foo2"),
-        "bar", List.of("bar1"));
-    List<Error> errors = AnonymizeLoansRepresentation.mapToErrors(multiMap);
-    assertThat(errors.size(), is(2));
-    Error foo = errors.get(0);
-    Error bar = errors.get(1);
-    // accept in any order
-    if ("foo".equals(bar.getMessage())) {
-      foo = errors.get(1);
-      bar = errors.get(0);
-    }
-    assertThat(foo.getMessage(), is("foo"));
-    var parameters = foo.getParameters();
-    assertThat(parameters.get(0).getKey(), is("loanIds"));
-    assertThat(parameters.get(0).getValue(), is("[\"foo1\",\"foo2\"]"));
+  void errors() {
+    Map<String, Set<String>> errorMap = Map.of(
+        "foo", Set.of("foo1", "foo2"),
+        "bar", Set.of("bar1"));
+    Result<LoanAnonymizationRecords> result = Result.succeeded(
+        new LoanAnonymizationRecords().withNotAnonymizedLoans(errorMap));
 
-    assertThat(bar.getMessage(), is("bar"));
-    parameters = bar.getParameters();
-    assertThat(parameters.get(0).getKey(), is("loanIds"));
-    assertThat(parameters.get(0).getValue(), is("[\"bar1\"]"));
+    JsonArray errors = AnonymizeLoansRepresentation.from(result).value().getJsonArray("errors");
+
+    assertThat(errors.size(), is(2));
+    // accept in any order
+    if ("bar".equals(errors.getJsonObject(0).getString("message"))) {
+      Collections.swap(errors.getList(), 0, 1);
+    }
+
+    assertThat(errors.getJsonObject(0).getString("message"), is("foo"));
+    var parameters = errors.getJsonObject(0).getJsonArray("parameters");
+    assertThat(parameters.getJsonObject(0).getString("key"), is("loanIds"));
+    assertThat(parameters.getJsonObject(0).getString("value"),
+        is(oneOf("[\"foo1\",\"foo2\"]", "[\"foo2\",\"foo1\"]")));
+
+    assertThat(errors.getJsonObject(1).getString("message"), is("bar"));
+    parameters = errors.getJsonObject(1).getJsonArray("parameters");
+    assertThat(parameters.getJsonObject(0).getString("key"), is("loanIds"));
+    assertThat(parameters.getJsonObject(0).getString("value"), is("[\"bar1\"]"));
   }
 
 }
