3d5944697271e85f1240574ffbe9894eb49fd0a6
Vignesh-Kalyanasundaram <vignesh_kalyansundaram@epam.com>
2023-04-19 13:16:40 +0530

    CIRC-1590 Adding Departments in the requester object of PickslipResource

--- src/main/java/org/folio/circulation/infrastructure/storage/users/DepartmentRepository.java
+++ src/main/java/org/folio/circulation/infrastructure/storage/users/DepartmentRepository.java
@@ -3,6 +3,8 @@ package org.folio.circulation.infrastructure.storage.users;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.folio.circulation.domain.Department;
+import org.folio.circulation.domain.MultipleRecords;
+import org.folio.circulation.domain.Request;
 import org.folio.circulation.domain.User;
 import org.folio.circulation.support.Clients;
 import org.folio.circulation.support.CollectionResourceClient;
@@ -12,6 +14,8 @@ import org.folio.circulation.support.results.Result;
 import java.lang.invoke.MethodHandles;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
@@ -32,6 +36,35 @@ public class DepartmentRepository {
       return completedFuture(succeeded(null));
     }
     return user.combineAfter(this::findDepartments, User::withDepartments);
+  }
+
+  public CompletableFuture<Result<MultipleRecords<Request>>> findDepartmentsForRequestUsers(MultipleRecords<Request> multipleRequests) {
+    log.info("findDepartmentsForRequestUsers:: multipleRequests size {} ", multipleRequests.getTotalRecords());
+    List<String> departmentIds = multipleRequests.getRecords().stream()
+      .filter(req -> req.getRequester() != null)
+      .map(req -> req.getRequester().getDepartments().stream()
+        .filter(Objects::nonNull)
+        .map(String.class::cast)
+        .collect(Collectors.toList()))
+      .flatMap(List::stream)
+      .distinct()
+      .collect(Collectors.toList());
+    return createDepartmentsFetcher()
+      .findByIds(departmentIds)
+      .thenApply(r -> r.next(dep -> matchDepartmentToRequest(dep, multipleRequests)));
+  }
+
+  private Result<MultipleRecords<Request>> matchDepartmentToRequest(
+    MultipleRecords<Department> departments, MultipleRecords<Request> requests){
+    Map<String, Department> departmentsMap = departments.toMap(Department::getId);
+    return Result.succeeded(requests.mapRecords(req -> req.withRequester(
+      req.getRequester().withDepartments(req.getRequester().getDepartmentIds()
+        .stream()
+        .map(String.class::cast)
+        .collect(Collectors.toList())
+        .stream().map(departmentsMap::get)
+        .filter(Objects::nonNull)
+        .collect(Collectors.toList())))));
   }
 
   private CompletableFuture<Result<List<Department>>> findDepartments(User user) {
