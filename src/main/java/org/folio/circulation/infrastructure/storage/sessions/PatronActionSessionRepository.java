a580d4519e61579be2ab31ab03b08f54872492a5
Oleksandr Vidinieiev <Oleksandr_Vidinieiev@epam.com>
2023-05-13 22:02:03 +0300

    CIRC-1725 Introduce OverdueFineScheduledNoticeProcessingResource

--- src/main/java/org/folio/circulation/infrastructure/storage/sessions/PatronActionSessionRepository.java
+++ src/main/java/org/folio/circulation/infrastructure/storage/sessions/PatronActionSessionRepository.java
@@ -7,9 +7,11 @@ import static org.folio.circulation.domain.notice.session.PatronActionSessionPro
 import static org.folio.circulation.domain.notice.session.PatronActionSessionProperties.LOAN_ID;
 import static org.folio.circulation.domain.notice.session.PatronActionSessionProperties.PATRON_ACTION_SESSIONS;
 import static org.folio.circulation.domain.notice.session.PatronActionSessionProperties.PATRON_ID;
+import static org.folio.circulation.domain.notice.session.PatronActionSessionProperties.SESSION_ID;
 import static org.folio.circulation.support.fetching.RecordFetching.findWithMultipleCqlIndexValues;
 import static org.folio.circulation.support.http.ResponseMapping.flatMapUsingJson;
 import static org.folio.circulation.support.http.client.CqlQuery.exactMatch;
+import static org.folio.circulation.support.http.client.CqlQuery.exactMatchAny;
 import static org.folio.circulation.support.http.client.CqlQuery.noQuery;
 import static org.folio.circulation.support.json.JsonPropertyFetcher.getProperty;
 import static org.folio.circulation.support.json.JsonPropertyWriter.write;
@@ -22,6 +24,7 @@ import static org.folio.circulation.support.results.ResultBinding.mapResult;
 
 import java.lang.invoke.MethodHandles;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -51,6 +54,7 @@ import org.folio.circulation.infrastructure.storage.users.UserRepository;
 import org.folio.circulation.support.Clients;
 import org.folio.circulation.support.CollectionResourceClient;
 import org.folio.circulation.support.ForwardOnFailure;
+import org.folio.circulation.support.fetching.CqlQueryFinder;
 import org.folio.circulation.support.http.client.CqlQuery;
 import org.folio.circulation.support.http.client.PageLimit;
 import org.folio.circulation.support.http.client.ResponseInterpreter;
@@ -148,6 +152,15 @@ public class PatronActionSessionRepository {
       .after(query -> findBy(query, pageLimit))
       .thenCompose(r -> r.combineAfter(() -> userRepository.getUser(patronId), this::setUserForLoans))
       .thenApply(mapResult(this::toList));
+  }
+
+  public CompletableFuture<Result<Collection<PatronSessionRecord>>> findPatronActionSessions(
+    Collection<String> sessionIds, PageLimit pageLimit) {
+
+      return new CqlQueryFinder<>(patronActionSessionsStorageClient, "patronActionSessions",
+        PatronSessionRecord::from)
+        .findByQuery(exactMatchAny(SESSION_ID, sessionIds), pageLimit)
+        .thenApply(r -> r.map(MultipleRecords::getRecords));
   }
 
   public CompletableFuture<Result<List<PatronSessionRecord>>> findPatronActionSessions(
---
+++ src/main/java/org/folio/circulation/resources/OverdueFineScheduledNoticeProcessingResource.java
@@ -0,0 +1,80 @@
+package org.folio.circulation.resources;
+
+import static org.folio.circulation.domain.notice.schedule.TriggeringEvent.OVERDUE_FINE_RENEWED;
+import static org.folio.circulation.domain.notice.schedule.TriggeringEvent.OVERDUE_FINE_RETURNED;
+import static org.folio.circulation.support.http.client.PageLimit.oneThousand;
+
+import java.util.Collection;
+import java.util.EnumSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+
+import org.folio.circulation.domain.MultipleRecords;
+import org.folio.circulation.domain.notice.schedule.GroupedFeeFineScheduledNoticeHandler;
+import org.folio.circulation.domain.notice.schedule.GroupedScheduledNoticeHandler;
+import org.folio.circulation.domain.notice.schedule.ScheduledNotice;
+import org.folio.circulation.domain.notice.session.PatronSessionRecord;
+import org.folio.circulation.infrastructure.storage.ConfigurationRepository;
+import org.folio.circulation.infrastructure.storage.loans.LoanRepository;
+import org.folio.circulation.infrastructure.storage.notices.ScheduledNoticesRepository;
+import org.folio.circulation.infrastructure.storage.sessions.PatronActionSessionRepository;
+import org.folio.circulation.support.Clients;
+import org.folio.circulation.support.http.client.PageLimit;
+import org.folio.circulation.support.results.Result;
+
+import io.vertx.core.http.HttpClient;
+
+public class OverdueFineScheduledNoticeProcessingResource
+  extends GroupingScheduledNoticeProcessingResource {
+
+  public OverdueFineScheduledNoticeProcessingResource(HttpClient client) {
+    super(client, "/circulation/overdue-fine-scheduled-notices-processing",
+      EnumSet.of(OVERDUE_FINE_RETURNED, OVERDUE_FINE_RENEWED), true);
+  }
+
+  @Override
+  protected GroupedScheduledNoticeHandler getHandler(Clients clients, LoanRepository loanRepository) {
+    return new GroupedFeeFineScheduledNoticeHandler(clients, loanRepository);
+  }
+
+  @Override
+  protected CompletableFuture<Result<MultipleRecords<ScheduledNotice>>> findNoticesToSend(
+    ConfigurationRepository configurationRepository,
+    ScheduledNoticesRepository scheduledNoticesRepository,
+    PatronActionSessionRepository patronActionSessionRepository, PageLimit pageLimit) {
+
+    return super.findNoticesToSend(configurationRepository, scheduledNoticesRepository,
+        patronActionSessionRepository, pageLimit)
+      .thenCompose(r -> r.after(notices -> filterNotices(notices, patronActionSessionRepository)));
+  }
+
+  private CompletableFuture<Result<MultipleRecords<ScheduledNotice>>> filterNotices(
+    MultipleRecords<ScheduledNotice> notices,
+    PatronActionSessionRepository patronActionSessionRepository) {
+
+    Set<String> sessionIdsFromNotices = notices.getRecords()
+      .stream()
+      .map(ScheduledNotice::getSessionId)
+      .filter(Objects::nonNull)
+      .collect(Collectors.toSet());
+
+    return patronActionSessionRepository.findPatronActionSessions(sessionIdsFromNotices, oneThousand())
+      .thenApply(r -> r.map(sessions -> skipNoticesWithOpenPatronActionSessions(notices, sessions)));
+  }
+
+  private static MultipleRecords<ScheduledNotice> skipNoticesWithOpenPatronActionSessions(
+    MultipleRecords<ScheduledNotice> notices, Collection<PatronSessionRecord> sessions) {
+
+    Set<String> openSessionIds = sessions.stream()
+      .map(PatronSessionRecord::getSessionId)
+      .filter(Objects::nonNull)
+      .map(UUID::toString)
+      .collect(Collectors.toSet());
+
+    return notices.filter(notice -> !openSessionIds.contains(notice.getSessionId()));
+  }
+
+}
