443689cf53799affae5bc81933389a89290bbfb0
Oleksandr Vidinieiev <oleksandr_vidinieiev@epam.com>
2023-09-12 15:15:29 +0300

    CIRC-1709 Populate logging context

--- src/main/java/org/folio/circulation/support/logging/LogHelper.java
+++ src/main/java/org/folio/circulation/support/logging/LogHelper.java
@@ -4,19 +4,24 @@ import static java.util.stream.Collectors.toList;
 import static org.folio.circulation.support.http.OkapiHeader.REQUEST_ID;
 import static org.folio.circulation.support.http.OkapiHeader.TENANT;
 
-import io.vertx.ext.web.RoutingContext;
 import java.lang.invoke.MethodHandles;
 import java.util.Collection;
 import java.util.function.Function;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.folio.circulation.domain.MultipleRecords;
 import org.folio.circulation.support.http.client.Response;
 import org.folio.circulation.support.results.Result;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.folio.okapi.common.logging.FolioLoggingContext;
+import org.folio.rest.RestVerticle;
+
+import io.vertx.core.http.HttpServerRequest;
+import io.vertx.ext.web.RoutingContext;
 
 public class LogHelper {
   private static final Logger log = LogManager.getLogger(MethodHandles.lookup().lookupClass());
+  public static final String MODULE_NAME = "mod-circulation";
 
   private LogHelper() {
     throw new UnsupportedOperationException("Do not instantiate");
@@ -27,12 +32,16 @@ public class LogHelper {
   }
 
   public static void logRequest(RoutingContext rc, Logger logger) {
+    final HttpServerRequest request = rc.request();
+    populateLoggingContext(request);
+
     if (logger.isInfoEnabled()) {
       logger.info("[{}] [{}] {} {}",
           null2empty(rc.request().getHeader(REQUEST_ID)),
           null2empty(rc.request().getHeader(TENANT)),
           rc.request().method(), rc.request().path());
     }
+
     rc.next();
   }
 
@@ -72,5 +81,19 @@ public class LogHelper {
     return String.format("%d object(s): %s",
         collection.size(),
         collection.stream().map(elementMapper).collect(toList()));
+  }
+
+  private static void populateLoggingContext(HttpServerRequest request) {
+    FolioLoggingContext.put(FolioLoggingContext.TENANT_ID_LOGGING_VAR_NAME, request.getHeader(RestVerticle.OKAPI_HEADER_TENANT));
+    FolioLoggingContext.put(FolioLoggingContext.REQUEST_ID_LOGGING_VAR_NAME, request.getHeader(RestVerticle.OKAPI_REQUESTID_HEADER));
+    FolioLoggingContext.put(FolioLoggingContext.USER_ID_LOGGING_VAR_NAME,request.getHeader(RestVerticle.OKAPI_USERID_HEADER));
+    FolioLoggingContext.put(FolioLoggingContext.MODULE_ID_LOGGING_VAR_NAME, MODULE_NAME);
+
+    request.endHandler(v -> {
+      FolioLoggingContext.put(FolioLoggingContext.TENANT_ID_LOGGING_VAR_NAME, null);
+      FolioLoggingContext.put(FolioLoggingContext.REQUEST_ID_LOGGING_VAR_NAME, null);
+      FolioLoggingContext.put(FolioLoggingContext.USER_ID_LOGGING_VAR_NAME, null);
+      FolioLoggingContext.put(FolioLoggingContext.MODULE_ID_LOGGING_VAR_NAME, null);
+    });
   }
 }
