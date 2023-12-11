ebd1eca7609d006a556cc68e5e0bbf2fd88941b4
Niels Erik Nielsen <nielserik@indexdata.com>
2023-12-11 09:42:08 +0100

    SQ CIRC-1923

--- src/main/java/org/folio/circulation/domain/validation/RenewalOfItemsWithReminderFeesValidator.java
+++ src/main/java/org/folio/circulation/domain/validation/RenewalOfItemsWithReminderFeesValidator.java
@@ -38,7 +38,7 @@ public class RenewalOfItemsWithReminderFeesValidator {
 
     if ((lastFeeBilledCount != null && lastFeeBilledCount > 0) && Boolean.FALSE.equals(allowRenewalWhenThereAreReminders)) {
       String reason = "Renewals not allowed for loans with reminders.";
-      log.info(String.format("createBlockedRenewalDueToReminderFeesPolicyError:: %s", reason));
+      log.info("createBlockedRenewalDueToReminderFeesPolicyError:: {}", reason);
       String message = "Loan cannot be renewed because it has reminders.";
       return failed(createBlockedRenewalDueToReminderFeesPolicyError(message, reason));
     } else {
