5ba8f8a4ea4e8db84a3d5bece2178a71169d0940
Niels Erik <nielserik@indexdata.com>
2023-12-23 10:24:30 +0100

    Update src/main/java/org/folio/circulation/domain/validation/RenewalOfItemsWithReminderFeesValidator.java
    Co-authored-by: Roman Barannyk <53909129+roman-barannyk@users.noreply.github.com>

--- src/main/java/org/folio/circulation/domain/validation/RenewalOfItemsWithReminderFeesValidator.java
+++ src/main/java/org/folio/circulation/domain/validation/RenewalOfItemsWithReminderFeesValidator.java
@@ -31,7 +31,6 @@ public class RenewalOfItemsWithReminderFeesValidator {
 
     Loan loan = renewalContext.getLoan();
     Integer lastFeeBilledCount = loan.getLastReminderFeeBilledNumber();
-
     OverdueFinePolicy overdueFinePolicy = loan.getOverdueFinePolicy();
 Boolean allowRenewalWithReminders = overdueFinePolicy.getRemindersPolicy().getAllowRenewalOfItemsWithReminderFees();
 
