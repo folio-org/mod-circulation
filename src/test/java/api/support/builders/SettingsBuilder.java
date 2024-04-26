cdee9e870d03a91f61baa073294cdfe7b1ab94df
Oleksandr Vidinieiev <oleksandr_vidinieiev@epam.com>
2024-04-26 12:07:40 +0300

    CIRC-2081 Merge SettingsBuilder constructors

--- src/test/java/api/support/builders/SettingsBuilder.java
+++ src/test/java/api/support/builders/SettingsBuilder.java
@@ -8,15 +8,7 @@ public class SettingsBuilder implements Builder {
 
   private final JsonObject representation;
 
-  public SettingsBuilder(UUID id, String scope, String key, JsonObject value) {
-    this.representation = new JsonObject()
-      .put("id", id)
-      .put("scope", scope)
-      .put("key", key)
-      .put("value", value);
-  }
-
-  public SettingsBuilder(UUID id, String scope, String key, String value) {
+  public SettingsBuilder(UUID id, String scope, String key, Object value) {
     this.representation = new JsonObject()
       .put("id", id)
       .put("scope", scope)
