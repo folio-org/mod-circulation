949689a85b9392e476052ecd4ecf3c91d41d7fc0
SreejaMangarapu <sreeja_mangarapu@epam.com>
2024-07-15 16:17:25 +0530

    CIRC-2099 added validations for print event flag and request

--- src/test/java/api/printEvents/PrintEventsTests.java
+++ src/test/java/api/printEvents/PrintEventsTests.java
@@ -1,11 +1,16 @@
 package api.printEvents;
 
 import api.support.APITests;
+import api.support.builders.CirculationSettingBuilder;
+import api.support.builders.RequestBuilder;
 import io.vertx.core.json.JsonObject;
 import org.folio.circulation.support.http.client.Response;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static api.support.http.InterfaceUrls.printEventsUrl;
 import static api.support.matchers.ResponseStatusCodeMatcher.hasStatus;
@@ -15,12 +20,67 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 class PrintEventsTests extends APITests {
 
+
   @Test
   void postPrintEventsTest() {
+    circulationSettingsClient.create(new CirculationSettingBuilder()
+      .withName("Enable print event log")
+      .withValue(new JsonObject().put("Enable Print Event", true)));
     JsonObject printRequest = getPrintEvent();
+    printRequest.put("requestIds", createOneHundredRequests());
+    System.out.println(printRequest.getString("requestIds"));
     Response response = restAssuredClient.post(printRequest, printEventsUrl("/print-events-entry"), "post-print-event");
     assertThat(response, hasStatus(HTTP_CREATED));
   }
+
+  @Test
+  void postPrintEventsWhenCirculationSettingIsNotPresentTest() {
+    JsonObject printRequest = getPrintEvent();
+    printRequest.put("requestIds", List.of(UUID.randomUUID()));
+    Response response = restAssuredClient.post(printRequest, printEventsUrl("/print-events-entry"), "post-print-event");
+    assertThat(response, hasStatus(HTTP_UNPROCESSABLE_ENTITY));
+  }
+
+  @Test
+  void postPrintEventsWhenDuplicateCirculationSettingFound() {
+    circulationSettingsClient.create(new CirculationSettingBuilder()
+      .withName("Enable print event log")
+      .withValue(new JsonObject().put("Enable Print Event", true)));
+    circulationSettingsClient.create(new CirculationSettingBuilder()
+      .withName("Enable print event log")
+      .withValue(new JsonObject().put("Enable-Print-Event", false)));
+
+    JsonObject printRequest = getPrintEvent();
+    printRequest.put("requestIds", List.of(UUID.randomUUID()));
+    Response response = restAssuredClient.post(printRequest, printEventsUrl("/print-events-entry"), "post-print-event");
+    assertThat(response, hasStatus(HTTP_UNPROCESSABLE_ENTITY));
+  }
+
+  @Test
+  void postPrintEventsWhenPrintEventSettingIsDisable() {
+    circulationSettingsClient.create(new CirculationSettingBuilder()
+      .withName("Enable print event log")
+      .withValue(new JsonObject().put("Enable Print Event", false)));
+
+    JsonObject printRequest = getPrintEvent();
+    printRequest.put("requestIds", List.of(UUID.randomUUID()));
+    Response response = restAssuredClient.post(printRequest, printEventsUrl("/print-events-entry"), "post-print-event");
+    assertThat(response, hasStatus(HTTP_UNPROCESSABLE_ENTITY));
+  }
+
+  @Test
+  void postPrintEventsWithInvalidRequestId() {
+    circulationSettingsClient.create(new CirculationSettingBuilder()
+      .withName("Enable print event log")
+      .withValue(new JsonObject().put("Enable Print Event", true)));
+    JsonObject printRequest = getPrintEvent();
+    List<UUID> RequestIds = createOneHundredRequests();
+    RequestIds.add(UUID.randomUUID());
+    printRequest.put("requestIds", RequestIds);
+    Response response = restAssuredClient.post(printRequest, printEventsUrl("/print-events-entry"), "post-print-event");
+    assertThat(response, hasStatus(HTTP_UNPROCESSABLE_ENTITY));
+  }
+
 
   @Test
   void postPrintEventsWithInvalidField() {
@@ -48,11 +108,23 @@ class PrintEventsTests extends APITests {
   }
 
   private JsonObject getPrintEvent() {
-    List<String> requestIds = List.of("5f5751b4-e352-4121-adca-204b0c2aec43", "5f5751b4-e352-4121-adca-204b0c2aec44");
     return new JsonObject()
-      .put("requestIds", requestIds)
       .put("requesterId", "5f5751b4-e352-4121-adca-204b0c2aec43")
       .put("requesterName", "requester")
       .put("printEventDate", "2024-06-25T14:30:00Z");
+  }
+
+  private List<UUID> createOneHundredRequests() {
+    final UUID pickupServicePointId = servicePointsFixture.cd1().getId();
+
+    return IntStream.range(0, 100).mapToObj(notUsed -> requestsFixture.place(
+        new RequestBuilder()
+          .open()
+          .page()
+          .forItem(itemsFixture.basedUponSmallAngryPlanet())
+          .by(usersFixture.charlotte())
+          .fulfillToHoldShelf()
+          .withPickupServicePointId(pickupServicePointId)).getId())
+      .collect(Collectors.toList());
   }
 }
