e0757878e7e99d7ed201355ba95dca2c5670a633
Steve Ellis <sellis@ebsco.com>
2025-01-06 16:24:18 -0500

    Adds logging

--- src/main/java/org/folio/circulation/support/results/CommonFailures.java
+++ src/main/java/org/folio/circulation/support/results/CommonFailures.java
@@ -2,16 +2,25 @@ package org.folio.circulation.support.results;
 
 import static org.folio.circulation.support.results.Result.failed;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.folio.circulation.support.ServerErrorFailure;
 
+import java.lang.invoke.MethodHandles;
+
 public class CommonFailures {
+  private static final Logger log = LogManager.getLogger(MethodHandles.lookup().lookupClass());
+  private static final String INTERNAL_SERVER_ERROR = "Internal Server Error";
+
   private CommonFailures() {}
 
   public static <T> Result<T> failedDueToServerError(Throwable e) {
+    log.error(INTERNAL_SERVER_ERROR, e);
     return failed(new ServerErrorFailure(e));
   }
 
   public static <T> Result<T> failedDueToServerError(String reason) {
+    log.error(INTERNAL_SERVER_ERROR, new Throwable(reason));
     return failed(new ServerErrorFailure(reason));
   }
 }
