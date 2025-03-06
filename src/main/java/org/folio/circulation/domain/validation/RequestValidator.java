d874574e5698c80453f1dde8d9d579ca680bd8df
Antony_Hruschev <anton.gitgit@gmail.com>
2025-03-06 12:08:57 +0400

    CIRC-2269 avoid NPE

--- src/main/java/org/folio/circulation/domain/validation/RequestValidator.java
+++ src/main/java/org/folio/circulation/domain/validation/RequestValidator.java
@@ -1,7 +1,12 @@
 package org.folio.circulation.domain.validation;
 
+import java.util.Objects;
+
 import org.folio.circulation.domain.User;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class RequestValidator {
 
   private static final String FAKE_PATRON_FIRST_NAME = "Secure";
@@ -9,9 +14,15 @@ public class RequestValidator {
   private static final String FAKE_PATRON_BARCODE = "securepatron";
 
   public static boolean isMediatedRequest(User requester) {
-    return requester.getFirstName().equals(FAKE_PATRON_FIRST_NAME) &&
-      requester.getLastName().equals(FAKE_PATRON_LAST_NAME) &&
-      requester.getBarcode().equals(FAKE_PATRON_BARCODE);
+
+    if(Objects.isNull(requester)) {
+      log.error("isMediatedRequest:: Requester is null");
+      return false;
+    }
+
+    return requester.getFirstName().equals(FAKE_PATRON_FIRST_NAME)
+      && requester.getLastName().equals(FAKE_PATRON_LAST_NAME)
+      && requester.getBarcode().startsWith(FAKE_PATRON_BARCODE);
   }
 
 }
