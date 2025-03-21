f45d2a980403d2885c33152c345ec228425ab22a
Magzhan <magzhan.artykov@gmail.com>
2025-03-21 18:01:21 +0500

    CIRC-2152 Fix incorrect overdue fine amount (#1545)
    * CIRC-2152 Incorrect overdue fine amount
    
    * CIRC-2152 Incorrect overdue fine amount
    
    * CIRC-2152 Incorrect overdue fine amount
    
    * CIRC-2152 Incorrect overdue fine amount
    
    * CIRC-2152 Incorrect overdue fine amount
    
    ---------
    
    Co-authored-by: Roman Barannyk <53909129+roman-barannyk@users.noreply.github.com>
    (cherry picked from commit 4d6031b27603955df359a4f4ea4df3f429f366a5)

--- src/test/java/org/folio/circulation/infrastructure/storage/CalendarRepositoryTest.java
+++ src/test/java/org/folio/circulation/infrastructure/storage/CalendarRepositoryTest.java
@@ -1,6 +1,6 @@
 package org.folio.circulation.infrastructure.storage;
 
-import static java.time.ZoneOffset.UTC;
+import static org.folio.circulation.support.results.Result.ofAsync;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
@@ -8,6 +8,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
@@ -19,6 +20,7 @@ import org.mockito.ArgumentCaptor;
 
 class CalendarRepositoryTest {
 
+  private static final ZoneId UTC = ZoneId.of("UTC");
   private static final String EXPECTED_PATH =
     "%s/all-openings?startDate=%s&endDate=%s&includeClosed=false&limit=2147483647";
 
@@ -35,7 +37,7 @@ class CalendarRepositoryTest {
     ZonedDateTime endDate = ZonedDateTime.of(2020, 10, 22, 15, 30, 0, 0, UTC);
 
     CalendarRepository calendarRepository = new CalendarRepository(clients);
-    calendarRepository.fetchOpeningDaysBetweenDates(servicePointId, startDate, endDate);
+    calendarRepository.fetchOpeningDaysBetweenDates(servicePointId, startDate, endDate, UTC);
 
     ArgumentCaptor<String> paramsArgumentCaptor = ArgumentCaptor.forClass(String.class);
     verify(collectionResourceClient).get(paramsArgumentCaptor.capture());
@@ -45,4 +47,28 @@ class CalendarRepositoryTest {
       String.format(EXPECTED_PATH, servicePointId, startDate.toLocalDate(), endDate.toLocalDate());
     assertThat(actualPath, is(expectedPath));
   }
+
+  //Covers case CIRC-2152
+  @Test
+  void shouldUseCorrectLocalDatesWhenTimeZoneAffectsDateConversion() {
+    Clients clients = mock(Clients.class);
+    CollectionResourceClient collectionResourceClient = mock(CollectionResourceClient.class);
+    when(clients.calendarStorageClient()).thenReturn(collectionResourceClient);
+    when(collectionResourceClient.get(any(String.class))).thenReturn(ofAsync(null));
+
+    ZoneId zone = ZoneId.of("America/Toronto");
+    String servicePointId = UUID.randomUUID().toString();
+    ZonedDateTime dueDate = ZonedDateTime.of(2024, 7, 24, 1, 29, 3, 0, ZoneId.of("UTC"));
+    ZonedDateTime returnDate = ZonedDateTime.of(2024, 7, 24, 17, 56, 37, 0, ZoneId.of("UTC"));
+
+    CalendarRepository calendarRepository = new CalendarRepository(clients);
+    calendarRepository.fetchOpeningDaysBetweenDates(servicePointId, dueDate, returnDate, zone);
+
+    ArgumentCaptor<String> paramsArgumentCaptor = ArgumentCaptor.forClass(String.class);
+    verify(collectionResourceClient).get(paramsArgumentCaptor.capture());
+    String actualPath = paramsArgumentCaptor.getValue();
+    String expectedPath = String.format(EXPECTED_PATH, servicePointId, "2024-07-23", "2024-07-24");
+    assertThat(actualPath, is(expectedPath));
+  }
+
 }
