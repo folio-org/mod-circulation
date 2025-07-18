75791239947e67143cea1cae0ce46ebb70f7f013
Oleksandr Vidinieiev <oleksandr_vidinieiev@epam.com>
2025-07-18 15:21:44 +0200

    CIRC-2322 Add merge functions

--- src/main/java/org/folio/circulation/services/RequestFetchService.java
+++ src/main/java/org/folio/circulation/services/RequestFetchService.java
@@ -97,7 +97,7 @@ public class RequestFetchService {
     MultipleRecords<Request> requests, Collection<Item> items) {
 
     Map<String, Item> itemMap = items.stream()
-      .collect(toMap(Item::getItemId, identity()));
+      .collect(toMap(Item::getItemId, identity(), (a, b) -> a));
 
     return succeeded(requests.mapRecords(request -> request.withItem(
       itemMap.getOrDefault(request.getItemId(), null))));
@@ -139,7 +139,7 @@ public class RequestFetchService {
     }
 
     var fetchedInstancesByIdMap = context.getInstances().getRecords().stream()
-      .collect(Collectors.toMap(Instance::getId, identity()));
+      .collect(Collectors.toMap(Instance::getId, identity(), (a, b) -> a));
     log.info("mapRequestsToInstances:: fetchedInstanceIds: {}",
       () -> collectionAsString(fetchedInstancesByIdMap.keySet()));
 
