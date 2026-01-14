e3d48f4e43b70b8971eb3af3ff8718aca9558b9d
Yuntian Hu <48864579+yuntianhu@users.noreply.github.com>
2026-01-14 14:02:32 -0500

    mod-circulatoin-2520 format correction

--- src/main/java/org/folio/circulation/resources/ScheduledRequestAnonymizationProcessingResource.java
+++ src/main/java/org/folio/circulation/resources/ScheduledRequestAnonymizationProcessingResource.java
@@ -1,23 +1,12 @@
 package org.folio.circulation.resources;
 
-import static org.folio.circulation.support.results.AsynchronousResultBindings.safelyInitialise;
-
 import java.lang.invoke.MethodHandles;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.folio.Environment;
-import org.folio.circulation.infrastructure.storage.ConfigurationRepository;
-import org.folio.circulation.infrastructure.storage.feesandfines.AccountRepository;
-import org.folio.circulation.infrastructure.storage.inventory.ItemRepository;
-import org.folio.circulation.infrastructure.storage.users.UserRepository;
-import org.folio.circulation.services.EventPublisher;
 import org.folio.circulation.support.Clients;
 import org.folio.circulation.support.RouteRegistration;
-import org.folio.circulation.support.http.server.JsonHttpResponse;
 import org.folio.circulation.support.http.server.WebContext;
-import org.folio.circulation.support.results.CommonFailures;
-import org.folio.circulation.support.utils.ClockUtil;
 
 import io.vertx.core.http.HttpClient;
 import io.vertx.ext.web.Router;
@@ -26,7 +15,6 @@ import io.vertx.ext.web.RoutingContext;
 /**
  * Perform automatic loan anonymization based on tenant settings for request
  * This process is intended to run in short intervals.
- *
  */
 public class ScheduledRequestAnonymizationProcessingResource extends Resource {
   private final Logger log = LogManager.getLogger(MethodHandles.lookup().lookupClass());
--- src/test/java/org/folio/circulation/resources/ScheduledRequestAnonymizationProcessingResourceTest.java
+++ src/test/java/org/folio/circulation/resources/ScheduledRequestAnonymizationProcessingResourceTest.java
@@ -1,20 +1,12 @@
 package org.folio.circulation.resources;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.folio.circulation.resources.ScheduledRequestAnonymizationProcessingResource;
 
-
-import io.vertx.core.http.HttpClient;
 import org.junit.jupiter.api.Test;
 
 import io.vertx.core.Vertx;
-import io.vertx.core.AsyncResult;
-import io.vertx.core.buffer.Buffer;
+import io.vertx.core.http.HttpClient;
 import io.vertx.ext.web.Router;
-import io.vertx.ext.web.client.WebClient;
-import io.vertx.ext.web.client.WebClientOptions;
-import io.vertx.ext.web.client.HttpResponse;
-
 
 class ScheduledRequestAnonymizationProcessingResourceTest {
 
@@ -42,7 +34,6 @@ class ScheduledRequestAnonymizationProcessingResourceTest {
       .anyMatch(route ->
         "/circulation/scheduled-request-anonymize-processing"
           .equals(route.getPath()));
-
     assertEquals(true, routeExists);
   }
 }
