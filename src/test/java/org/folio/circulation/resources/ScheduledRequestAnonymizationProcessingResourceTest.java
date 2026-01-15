d41a30f0fbfbb866a3581171fb7857fc1adb5d19
Yuntian Hu <48864579+yuntianhu@users.noreply.github.com>
2026-01-15 15:38:33 -0500

    mod-circulatoin-2520 fix issues

--- src/test/java/org/folio/circulation/resources/ScheduledRequestAnonymizationProcessingResourceTest.java
+++ src/test/java/org/folio/circulation/resources/ScheduledRequestAnonymizationProcessingResourceTest.java
@@ -1,14 +1,12 @@
 package org.folio.circulation.resources;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpClient;
-import io.vertx.core.http.HttpServerResponse;
-import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.Router;
 
 class ScheduledRequestAnonymizationProcessingResourceTest {
@@ -17,11 +15,17 @@ class ScheduledRequestAnonymizationProcessingResourceTest {
   void scheduledRequestAnonymizationResourceRegisters() {
     Vertx vertx = Vertx.vertx();
     Router router = Router.router(vertx);
-
     HttpClient client = vertx.createHttpClient();
 
     new ScheduledRequestAnonymizationProcessingResource(client)
       .register(router);
+
+    assertTrue(
+      router.getRoutes().stream()
+        .anyMatch(route ->
+          "/circulation/scheduled-request-anonymize-processing"
+            .equals(route.getPath()))
+    );
   }
 
   @Test
