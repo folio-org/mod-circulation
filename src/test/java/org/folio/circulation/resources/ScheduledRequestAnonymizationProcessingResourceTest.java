1fa75eedfd1fb0a9cbb4c13f8e1b0f783b423571
Yuntian Hu <48864579+yuntianhu@users.noreply.github.com>
2026-01-15 09:59:17 -0500

    mod-circulatoin-2520 add test

--- src/test/java/org/folio/circulation/resources/ScheduledRequestAnonymizationProcessingResourceTest.java
+++ src/test/java/org/folio/circulation/resources/ScheduledRequestAnonymizationProcessingResourceTest.java
@@ -3,9 +3,12 @@ package org.folio.circulation.resources;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpClient;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.Router;
 
 class ScheduledRequestAnonymizationProcessingResourceTest {
@@ -36,4 +39,20 @@ class ScheduledRequestAnonymizationProcessingResourceTest {
           .equals(route.getPath()));
     assertEquals(true, routeExists);
   }
+
+  @Test
+  void scheduledAnonymizeRequest_initializesWebContextAndClients() {
+    Vertx vertx = Vertx.vertx();
+    HttpClient httpClient = vertx.createHttpClient();
+
+    ScheduledRequestAnonymizationProcessingResource resource =
+      new ScheduledRequestAnonymizationProcessingResource(httpClient);
+
+    RoutingContext routingContext = Mockito.mock(RoutingContext.class);
+    HttpServerResponse response = Mockito.mock(HttpServerResponse.class);
+
+    Mockito.when(routingContext.response()).thenReturn(response);
+    Mockito.when(response.setStatusCode(Mockito.anyInt())).thenReturn(response);
+  }
+
 }
