136da60e6bd7a11c1e22167bc3a68ac1ec22c595
Julian Ladisch <julianladisch@users.noreply.github.com>
2026-02-06 17:38:44 +0100

    test with KAFKA_PORT=x

--- src/test/java/org/folio/circulation/LauncherTest.java
+++ src/test/java/org/folio/circulation/LauncherTest.java
@@ -1,9 +1,13 @@
 package org.folio.circulation;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.instanceOf;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.concurrent.ExecutionException;
+
+import org.apache.kafka.common.KafkaException;
 import org.folio.circulation.support.VertxAssistant;
 import org.folio.rest.tools.utils.NetworkUtils;
 import org.junit.jupiter.api.AfterAll;
@@ -16,7 +20,7 @@ class LauncherTest {
   @BeforeAll
   static void beforeAll() {
     oldKafkaPort = System.getProperty("KAFKA_PORT");
-    System.setProperty("KAFKA_PORT", "1");
+    System.setProperty("KAFKA_PORT", "x");
   }
 
   @AfterAll
@@ -32,7 +36,7 @@ class LauncherTest {
   void failOnWrongKafkaConfig() {
     var port = NetworkUtils.nextFreePort();
     var future = new Launcher(new VertxAssistant()).start(port);
-    var e = assertThrows(Exception.class, future::get);
-    assertThat(e.getMessage(), containsString("Kafka"));
+    var e = assertThrows(ExecutionException.class, future::get);
+    assertThat(e.getCause(), is(instanceOf(KafkaException.class)));
   }
 }
