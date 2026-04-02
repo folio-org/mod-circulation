554fa2a162cf0e0e8d86a5346c6299ed7a3d5b3d
Long.Vo(Connor) <longvo.cv@gmail.com>
2026-04-02 14:52:57 -0400

    CIRC-2531 Add logging and use HTTP_CREATED constant

--- src/main/java/org/folio/circulation/infrastructure/storage/requests/AnonymizeStorageRequestsRepository.java
+++ src/main/java/org/folio/circulation/infrastructure/storage/requests/AnonymizeStorageRequestsRepository.java
@@ -5,10 +5,14 @@ import static org.folio.circulation.support.http.ResponseMapping.forwardOnFailur
 import static org.folio.circulation.support.http.ResponseMapping.mapUsingJson;
 import static org.folio.circulation.support.json.JsonStringArrayPropertyFetcher.toStream;
 import static org.folio.circulation.support.results.Result.succeeded;
+import static org.folio.HttpStatus.HTTP_CREATED;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Function;
+import java.lang.invoke.MethodHandles;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.folio.circulation.domain.anonymization.RequestAnonymizationRecords;
 import org.folio.circulation.support.Clients;
 import org.folio.circulation.support.CollectionResourceClient;
@@ -21,6 +25,8 @@ import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 
 public class AnonymizeStorageRequestsRepository {
+  private static final Logger log = LogManager.getLogger(MethodHandles.lookup().lookupClass());
+
   private final CollectionResourceClient requestStorageClient;
 
   public AnonymizeStorageRequestsRepository(Clients clients) {
@@ -31,20 +37,30 @@ public class AnonymizeStorageRequestsRepository {
   createStorageRequestResponseInterpreter(RequestAnonymizationRecords records) {
 
     Function<Response, Result<RequestAnonymizationRecords>> mapper = mapUsingJson(
-      response -> records.withAnonymizedRequests(
-        toStream(response, "anonymizedRequests").toList()));
+      response -> {
+        var anonymized = toStream(response, "anonymizedRequests").toList();
+        log.info("createStorageRequestResponseInterpreter:: successfully anonymized {} requests",
+          anonymized.size());
+        return records.withAnonymizedRequests(anonymized);
+      });
 
     return new ResponseInterpreter<RequestAnonymizationRecords>()
-      .on(201, Result.of(() -> records))
+      .on(HTTP_CREATED.toInt(), Result.of(() -> records))
       .otherwise(forwardOnFailure());
   }
 
   public CompletableFuture<Result<RequestAnonymizationRecords>>
   postAnonymizeStorageRequests(RequestAnonymizationRecords records) {
 
+    log.debug("postAnonymizeStorageRequests:: parameters records: {}", records);
+
     if (records.getAnonymizedRequestIds().isEmpty()) {
+      log.info("postAnonymizeStorageRequests:: no requests to anonymize, skipping");
       return completedFuture(succeeded(records));
     }
+
+    log.info("postAnonymizeStorageRequests:: anonymizing {} requests",
+      records.getAnonymizedRequestIds().size());
     RequestBatch requestBatch = new RequestBatch(records.getAnonymizedRequests());
 
     return requestStorageClient.post(requestBatch.toJson())
